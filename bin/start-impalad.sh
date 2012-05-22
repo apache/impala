a3eef52f9f96349119e6d029f36bc3ff706d18ab
Lenni Kuff <lskuff@cloudera.com>
2012-05-22 16:14:45 -0700

    Added additional dependency to start-impala classpath settings

--- bin/start-impalad.sh
+++ bin/start-impalad.sh
@@ -6,7 +6,8 @@ $IMPALA_HOME/fe/target/dependency:\
 $IMPALA_HOME/fe/target/test-classes:\
 $IMPALA_HOME/thirdparty/hive-0.8.0-cdh4b1/lib/datanucleus-core-2.0.3.jar:\
 $IMPALA_HOME/thirdparty/hive-0.8.0-cdh4b1/lib/datanucleus-enhancer-2.0.3.jar:\
-$IMPALA_HOME/thirdparty/hive-0.8.0-cdh4b1/lib/datanucleus-rdbms-2.0.3.jar:
+$IMPALA_HOME/thirdparty/hive-0.8.0-cdh4b1/lib/datanucleus-rdbms-2.0.3.jar:\
+$IMPALA_HOME/thirdparty/hive-0.8.0-cdh4b1/lib/datanucleus-connectionpool-2.0.3.jar
 
 export CLASSPATH
 $IMPALA_HOME/be/build/debug/service/impalad
