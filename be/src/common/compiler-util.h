e7df0160c236ea8c69af6a84d6f87f409f781c4b
Michael Ubell <ubell@cloudera.com>
2012-06-12 11:26:14 -0700

    LIKELY and UNLIKELY are defined backwards.

--- be/src/common/compiler-util.h
+++ be/src/common/compiler-util.h
@@ -3,13 +3,13 @@
 #ifndef IMPALA_COMMON_COMPILER_UTIL_H
 #define IMPALA_COMMON_COMPILER_UTIL_H
 
-// Compiler hint that this branch is likely or unlikely to 
+// Compiler hint that this branch is likely or unlikely to
 // be taken. Take from the "What all programmers should know
 // about memory" paper.
 // example: if (LIKELY(size > 0)) { ... }
 // example: if (UNLIKELY(!status.ok())) { ... }
-#define LIKELY(expr) __builtin_expect(!!(expr), 0)
-#define UNLIKELY(expr) __builtin_expect(!!(expr), 1)
+#define LIKELY(expr) __builtin_expect(!!(expr), 1)
+#define UNLIKELY(expr) __builtin_expect(!!(expr), 0)
 
 #define PREFETCH(addr) __builtin_prefetch(addr)
 
