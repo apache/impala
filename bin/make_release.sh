0e844e71871921af08932fe6ea7b557feb8fc941
Lenni Kuff <lskuff@cloudera.com>
2012-06-12 17:31:08 -0700

    Updated make_release script to add flag for controlling whether or not to do PGO build

--- bin/make_release.sh
+++ bin/make_release.sh
@@ -2,52 +2,84 @@
 set -e
 set -u
 
+USE_PGO=1
+
+# parse command line options
+for ARG in $*
+do
+  case "$ARG" in
+    -nopgo)
+      USE_PGO=0
+      ;;
+    -help|*)
+      echo "make_release.sh [-nopgo]"
+      echo "[-nopgo] : do not use performance guided optimizations for building"
+      exit 1
+      ;;
+  esac
+done
+
+
 cd $IMPALA_HOME
 
-# build with profile gen enabled
-cmake -DCMAKE_BUILD_TYPE=PROFILE_GEN .
-make clean
-cd $IMPALA_HOME/common/function-registry
-make
-cd $IMPALA_HOME/common/thrift
-make
-cd $IMPALA_BE_DIR
-make -j
-cd $IMPALA_HOME
+if [ $USE_PGO -eq 1 ]
+then
 
-# Run sample queries - outputs .gcda files
-be/build/release/service/runquery -query="\
-  select count(field) from grep1gb_text_none where field like '%xyz%';\
-  select count(field) from grep1gb_rc_file_none where field like '%xyz%';\
-  select count(field) from grep1gb_sequence_file_snappy where field like '%xyz%';\
-  select sourceIP, SUM(adRevenue) FROM uservisits_web_sequence_file_none \
-    GROUP by sourceIP order by SUM(adRevenue) desc limit 10;\
-  select sourceIP, SUM(adRevenue) FROM uservisits_web_text_none \
-    GROUP by sourceIP order by SUM(adRevenue) desc limit 10;\
-  select sourceIP, SUM(adRevenue) FROM uservisits_web_rc_file_none GROUP by sourceIP \
-    order by SUM(adRevenue) desc limit 10;select sourceIP, SUM(adRevenue) \
-    FROM uservisits_web_text_none \
-    GROUP by sourceIP order by SUM(adRevenue) desc limit 10;\
-  select uv.sourceip, avg(r.pagerank), sum(uv.adrevenue) as totalrevenue \
-    from uservisits_web_text_none uv join rankings_web_text_none r on \
-    (r.pageurl = uv.desturl) \
-    where uv.visitdate > '1999-01-01' and uv.visitdate < '2000-01-01' \
-    group by uv.sourceip order by totalrevenue desc limit 1"\
-  -profile_output_file=""
+  # build with profile gen enabled
+  cmake -DCMAKE_BUILD_TYPE=PROFILE_GEN .
+  make clean
+  cd $IMPALA_HOME/common/function-registry
+  make
+  cd $IMPALA_HOME/common/thrift
+  make
+  cd $IMPALA_BE_DIR
+  make -j
+  cd $IMPALA_HOME
 
-# Build again using the PGO data
-cmake -DCMAKE_BUILD_TYPE=PROFILE_BUILD .
-make clean
-cd $IMPALA_HOME/common/function-registry
-make
-cd $IMPALA_HOME/common/thrift
-make
-cd $IMPALA_BE_DIR
-make -j
-cd $IMPALA_HOME
+  # Run sample queries - outputs .gcda files
+  be/build/release/service/runquery -query="\
+    select count(field) from grep1gb_text_none where field like '%xyz%';\
+    select count(field) from grep1gb_rc_file_none where field like '%xyz%';\
+    select count(field) from grep1gb_sequence_file_snappy where field like '%xyz%';\
+    select sourceIP, SUM(adRevenue) FROM uservisits_web_sequence_file_none \
+      GROUP by sourceIP order by SUM(adRevenue) desc limit 10;\
+    select sourceIP, SUM(adRevenue) FROM uservisits_web_text_none \
+      GROUP by sourceIP order by SUM(adRevenue) desc limit 10;\
+    select sourceIP, SUM(adRevenue) FROM uservisits_web_rc_file_none GROUP by sourceIP \
+      order by SUM(adRevenue) desc limit 10;select sourceIP, SUM(adRevenue) \
+      FROM uservisits_web_text_none \
+      GROUP by sourceIP order by SUM(adRevenue) desc limit 10;\
+    select uv.sourceip, avg(r.pagerank), sum(uv.adrevenue) as totalrevenue \
+      from uservisits_web_text_none uv join rankings_web_text_none r on \
+      (r.pageurl = uv.desturl) \
+      where uv.visitdate > '1999-01-01' and uv.visitdate < '2000-01-01' \
+      group by uv.sourceip order by totalrevenue desc limit 1"\
+    -profile_output_file=""
 
-# Remove all the PGO intermediates and set build to release.  This seems to be the most
-# convenient workflow.  Otherwise, changing one src file after a PGO build causes many warnings
-# about stale PGO data.
-find . -type f -name "*.gcda" -exec rm -rf {} \;
-cmake -DCMAKE_BUILD_TYPE=RELEASE
+  # Build again using the PGO data
+  cmake -DCMAKE_BUILD_TYPE=PROFILE_BUILD .
+  make clean
+  cd $IMPALA_HOME/common/function-registry
+  make
+  cd $IMPALA_HOME/common/thrift
+  make
+  cd $IMPALA_BE_DIR
+  make -j
+  cd $IMPALA_HOME
+
+  # Remove all the PGO intermediates and set build to release.  This seems to be the most
+  # convenient workflow.  Otherwise, changing one src file after a PGO build causes many warnings
+  # about stale PGO data.
+  find . -type f -name "*.gcda" -exec rm -rf {} \;
+  cmake -DCMAKE_BUILD_TYPE=RELEASE
+else
+  cmake -DCMAKE_BUILD_TYPE=RELEASE
+  make clean
+
+  cd $IMPALA_HOME/common/function-registry
+  make
+  cd $IMPALA_HOME/common/thrift
+  make
+  cd $IMPALA_BE_DIR
+  make -j
+fi
