96543325a72180a36fb6adb36190905979cba633
Lenni Kuff <lskuff@cloudera.com>
2012-06-29 09:45:32 -0700

    Fixed run-mini-dfs script to resolve issue starting cluster

--- testdata/bin/run-mini-dfs.sh
+++ testdata/bin/run-mini-dfs.sh
@@ -5,11 +5,7 @@
 $IMPALA_HOME/testdata/bin/kill-mini-dfs.sh
 
 # Starts up a three-node single-process cluster; the NN listens on port 20500.
-CLASSPATH=
-for jar in `find ${IMPALA_HOME}/thirdparty/hadoop-2.0.0-cdh4.1.0-SNAPSHOT/ -name "*.jar"`; do
-  CLASSPATH=${CLASSPATH}:${jar}
-done;
 pushd ${HADOOP_HOME}
-java org.apache.hadoop.test.MiniDFSClusterManager -datanodes 3 -nnport=20500 $@ &
+hadoop org.apache.hadoop.test.MiniDFSClusterManager -datanodes 3 -nnport=20500 $@ &
 popd
 sleep 10
