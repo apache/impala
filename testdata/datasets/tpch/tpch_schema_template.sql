a0229cd12e661539c86fd6bc6145a57d0234af9e
Nong Li <nong@cloudera.com>
2013-02-14 14:57:44 -0800

    Update tpch schema to use bigint for keys.

--- testdata/datasets/tpch/tpch_schema_template.sql
+++ testdata/datasets/tpch/tpch_schema_template.sql
@@ -8,9 +8,9 @@ tpch%(scale_factor)s.lineitem
 ---- CREATE
 CREATE DATABASE IF NOT EXISTS tpch%(scale_factor)s;
 CREATE EXTERNAL TABLE %(table_name)s (
-L_ORDERKEY INT,
-L_PARTKEY INT,
-L_SUPPKEY INT,
+L_ORDERKEY BIGINT,
+L_PARTKEY BIGINT,
+L_SUPPKEY BIGINT,
 L_LINENUMBER INT,
 L_QUANTITY DOUBLE,
 L_EXTENDEDPRICE DOUBLE,
@@ -39,7 +39,7 @@ tpch
 tpch%(scale_factor)s.part
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-P_PARTKEY INT,
+P_PARTKEY BIGINT,
 P_NAME STRING,
 P_MFGR STRING,
 P_BRAND STRING,
@@ -64,8 +64,8 @@ tpch
 tpch%(scale_factor)s.partsupp
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-PS_PARTKEY INT,
-PS_SUPPKEY INT,
+PS_PARTKEY BIGINT,
+PS_SUPPKEY BIGINT,
 PS_AVAILQTY INT,
 PS_SUPPLYCOST DOUBLE,
 PS_COMMENT STRING)
@@ -84,10 +84,10 @@ tpch
 tpch%(scale_factor)s.supplier
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-S_SUPPKEY INT,
+S_SUPPKEY BIGINT,
 S_NAME STRING,
 S_ADDRESS STRING,
-S_NATIONKEY INT,
+S_NATIONKEY SMALLINT,
 S_PHONE STRING,
 S_ACCTBAL DOUBLE,
 S_COMMENT STRING)
@@ -106,9 +106,9 @@ tpch
 tpch%(scale_factor)s.nation
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-N_NATIONKEY INT,
+N_NATIONKEY SMALLINT,
 N_NAME STRING,
-N_REGIONKEY INT,
+N_REGIONKEY SMALLINT,
 N_COMMENT STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '|'
 STORED AS %(file_format)s
@@ -125,7 +125,7 @@ tpch
 tpch%(scale_factor)s.region
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-R_REGIONKEY INT,
+R_REGIONKEY SMALLINT,
 R_NAME STRING,
 R_COMMENT STRING)
 ROW FORMAT DELIMITED FIELDS TERMINATED BY '|'
@@ -143,8 +143,8 @@ tpch
 tpch%(scale_factor)s.orders
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-O_ORDERKEY INT,
-O_CUSTKEY INT,
+O_ORDERKEY BIGINT,
+O_CUSTKEY BIGINT,
 O_ORDERSTATUS STRING,
 O_TOTALPRICE DOUBLE,
 O_ORDERDATE STRING,
@@ -167,10 +167,10 @@ tpch
 tpch%(scale_factor)s.customer
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-C_CUSTKEY INT,
+C_CUSTKEY BIGINT,
 C_NAME STRING,
 C_ADDRESS STRING,
-C_NATIONKEY INT,
+C_NATIONKEY SMALLINT,
 C_PHONE STRING,
 C_ACCTBAL DOUBLE,
 C_MKTSEGMENT STRING,
@@ -193,7 +193,7 @@ CREATE EXTERNAL TABLE %(table_name)s (
 s_acctbal double,
 s_name string,
 n_name string,
-p_partkey int,
+p_partkey bigint,
 ps_supplycost double,
 p_mfgr string,
 s_address string,
@@ -208,7 +208,7 @@ tpch
 tpch%(scale_factor)s.q2_minimum_cost_supplier_tmp2
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-p_partkey int,
+p_partkey bigint,
 ps_min_supplycost double)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
@@ -221,8 +221,8 @@ tpch%(scale_factor)s.q7_volume_shipping_tmp
 CREATE EXTERNAL TABLE %(table_name)s (
 supp_nation string,
 cust_nation string,
-s_nationkey int,
-c_nationkey int)
+s_nationkey smallint,
+c_nationkey smallint)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
 ====
@@ -232,7 +232,7 @@ tpch
 tpch%(scale_factor)s.q11_part_tmp
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-ps_partkey int,
+ps_partkey bigint,
 part_value double)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
@@ -252,7 +252,7 @@ tpch
 tpch%(scale_factor)s.revenue
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-supplier_no int,
+supplier_no bigint,
 total_revenue double)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
@@ -271,7 +271,7 @@ tpch
 ---- BASE_TABLE_NAME
 tpch%(scale_factor)s.supplier_tmp
 ---- CREATE
-CREATE EXTERNAL TABLE %(table_name)s (s_suppkey int)
+CREATE EXTERNAL TABLE %(table_name)s (s_suppkey bigint)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
 ====
@@ -284,7 +284,7 @@ CREATE EXTERNAL TABLE %(table_name)s (
 p_brand string,
 p_type string,
 p_size int,
-ps_suppkey int)
+ps_suppkey bigint)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
 ====
@@ -294,7 +294,7 @@ tpch
 tpch%(scale_factor)s.lineitem_tmp
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-t_partkey int,
+t_partkey bigint,
 t_avg_quantity double)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
@@ -305,7 +305,7 @@ tpch
 tpch%(scale_factor)s.q18_tmp
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-l_orderkey int,
+l_orderkey bigint,
 t_sum_quantity double)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
@@ -315,7 +315,7 @@ tpch
 ---- BASE_TABLE_NAME
 tpch%(scale_factor)s.q20_tmp1
 ---- CREATE
-CREATE EXTERNAL TABLE %(table_name)s (p_partkey int)
+CREATE EXTERNAL TABLE %(table_name)s (p_partkey bigint)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
 ====
@@ -325,8 +325,8 @@ tpch
 tpch%(scale_factor)s.q20_tmp2
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-l_partkey int,
-l_suppkey int,
+l_partkey bigint,
+l_suppkey bigint,
 sum_quantity double)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
@@ -337,7 +337,7 @@ tpch
 tpch%(scale_factor)s.q20_tmp3
 ---- CREATE
 CREATE EXTERNAL TABLE %(table_name)s (
-ps_suppkey int,
+ps_suppkey bigint,
 ps_availqty int,
 sum_quantity double)
 STORED AS %(file_format)s
@@ -348,7 +348,7 @@ tpch
 ---- BASE_TABLE_NAME
 tpch%(scale_factor)s.q20_tmp4
 ---- CREATE
-CREATE EXTERNAL TABLE %(table_name)s (ps_suppkey int)
+CREATE EXTERNAL TABLE %(table_name)s (ps_suppkey bigint)
 STORED AS %(file_format)s
 LOCATION '${hiveconf:hive.metastore.warehouse.dir}/%(table_name)s';
 ====
