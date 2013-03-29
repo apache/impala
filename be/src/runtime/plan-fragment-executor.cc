f078beda87d82589d090ea26e30fa44f2db90f23
Alan Choi <alan@cloudera.com>
2013-03-28 22:24:55 -0700

    IMPALA-141 do not log status to error log if status is cancelled.

--- be/src/runtime/plan-fragment-executor.cc
+++ be/src/runtime/plan-fragment-executor.cc
@@ -229,7 +229,7 @@ Status PlanFragmentExecutor::Open() {
   }
 
   Status status = OpenInternal();
-  if (!status.ok() && runtime_state_->LogHasSpace()) {
+  if (!status.ok() && !status.IsCancelled() && runtime_state_->LogHasSpace()) {
     // Log error message in addition to returning in Status. Queries that do not
     // fetch results (e.g. insert) may not receive the message directly and can
     // only retrieve the log.
