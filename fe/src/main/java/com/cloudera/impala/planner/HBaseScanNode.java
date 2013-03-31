8207047b85091b4bddfee91146541ab33aef6008
Marcel Kornacker <marcel@cloudera.com>
2013-03-31 07:18:29 -0700

    fix broken build

--- fe/src/main/java/com/cloudera/impala/planner/HBaseScanNode.java
+++ fe/src/main/java/com/cloudera/impala/planner/HBaseScanNode.java
@@ -346,8 +346,6 @@ public class HBaseScanNode extends ScanNode {
     if (!Bytes.equals(stopKey, HConstants.EMPTY_END_ROW)) {
       output.append(prefix + "  STOP KEY: " + printKey(stopKey) + "\n");
     }
-    output.append(prefix +  "  ESTIMATED ROWS BEFORE FILTER: " +
-        tbl.getEstimatedRowCount(this.startKey, this.stopKey)  + "\n");
     if (!filters.isEmpty()) {
       output.append(prefix + "  HBASE FILTERS: ");
       if (filters.size() == 1) {
