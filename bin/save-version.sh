83ba08cf1319ed06021180d6785211bcf24aa508
Nong Li <nong@cloudera.com>
2013-04-16 13:02:13 -0700

    Bump version to .7.1

--- bin/save-version.sh
+++ bin/save-version.sh
@@ -14,7 +14,7 @@
 # limitations under the License.
 
 # Generates the impala version and build information.
-VERSION=0.7
+VERSION=0.7.1
 GIT_HASH=$(git rev-parse HEAD)
 BUILD_TIME=`date`
 HEADER="# Generated version information from save-version.sh"
