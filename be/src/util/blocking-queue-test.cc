1a5c2c137ef8bd603e3a0a89f0a1c2648acba9d7
Aaron Davidson <aaron.davidson@cloudera.com>
2013-06-17 18:57:00 -0700

    Adds a BlockingQueue
    This queue just implements the most basic interface with BlockingGet() and
    BlockingPut(), plus an additional Shutdown() method which prevents further
    BlockingPut()s.

---
+++ be/src/util/blocking-queue-test.cc
@@ -0,0 +1,140 @@
+// Copyright 2013 Cloudera Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#include <boost/thread.hpp>
+#include <boost/thread/mutex.hpp>
+#include <glog/logging.h>
+#include <gtest/gtest.h>
+#include <unistd.h>
+
+#include "util/blocking-queue.h"
+
+using namespace boost;
+using namespace std;
+
+namespace impala {
+
+TEST(BlockingQueueTest, TestBasic) {
+  int32_t i;
+  BlockingQueue<int32_t> test_queue(5);
+  test_queue.BlockingPut(1);
+  test_queue.BlockingPut(2);
+  test_queue.BlockingPut(3);
+  ASSERT_TRUE(test_queue.BlockingGet(&i));
+  ASSERT_EQ(1, i);
+  ASSERT_TRUE(test_queue.BlockingGet(&i));
+  ASSERT_EQ(2, i);
+  ASSERT_TRUE(test_queue.BlockingGet(&i));
+  ASSERT_EQ(3, i);
+}
+
+TEST(BlockingQueueTest, TestGetFromShutdownQueue) {
+  int64_t i;
+  BlockingQueue<int64_t> test_queue(2);
+  test_queue.BlockingPut(123);
+  test_queue.Shutdown();
+  ASSERT_FALSE(test_queue.BlockingPut(456));
+  ASSERT_TRUE(test_queue.BlockingGet(&i));
+  ASSERT_EQ(123, i);
+  ASSERT_FALSE(test_queue.BlockingGet(&i));
+}
+
+class MultiThreadTest {
+ public:
+  MultiThreadTest()
+    : iterations_(10000),
+      nthreads_(5),
+      queue_(iterations_*nthreads_/10),
+      num_inserters_(nthreads_) {
+  }
+
+  void InserterThread(int arg) {
+    for (int i = 0; i < iterations_; ++i) {
+      queue_.BlockingPut(arg);
+    }
+
+    {
+      lock_guard<mutex> guard(lock_);
+      if (--num_inserters_ == 0) {
+        queue_.Shutdown();
+      }
+    }
+  }
+
+  void RemoverThread() {
+    for (int i = 0; i < iterations_; ++i) {
+      int32_t arg;
+      bool got = queue_.BlockingGet(&arg);
+      if (!got) arg = -1;
+
+      {
+        lock_guard<mutex> guard(lock_);
+        gotten_[arg] = gotten_[arg] + 1;
+      }
+    }
+  }
+
+  void Run() {
+    for (int i = 0; i < nthreads_; ++i) {
+      threads_.push_back(shared_ptr<thread>(
+          new thread(bind(&MultiThreadTest::InserterThread, this, i))));
+      threads_.push_back(shared_ptr<thread>(
+          new thread(bind(&MultiThreadTest::RemoverThread, this))));
+    }
+    // We add an extra thread to ensure that there aren't enough elements in
+    // the queue to go around.  This way, we test removal after Shutdown.
+    threads_.push_back(shared_ptr<thread>(
+            new thread(bind(
+              &MultiThreadTest::RemoverThread, this))));
+    for (int i = 0; i < threads_.size(); ++i) {
+      threads_[i]->join();
+    }
+
+    // Let's check to make sure we got what we should have.
+    lock_guard<mutex> guard(lock_);
+    for (int i = 0; i < nthreads_; ++i) {
+      ASSERT_EQ(iterations_, gotten_[i]);
+    }
+    // And there were nthreads_ * (iterations_ + 1)  elements removed, but only
+    // nthreads_ * iterations_ elements added.  So some removers hit the shutdown
+    // case.
+    ASSERT_EQ(iterations_, gotten_[-1]);
+  }
+
+ private:
+  typedef vector<shared_ptr<thread> > ThreadVector;
+
+  int iterations_;
+  int nthreads_;
+  BlockingQueue<int32_t> queue_;
+  // Lock for gotten_ and num_inserters_.
+  mutex lock_;
+  // Map from inserter thread id to number of consumed elements from that id.
+  // Ultimately, this should map each thread id to insertions_ elements.
+  // Additionally, if the BlockingGet returns false, this increments id=-1.
+  map<int32_t, int> gotten_;
+  // All inserter and remover threads.
+  ThreadVector threads_;
+  // Number of inserters which haven't yet finished inserting.
+  int num_inserters_;
+};
+
+TEST(BlockingQueueTest, TestMultipleThreads) {
+  MultiThreadTest test;
+  test.Run();
+}
+
+}
+
---
+++ be/src/util/blocking-queue.h
@@ -0,0 +1,122 @@
+// Copyright 2013 Cloudera Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef IMPALA_UTIL_BLOCKING_QUEUE_H
+#define IMPALA_UTIL_BLOCKING_QUEUE_H
+
+#include <boost/thread/condition_variable.hpp>
+#include <boost/thread/mutex.hpp>
+#include <list>
+#include <unistd.h>
+
+#include "util/stopwatch.h"
+
+namespace impala {
+
+// Fixed capacity FIFO queue, where both BlockingGet and BlockingPut operations block
+// if the queue is empty or full, respectively.
+template <typename T>
+class BlockingQueue {
+ public:
+  BlockingQueue(size_t max_elements)
+    : shutdown_(false),
+      max_elements_(max_elements),
+      total_get_wait_time_(0),
+      total_put_wait_time_(0) {
+  }
+
+  // Get an element from the queue, waiting indefinitely for one to become available.
+  // Returns false if we were shut down prior to getting the element, and there
+  // are no more elements available.
+  bool BlockingGet(T* out) {
+    MonotonicStopWatch timer;
+    boost::unique_lock<boost::mutex> unique_lock(lock_);
+
+    while (true) {
+      if (!list_.empty()) {
+        *out = list_.front();
+        list_.pop_front();
+        total_get_wait_time_ += timer.ElapsedTime();
+        unique_lock.unlock();
+        put_cv_.notify_one();
+        return true;
+      }
+      if (shutdown_) return false;
+      
+      timer.Start();
+      get_cv_.wait(unique_lock);
+      timer.Stop();
+    }
+  }
+
+  // Puts an element into the queue, waiting indefinitely until there is space.
+  // If the queue is shut down, returns false.
+  bool BlockingPut(const T& val) {
+    MonotonicStopWatch timer;
+    boost::unique_lock<boost::mutex> unique_lock(lock_);
+
+    while (list_.size() >= max_elements_ && !shutdown_) {
+      timer.Start();
+      put_cv_.wait(unique_lock);
+      timer.Stop();
+    }
+    total_put_wait_time_ += timer.ElapsedTime();
+
+    if (shutdown_) return false;
+
+    DCHECK_LT(list_.size(), max_elements_);
+    list_.push_back(val);
+    unique_lock.unlock();
+    get_cv_.notify_one();
+  }
+  
+  // Shut down the queue. Wakes up all threads waiting on BlockingGet or BlockingPut.
+  void Shutdown() {
+    {
+      boost::lock_guard<boost::mutex> guard(lock_);
+      shutdown_ = true;
+    }
+    
+    get_cv_.notify_all();
+    put_cv_.notify_all();
+  }
+
+  // Returns the total amount of time threads have blocked in BlockingGet.
+  uint64_t total_get_wait_time() const {
+    boost::lock_guard<boost::mutex> guard(lock_);
+    return total_get_wait_time_;
+  }
+
+  // Returns the total amount of time threads have blocked in BlockingPut.
+  uint64_t total_put_wait_time() const {
+    boost::lock_guard<boost::mutex> guard(lock_);
+    return total_put_wait_time_;
+  }
+
+ private:
+  bool shutdown_;
+  const int max_elements_;
+  boost::condition_variable get_cv_;   // 'get' callers wait on this
+  boost::condition_variable put_cv_;   // 'put' callers wait on this
+  // lock_ guards access to list_, total_get_wait_time, and total_put_wait_time
+  mutable boost::mutex lock_;
+  std::list<T> list_;
+  uint64_t total_get_wait_time_;
+  uint64_t total_put_wait_time_;
+};
+
+}
+
+#endif
