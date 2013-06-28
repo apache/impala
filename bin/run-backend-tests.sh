63fe068d70b4a434e2529118b7198ab606f0d0ff
Lenni Kuff <lskuff@cloudera.com>
2013-06-28 11:08:54 -0700

    Disable HEAPCHECK when running BE unit tests

--- bin/run-backend-tests.sh
+++ bin/run-backend-tests.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 # Copyright (c) 2012 Cloudera, Inc. All rights reserved.
 
-cd ${IMPALA_BE_DIR}
-. ${IMPALA_HOME}/bin/set-classpath.sh  
-make test
+# The backend unit tests currently do not work when HEAPCHECK is enabled.
+export HEAPCHECK=
+set -e
+set -u
 
+cd ${IMPALA_BE_DIR}
+. ${IMPALA_HOME}/bin/set-classpath.sh
+make test
