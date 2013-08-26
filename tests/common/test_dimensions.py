36b0ce6a7d78eaca8ab7ef0b500907b3d1827d67
Lenni Kuff <lskuff@cloudera.com>
2013-08-26 12:40:57 -0700

    Don't run with NUM_NODES=1 due to IMPALA-561
    Change-Id: I3f0d46725fbde57f845c1d2341857ac581db6bf1
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/330
    Reviewed-by: Alan Choi <alan@cloudera.com>
    Tested-by: jenkins
    Reviewed-by: Lenni Kuff <lskuff@cloudera.com>
    Tested-by: Lenni Kuff <lskuff@cloudera.com>

--- tests/common/test_dimensions.py
+++ tests/common/test_dimensions.py
@@ -85,7 +85,11 @@ class TableFormatInfo(object):
 
 # Common sets of values for the exec option vectors
 ALL_BATCH_SIZES = [0]
-ALL_CLUSTER_SIZES = [0, 1]
+
+# Don't run with NUM_NODES=1 due to IMPALA-561
+# ALL_CLUSTER_SIZES = [0, 1]
+ALL_CLUSTER_SIZES = [0]
+
 SINGLE_NODE_ONLY = [1]
 ALL_NODES_ONLY = [0]
 ALL_DISABLE_CODEGEN_OPTIONS = [True, False]
