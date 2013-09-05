9f542429415ce55686aeccb52394a29919841261
Lenni Kuff <lskuff@cloudera.com>
2013-09-05 13:15:13 -0700

    Add retry loop around split-hbase to fix build breaks
    Change-Id: I539407ce05d705b6b4e88d0791fc4ec236c79c80
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/399
    Reviewed-by: Lenni Kuff <lskuff@cloudera.com>
    Tested-by: Lenni Kuff <lskuff@cloudera.com>

--- testdata/bin/split-hbase.sh
+++ testdata/bin/split-hbase.sh
@@ -2,7 +2,7 @@
 # Copyright (c) 2012 Cloudera, Inc. All rights reserved.
 
 # Split hbasealltypesagg and hbasealltypessmall and assign their splits
-set -e
+set -u
 
 cd $IMPALA_HOME/testdata
 mvn clean package
@@ -10,4 +10,20 @@ mvn clean package
 . ${IMPALA_HOME}/bin/set-classpath.sh
 export CLASSPATH=$IMPALA_HOME/testdata/target/impala-testdata-0.1-SNAPSHOT.jar:$CLASSPATH
 
-java com.cloudera.impala.datagenerator.HBaseTestDataRegionAssigment functional_hbase.alltypesagg functional_hbase.alltypessmall
+RESULT=1
+RETRY_COUNT=0
+while [ $RESULT -ne 0 ] && [ $RETRY_COUNT -le 10 ]; do
+  java com.cloudera.impala.datagenerator.HBaseTestDataRegionAssigment \
+      functional_hbase.alltypesagg functional_hbase.alltypessmall
+  RESULT=$?
+
+  if [ $RESULT -ne 0 ]; then
+    ((RETRY_COUNT++))
+    # If the split failed, force reload the hbase tables before trying the next split
+    $IMPALA_HOME/bin/start-impala-cluster.py
+    $IMPALA_HOME/bin/load-data.py -w functional-query \
+        --table_names=alltypesagg,alltypessmall --table_formats=hbase/none --force
+  fi
+done
+
+exit $RESULT
