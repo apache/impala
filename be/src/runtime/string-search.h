5d90380bfdd002c794d7515684a3003aee9cf219
Nong Li <nong@cloudera.com>
2014-03-18 14:16:32 -0700

    Fix asan failure due to bug in string search.
    Change-Id: I9759109ec2c6c262f5c17393b84dde8dddf851ff
    (cherry picked from commit d31cb916637d79c7214d398f3e8d4a8599e74932)
    Reviewed-on: http://gerrit.ent.cloudera.com:8080/1972
    Reviewed-by: Nong Li <nong@cloudera.com>
    Tested-by: Nong Li <nong@cloudera.com>

--- be/src/runtime/string-search.h
+++ be/src/runtime/string-search.h
@@ -34,12 +34,12 @@ namespace impala {
 //
 // PYTHON SOFTWARE FOUNDATION LICENSE VERSION 2
 // --------------------------------------------
-// 
+//
 // 1. This LICENSE AGREEMENT is between the Python Software Foundation
 // ("PSF"), and the Individual or Organization ("Licensee") accessing and
 // otherwise using this software ("Python") in source or binary form and
 // its associated documentation.
-// 
+//
 // 2. Subject to the terms and conditions of this License Agreement, PSF
 // hereby grants Licensee a nonexclusive, royalty-free, world-wide
 // license to reproduce, analyze, test, perform and/or display publicly,
@@ -49,34 +49,34 @@ namespace impala {
 // 2001, 2002, 2003, 2004, 2005, 2006 Python Software Foundation; All Rights
 // Reserved" are retained in Python alone or in any derivative version
 // prepared by Licensee.
-// 
+//
 // 3. In the event Licensee prepares a derivative work that is based on
 // or incorporates Python or any part thereof, and wants to make
 // the derivative work available to others as provided herein, then
 // Licensee hereby agrees to include in any such work a brief summary of
 // the changes made to Python.
-// 
+//
 // 4. PSF is making Python available to Licensee on an "AS IS"
 // basis. PSF MAKES NO REPRESENTATIONS OR WARRANTIES, EXPRESS OR
 // IMPLIED. BY WAY OF EXAMPLE, BUT NOT LIMITATION, PSF MAKES NO AND
 // DISCLAIMS ANY REPRESENTATION OR WARRANTY OF MERCHANTABILITY OR FITNESS
 // FOR ANY PARTICULAR PURPOSE OR THAT THE USE OF PYTHON WILL NOT
 // INFRINGE ANY THIRD PARTY RIGHTS.
-// 
+//
 // 5. PSF SHALL NOT BE LIABLE TO LICENSEE OR ANY OTHER USERS OF PYTHON
 // FOR ANY INCIDENTAL, SPECIAL, OR CONSEQUENTIAL DAMAGES OR LOSS AS
 // A RESULT OF MODIFYING, DISTRIBUTING, OR OTHERWISE USING PYTHON,
 // OR ANY DERIVATIVE THEREOF, EVEN IF ADVISED OF THE POSSIBILITY THEREOF.
-// 
+//
 // 6. This License Agreement will automatically terminate upon a material
 // breach of its terms and conditions.
-// 
+//
 // 7. Nothing in this License Agreement shall be deemed to create any
 // relationship of agency, partnership, or joint venture between PSF and
 // Licensee. This License Agreement does not grant permission to use PSF
 // trademarks or trade name in a trademark sense to endorse or promote
 // products or services of Licensee, or any third party.
-// 
+//
 // 8. By copying, installing or otherwise using Python, Licensee
 // agrees to be bound by the terms and conditions of this License
 // Agreement.
@@ -103,8 +103,8 @@ class StringSearch {
     }
     BloomAdd(pattern_->ptr[mlast]);
   }
-  
-  // Search for this pattern in str.  
+
+  // Search for this pattern in str.
   //   Returns the offset into str if the pattern exists
   //   Returns -1 if the pattern is not found
   int Search(const StringValue* str) const {
@@ -112,7 +112,7 @@ class StringSearch {
     if (!str || !pattern_ || pattern_->len == 0) {
       return -1;
     }
-    
+
     int mlast = pattern_->len - 1;
     int w = str->len - pattern_->len;
     int n = str->len;
@@ -129,23 +129,26 @@ class StringSearch {
 
     // General case.
     int j;
+    // TODO: the original code seems to have an off by one error. It is possible
+    // to index at w + m which is the length of the input string. Checks have
+    // been added to make sure that w + m < str->len.
     for (int i = 0; i <= w; i++) {
-      // note: using mlast in the skip path slows things down on x86 
+      // note: using mlast in the skip path slows things down on x86
       if (s[i+m-1] == p[m-1]) {
-        // candidate match 
+        // candidate match
         for (j = 0; j < mlast; j++)
           if (s[i+j] != p[j]) break;
         if (j == mlast) {
           return i;
         }
-        // miss: check if next character is part of pattern 
-        if (!BloomQuery(s[i+m]))
+        // miss: check if next character is part of pattern
+        if (i + m < n && !BloomQuery(s[i+m]))
           i = i + m;
         else
           i = i + skip_;
       } else {
-        // skip: check if next character is part of pattern 
-        if (!BloomQuery(s[i+m])) {
+        // skip: check if next character is part of pattern
+        if (i + m < n && !BloomQuery(s[i+m])) {
           i = i + m;
         }
       }
@@ -158,12 +161,12 @@ class StringSearch {
 
   void BloomAdd(char c) {
     mask_ |= (1UL << (c & (BLOOM_WIDTH - 1)));
-  } 
+  }
 
   bool BloomQuery(char c) const {
     return mask_ & (1UL << (c & (BLOOM_WIDTH - 1)));
   }
-  
+
   const StringValue* pattern_;
   int64_t mask_;
   int64_t skip_;
