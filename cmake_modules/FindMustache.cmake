4b8ca060b8465a51441138aebd347b8c852818be
Henry Robinson <henry@cloudera.com>
2014-04-09 21:44:34 -0700

    Add Mustache template library
    The Mustache template library renders
    Mustache (httP;//mustache.github.io) templates with Json contexts
    produced by RapidJson.
    
    Change-Id: I38fa33aea88bea7d19a2ee8cb548a50e3ca2f4a8
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/2196
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: jenkins
    (cherry picked from commit f7a30054a180867bcb492444bbcb4fa2849c7b93)
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/3648
    Reviewed-by: Henry Robinson <henry@cloudera.com>

---
+++ cmake_modules/FindMustache.cmake
@@ -0,0 +1,26 @@
+# - Find Mustache (mustache.h and mustache.c)
+# This module defines
+#  MUSTACHE_INCLUDE_DIR, directory containing headers
+#  MUSTACHE_SRC_DIR, directory containing source
+#  MUSTACHE_FOUND, whether the Mustache library has been located
+
+find_path(MUSTACHE_INCLUDE_DIR mustache/mustache.h HINTS ${CMAKE_SOURCE_DIR}/thirdparty)
+find_path(MUSTACHE_SRC_DIR mustache.cc HINTS ${CMAKE_SOURCE_DIR}/thirdparty/mustache)
+
+if (MUSTACHE_INCLUDE_DIR)
+  set(MUSTACHE_FOUND TRUE)
+else ()
+  set(MUSTACHE_FOUND FALSE)
+endif ()
+
+if (MUSTACHE_FOUND)
+  if (NOT MUSTACHE_FIND_QUIETLY)
+    message(STATUS "Mustache template library found in ${MUSTACHE_INCLUDE_DIR}")
+  endif ()
+else ()
+  message(STATUS "Mustache template library includes NOT found. ")
+endif ()
+
+mark_as_advanced(
+  MUSTACHE_INCLUDE_DIR
+)
