39a8b9bc7a43ce29c70c846bd8dff28f63185c0d
Lenni Kuff <lskuff@cloudera.com>
2014-08-14 15:46:40 -0700

    Fix copy-test-data script to download raw data files using wget rather than scp
    Change-Id: I86ce0aec8cbd681a9ec4f56eff8841cadd513ef9
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/3861
    Reviewed-by: Lenni Kuff <lskuff@cloudera.com>
    Tested-by: jenkins
    (cherry picked from commit 0e7c91194bad8626a11c353e80130ffef31d79a0)
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/4119

--- bin/copy-test-data.sh
+++ bin/copy-test-data.sh
@@ -1,5 +1,21 @@
 #!/usr/bin/env bash
-# Copyright (c) 2012 Cloudera, Inc. All rights reserved.
+# Copyright 2014 Cloudera Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Downloads and extracts the raw data files for test datasets that are too large for the
+# main Impala repo.
+# TODO: Migrate this script to the aux test repo.
 
 set -e
 set -u
@@ -9,9 +25,20 @@ echo "Copying data files from the share.  If the file already exists locally, th
      "you need to force a copy, delete the local directory:"\
      "IMPALA_HOME/testdata/impala-data"
 
-# TODO: Waiting on helpdesk ticket (HD-2861) to move this to a better location
-DATASRC="a2226.halxg.cloudera.com:/data/1/workspace/impala-data"
-DATADST=$IMPALA_HOME/testdata/impala-data
-mkdir -p $DATADST
+DATASRC="http://util-1.ent.cloudera.com/impala-test-data/"
+DATADST=${IMPALA_HOME}/testdata/impala-data
 
-scp -i $HOME/.ssh/id_rsa_jenkins -o "StrictHostKeyChecking=no" -r systest@$DATASRC/* $DATADST
+mkdir -p ${DATADST}
+pushd ${DATADST}
+
+# Download all .tar.gz files from the source, excluding the hostname and directory name.
+# If the file already exists locally, skip the download.
+wget -q --cut-dirs=1 --no-clobber -r --no-parent -nH --accept="*.tar.gz" ${DATASRC}
+for filename in *.tar.gz
+do
+  echo "Extracting: ${filename}"
+  tar -xzf ${filename}
+done
+popd
+
+echo "Test data download successful."
