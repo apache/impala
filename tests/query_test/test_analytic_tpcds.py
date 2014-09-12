9e67b4a40153608c9d5e921c04f3da5405312b82
Taras Bobrovytsky <tbobrovytsky@cloudera.com>
2014-09-12 13:39:12 -0700

    Analytic functions tests
    Added several tests for analytical functions
    
    Tests for the following have not been added because it's not implemented yet:
    - Lag, Lead functions
    - Window clauses
    
    Change-Id: I34546c967a6d29c97327f4cba405006a50867dcb
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/4307
    Reviewed-by: Taras Bobrovytsky <tbobrovytsky@cloudera.com>
    Tested-by: jenkins

---
+++ tests/query_test/test_analytic_tpcds.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+# Copyright (c) 2014 Cloudera, Inc. All rights reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# Targeted tests to validate analytic functions use TPCDS dataset.
+
+import logging
+import pytest
+from tests.common.impala_test_suite import ImpalaTestSuite
+from tests.common.test_dimensions import create_parquet_dimension, TestDimension
+
+class TestAnalyticTpcds(ImpalaTestSuite):
+  """Test class to do functional validation of analytic functions.
+
+  Batch size parameter is varied if the exploration strategy is exhaustive.
+  """
+  @classmethod
+  def get_workload(self):
+    return 'functional-query'
+
+  @classmethod
+  def add_test_dimensions(cls):
+    super(TestAnalyticTpcds, cls).add_test_dimensions()
+    cls.TestMatrix.add_dimension(create_parquet_dimension('tpcds'))
+
+    if cls.exploration_strategy() == 'exhaustive':
+      batch_size = [1, 3, 10, 100, 0]
+    else:
+      batch_size = [0]
+
+    cls.TestMatrix.add_dimension(TestDimension('batch_size', *batch_size))
+
+  def test_analytic_functions_tpcds(self, vector):
+    vector.get_value('exec_option')['batch_size'] = vector.get_value('batch_size')
+    self.run_test_case('QueryTest/analytic-fns-tpcds', vector)
