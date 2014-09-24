39fef934252631ce8c479b6c6c04729fa340860d
Alex Behm <alex.behm@cloudera.com>
2014-09-23 17:30:06 -0700

    IMPALA-1278: Basic cardinality estimation for semi joins.
    Change-Id: I353c9c581f7a54e0b42bdd1e89cf99b93d6e18de
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/4634
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: jenkins

--- fe/src/main/java/com/cloudera/impala/planner/HashJoinNode.java
+++ fe/src/main/java/com/cloudera/impala/planner/HashJoinNode.java
@@ -32,7 +32,6 @@ import com.cloudera.impala.catalog.Table;
 import com.cloudera.impala.catalog.Type;
 import com.cloudera.impala.common.AnalysisException;
 import com.cloudera.impala.common.InternalException;
-import com.cloudera.impala.common.Pair;
 import com.cloudera.impala.thrift.TEqJoinCondition;
 import com.cloudera.impala.thrift.TExplainLevel;
 import com.cloudera.impala.thrift.THashJoinNode;
@@ -178,32 +177,31 @@ public class HashJoinNode extends PlanNode {
     eqJoinConjuncts_ = newEqJoinConjuncts;
   }
 
-  @Override
-  public void computeStats(Analyzer analyzer) {
-    super.computeStats(analyzer);
-
-    // For a join between child(0) and child(1), we look for join conditions "L.c = R.d"
-    // (with L being from child(0) and R from child(1)) and use as the cardinality
-    // estimate the maximum of
-    //   child(0).cardinality * R.cardinality / # distinct values for R.d
-    //     * child(1).cardinality / R.cardinality
-    // across all suitable join conditions, which simplifies to
-    //   child(0).cardinality * child(1).cardinality / # distinct values for R.d
-    // The reasoning is that
-    // - each row in child(0) joins with R.cardinality/#DV_R.d rows in R
-    // - each row in R is 'present' in child(1).cardinality / R.cardinality rows in
-    //   child(1)
-    //
-    // This handles the very frequent case of a fact table/dimension table join
-    // (aka foreign key/primary key join) if the primary key is a single column, with
-    // possible additional predicates against the dimension table. An example:
-    // FROM FactTbl F JOIN Customers C D ON (F.cust_id = C.id) ... WHERE C.region = 'US'
-    // - if there are 5 regions, the selectivity of "C.region = 'US'" would be 0.2
-    //   and the output cardinality of the Customers scan would be 0.2 * # rows in
-    //   Customers
-    // - # rows in Customers == # of distinct values for Customers.id
-    // - the output cardinality of the join would be F.cardinality * 0.2
-
+  /**
+   * Returns the estimated cardinality of an inner or outer join.
+   * For a join between child(0) and child(1), we look for join conditions "L.c = R.d"
+   * (with L being from child(0) and R from child(1)) and use as the cardinality
+   * estimate the maximum of
+   *   |child(0)| * |R| / NDV(R.d) * |child(1)| / |R|
+   * across all suitable join conditions, which simplifies to
+   *   |child(0)| * |child(1)| / NDV(R.d)
+   * The reasoning is that
+   * - each row in child(0) joins with |R| / NDV(R.d) rows in R
+   * - each row in R is 'present' in |child(1)| / |R| rows in child(1)
+   *
+   * This handles the very frequent case of a fact table/dimension table join
+   * (aka foreign key/primary key join) if the primary key is a single column, with
+   * possible additional predicates against the dimension table. An example:
+   * FROM FactTbl F JOIN Customers C D ON (F.cust_id = C.id) ... WHERE C.region = 'US'
+   * - if there are 5 regions, the selectivity of "C.region = 'US'" would be 0.2
+   *   and the output cardinality of the Customers scan would be 0.2 * # rows in
+   *   Customers
+   * - # rows in Customers == # of distinct values for Customers.id
+   * - the output cardinality of the join would be F.cardinality * 0.2
+   */
+  private long getJoinCardinality() {
+    Preconditions.checkState(
+        joinOp_ == JoinOperator.INNER_JOIN || joinOp_.isOuterJoin());
     long maxNumDistinct = 0;
     for (Expr eqJoinPredicate: eqJoinConjuncts_) {
       if (eqJoinPredicate.getChild(0).unwrapSlotRef(false) == null) continue;
@@ -232,15 +230,117 @@ public class HashJoinNode extends PlanNode {
           + Long.toString(numDistinct));
     }
 
+    long result = -1;
     if (maxNumDistinct == 0) {
       // if we didn't find any suitable join predicates or don't have stats
       // on the relevant columns, we very optimistically assume we're doing an
       // FK/PK join (which doesn't alter the cardinality of the left-hand side)
-      cardinality_ = getChild(0).cardinality_;
+      result = getChild(0).cardinality_;
     } else if (getChild(0).cardinality_ != -1 && getChild(1).cardinality_ != -1) {
-      cardinality_ = multiplyCardinalities(getChild(0).cardinality_,
+      result = multiplyCardinalities(getChild(0).cardinality_,
           getChild(1).cardinality_);
-      cardinality_ = Math.round((double)cardinality_ / (double) maxNumDistinct);
+      result = Math.round((double)result / (double) maxNumDistinct);
+    }
+    return result;
+  }
+
+  /**
+   * Returns the estimated cardinality of a semi join node.
+   * For a left semi join between child(0) and child(1), we look for equality join
+   * conditions "L.c = R.d" (with L being from child(0) and R from child(1)) and use as
+   * the cardinality estimate the minimum of
+   *   |child(0)| * Min(NDV(L.c), NDV(R.d)) / NDV(L.c)
+   * over all suitable join conditions. The reasoning is that:
+   * - each row in child(0) is returned at most once
+   * - the probability of a row in child(0) having a match in R is
+   *   Min(NDV(L.c), NDV(R.d)) / NDV(L.c)
+   *
+   * For a left anti join we estimate the cardinality as the minimum of:
+   *   |L| * Max(NDV(L.c) - NDV(R.d), NDV(L.c)) / NDV(L.c)
+   * over all suitable join conditions. The reasoning is that:
+   * - each row in child(0) is returned at most once
+   * - if NDV(L.c) > NDV(R.d) then the probability of row in L having a match
+   *   in child(1) is (NDV(L.c) - NDV(R.d)) / NDV(L.c)
+   * - otherwise, we conservatively use |L| to avoid underestimation
+   *
+   * We analogously estimate the cardinality for right semi/anti joins, and treat the
+   * null-aware anti join like a regular anti join
+   *
+   * TODO: In order to take into account additional conjuncts in the child child subtrees
+   * adjust NDV(L.c) by |child(0)| / |L| and the NDV(R.d) by |child(1)| / |R|.
+   * The adjustment is currently too dangerous due to the other planner bugs compounding
+   * to bad plans causing perf regressions (IMPALA-976).
+   */
+  private long getSemiJoinCardinality() {
+    Preconditions.checkState(joinOp_.isSemiJoin());
+
+    // Return -1 if the cardinality of the returned side is unknown.
+    long cardinality;
+    if (joinOp_ == JoinOperator.RIGHT_SEMI_JOIN
+        || joinOp_ == JoinOperator.RIGHT_ANTI_JOIN) {
+      if (getChild(1).cardinality_ == -1) return -1;
+      cardinality = getChild(1).cardinality_;
+    } else {
+      if (getChild(0).cardinality_ == -1) return -1;
+      cardinality = getChild(0).cardinality_;
+    }
+    double minSelectivity = 1.0;
+    for (Expr eqJoinPredicate: eqJoinConjuncts_) {
+      long lhsNdv = getNdv(eqJoinPredicate.getChild(0));
+      lhsNdv = Math.min(lhsNdv, getChild(0).cardinality_);
+      long rhsNdv = getNdv(eqJoinPredicate.getChild(1));
+      rhsNdv = Math.min(rhsNdv, getChild(1).cardinality_);
+
+      double selectivity = 1.0;
+      switch (joinOp_) {
+        case LEFT_SEMI_JOIN: {
+          selectivity = (double) Math.min(lhsNdv, rhsNdv) / (double) (lhsNdv);
+          break;
+        }
+        case RIGHT_SEMI_JOIN: {
+          selectivity = (double) Math.min(lhsNdv, rhsNdv) / (double) (rhsNdv);
+          break;
+        }
+        case LEFT_ANTI_JOIN:
+        case NULL_AWARE_LEFT_ANTI_JOIN: {
+          selectivity = (double) Math.max(lhsNdv - rhsNdv, lhsNdv) / (double) lhsNdv;
+          break;
+        }
+        case RIGHT_ANTI_JOIN: {
+          selectivity = (double) Math.max(rhsNdv - lhsNdv, rhsNdv) / (double) rhsNdv;
+          break;
+        }
+        default: Preconditions.checkState(false);
+      }
+      minSelectivity = Math.min(minSelectivity, selectivity);
+    }
+
+    Preconditions.checkState(cardinality != -1);
+    return Math.round(cardinality * minSelectivity);
+  }
+
+  /**
+   * Unwraps the SlotRef in expr and returns the NDVs of it.
+   * Returns -1 if the NDVs are unknown or if expr is not a SlotRef.
+   */
+  private long getNdv(Expr expr) {
+    SlotRef slotRef = expr.unwrapSlotRef(false);
+    if (slotRef == null) return -1;
+    SlotDescriptor slotDesc = slotRef.getDesc();
+    if (slotDesc == null) return -1;
+    ColumnStats stats = slotDesc.getStats();
+    if (!stats.hasNumDistinctValues()) return -1;
+    return stats.getNumDistinctValues();
+  }
+
+  @Override
+  public void computeStats(Analyzer analyzer) {
+    super.computeStats(analyzer);
+    if (joinOp_ == JoinOperator.INNER_JOIN || joinOp_.isOuterJoin()) {
+      cardinality_ = getJoinCardinality();
+    } else {
+      Preconditions.checkState(joinOp_.isSemiJoin());
+      cardinality_ = getSemiJoinCardinality();
     }
 
     // Impose lower/upper bounds on the cardinality based on the join type.
@@ -278,22 +378,16 @@ public class HashJoinNode extends PlanNode {
         }
         break;
       }
-      // Cap cardinality at 1 because a value of 0 triggers pathological edge cases.
-      case LEFT_ANTI_JOIN: {
+      case LEFT_ANTI_JOIN:
+      case NULL_AWARE_LEFT_ANTI_JOIN: {
         if (leftCard != -1) {
-          cardinality_ = leftCard;
-          if (rightCard != -1) {
-            cardinality_ = Math.max(1, leftCard - rightCard);
-          }
+          cardinality_ = Math.min(leftCard, cardinality_);
         }
         break;
       }
       case RIGHT_ANTI_JOIN: {
         if (rightCard != -1) {
-          cardinality_ = rightCard;
-          if (leftCard != -1) {
-            cardinality_ = Math.max(1, rightCard - leftCard);
-          }
+          cardinality_ = Math.min(rightCard, cardinality_);
         }
         break;
       }
