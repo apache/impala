461a5dc15d7d9c2543f2feba63329ba9e000af13
casey <casey@cloudera.com>
2015-01-14 16:36:50 -0500

    TimestampValue refactor and cleanup (part 1)
    This just moves code around to make the review of part 2 easier to
    follow.
    
    There should be no functional change at all.
    
    Change-Id: I0ddbda0bb5688383d78ffbc3083c14e02d8402fc
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/5767
    Reviewed-by: Casey Ching <casey@cloudera.com>
    Tested-by: jenkins

--- be/src/runtime/timestamp-value.h
+++ be/src/runtime/timestamp-value.h
@@ -28,30 +28,78 @@ namespace impala {
 
 time_t to_time_t(boost::posix_time::ptime t);
 
-
 // The format of a timestamp-typed slot.
 class TimestampValue {
  public:
-  TimestampValue(const boost::gregorian::date& d,
-                 const boost::posix_time::time_duration& t)
-      : time_of_day_(t),
-        date_(d) {
-  }
-  TimestampValue() { }
+  TimestampValue() {}
 
+  TimestampValue(const boost::gregorian::date& d,
+      const boost::posix_time::time_duration& t)
+      : time_of_day_(t),
+        date_(d) {}
   TimestampValue(const boost::posix_time::ptime& t)
       : time_of_day_(t.time_of_day()),
-        date_(t.date()) {
-  }
-
+        date_(t.date()) {}
   TimestampValue(const TimestampValue& tv)
       : time_of_day_(tv.time_of_day_),
-        date_(tv.date_) {
+        date_(tv.date_) {}
+  TimestampValue(const char* str, int len);
+  TimestampValue(const char* str, int len, const DateTimeFormatContext& dt_ctx);
+
+  TimestampValue(int64_t t, int64_t n) {
+    boost::posix_time::ptime temp = boost::posix_time::from_time_t(t);
+    temp += boost::posix_time::nanoseconds(n);
+    *this = temp;
   }
 
-  TimestampValue& operator=(const boost::posix_time::ptime& t) {
-    *this =  TimestampValue(t);
-    return *this;
+  TimestampValue(double t) {
+    int64_t i = t;
+    boost::posix_time::ptime temp = boost::posix_time::from_time_t(i);
+    temp += boost::posix_time::nanoseconds((t-i)/FRACTIONAL);
+    *this = temp;
+  }
+
+  TimestampValue(int64_t t) {
+    *this = TimestampValue(boost::posix_time::from_time_t(t));
+  }
+
+  TimestampValue(int32_t t) {
+    *this = TimestampValue(boost::posix_time::from_time_t(t));
+  }
+
+  TimestampValue(int16_t t) {
+    *this = TimestampValue(boost::posix_time::from_time_t(t));
+  }
+
+  TimestampValue(int8_t t) {
+    *this = TimestampValue(boost::posix_time::from_time_t(t));
+  }
+
+  TimestampValue(bool t) {
+    *this = TimestampValue(boost::posix_time::from_time_t(t));
+  }
+
+  // Returns the local time with second accuracy
+  static TimestampValue local_time() {
+    return TimestampValue(boost::posix_time::second_clock::local_time());
+  }
+
+  // Returns the local time with microsecond accuracy
+  static TimestampValue local_time_micros() {
+    return TimestampValue(boost::posix_time::microsec_clock::local_time());
+  }
+
+  static TimestampValue FromTimestampVal(const impala_udf::TimestampVal& tv) {
+    TimestampValue v;
+    memcpy(&v.date_, &tv.date, sizeof(v.date_));
+    memcpy(&v.time_of_day_, &tv.time_of_day, sizeof(v.time_of_day_));
+    return v;
+  }
+
+  void ToTimestampVal(impala_udf::TimestampVal* tv) const {
+    memcpy(&tv->date, &date_, sizeof(date_));
+    memcpy(&tv->time_of_day, &time_of_day_, sizeof(time_of_day_));
+    tv->is_null = false;
   }
 
   void ToPtime(boost::posix_time::ptime* ptp) const {
@@ -59,43 +107,14 @@ class TimestampValue {
     *ptp = temp;
   }
 
-  TimestampValue(int64_t t, int64_t n) {
-    boost::posix_time::ptime temp = boost::posix_time::from_time_t(t);
-    temp += boost::posix_time::nanoseconds(n);
-    *this = temp;
-  }
-  TimestampValue(double t) {
-    int64_t i = t;
-    boost::posix_time::ptime temp = boost::posix_time::from_time_t(i);
-    temp += boost::posix_time::nanoseconds((t-i)/FRACTIONAL);
-    *this = temp;
-  }
-  TimestampValue(const char* str, int len);
-  TimestampValue(const char* str, int len, const DateTimeFormatContext& dt_ctx);
-  TimestampValue(int64_t t) {
-    *this = TimestampValue(boost::posix_time::from_time_t(t));
-  }
-  TimestampValue(int32_t t) {
-    *this = TimestampValue(boost::posix_time::from_time_t(t));
-  }
-  TimestampValue(int16_t t) {
-    *this = TimestampValue(boost::posix_time::from_time_t(t));
-  }
-  TimestampValue(int8_t t) {
-    *this = TimestampValue(boost::posix_time::from_time_t(t));
-  }
-  TimestampValue(bool t) {
-    *this = TimestampValue(boost::posix_time::from_time_t(t));
-  }
-
-  bool valid() const { return valid_date() || valid_time(); }
   bool valid_date() const { return ! date_.is_special(); }
   bool valid_time() const { return ! time_of_day_.is_special(); }
+  bool valid() const { return valid_date() || valid_time(); }
 
-  void set_date(boost::gregorian::date d) { date_ = d; }
-  void set_time(boost::posix_time::time_duration t) { time_of_day_ = t; }
-  const boost::gregorian::date& get_date() const { return date_; }
-  const boost::posix_time::time_duration& get_time() const { return time_of_day_; }
+  // If the date or time of day are valid then this is valid.
+  bool NotADateTime() const {
+    return this->date_.is_special() && this->time_of_day_.is_special();
+  }
 
   std::string DebugString() const {
     std::stringstream ss;
@@ -109,107 +128,6 @@ class TimestampValue {
     return ss.str();
   }
 
-  bool operator==(const TimestampValue& other) const {
-    return this->date_  == other.date_ && this->time_of_day_ == other.time_of_day_;
-  }
-  bool operator!=(const TimestampValue& other) const {
-    return !(*this == other);
-  }
-  bool operator<=(const TimestampValue& other) const {
-    return this->date_ < other.date_ || (this->date_ == other.date_ &&
-        (this->time_of_day_ <= other.time_of_day_));
-  }
-  bool operator>=(const TimestampValue& other) const {
-    return this->date_ > other.date_ ||
-        (this->date_ == other.date_ && this->time_of_day_ >= other.time_of_day_);
-  }
-  bool operator<(const TimestampValue& other) const {
-    return this->date_ < other.date_ ||
-        (this->date_ == other.date_ && this->time_of_day_ < other.time_of_day_);
-  }
-  bool operator>(const TimestampValue& other) const {
-    return this->date_ > other.date_ ||
-        (this->date_ == other.date_ && this->time_of_day_ > other.time_of_day_);
-  }
-
-  // If the date or time of day are valid then this is valid.
-  bool NotADateTime() const {
-    return this->date_.is_special() && this->time_of_day_.is_special();
-  }
-
-  operator bool() const {
-    boost::posix_time::ptime temp;
-    this->ToPtime(&temp);
-    return static_cast<bool>(to_time_t(temp));
-  }
-  operator int8_t() const {
-    boost::posix_time::ptime temp;
-    this->ToPtime(&temp);
-    return static_cast<char>(to_time_t(temp));
-  }
-  operator int16_t() const {
-    boost::posix_time::ptime temp;
-    this->ToPtime(&temp);
-    return static_cast<int16_t>(to_time_t(temp));
-  }
-  operator int32_t() const {
-    boost::posix_time::ptime temp;
-    this->ToPtime(&temp);
-    return static_cast<int32_t>(to_time_t(temp));
-  }
-  operator int64_t() const {
-    boost::posix_time::ptime temp;
-    this->ToPtime(&temp);
-    return static_cast<int64_t>(to_time_t(temp));
-  }
-  operator float() const {
-    boost::posix_time::ptime temp;
-    this->ToPtime(&temp);
-    return static_cast<float>(to_time_t(temp)) +
-        static_cast<float>(time_of_day_.fractional_seconds() * FRACTIONAL);
-  }
-  operator double() const {
-    boost::posix_time::ptime temp;
-    this->ToPtime(&temp);
-    return static_cast<double>(to_time_t(temp)) +
-        static_cast<double>(time_of_day_.fractional_seconds() * FRACTIONAL);
-  }
-  static size_t Size() {
-    return sizeof(boost::posix_time::time_duration) + sizeof(boost::gregorian::date);
-  }
-
-  boost::posix_time::time_duration time_of_day() { return time_of_day_; }
-  boost::gregorian::date date() { return date_;}
-
-  // Returns the local time
-  static TimestampValue local_time() {
-    return TimestampValue(boost::posix_time::second_clock::local_time());
-  }
-
-  // Returns the local time with microsecond accuracy
-  static TimestampValue local_time_micros() {
-    return TimestampValue(boost::posix_time::microsec_clock::local_time());
-  }
-
-  inline uint32_t Hash(int seed = 0) const {
-    uint32_t hash = HashUtil::Hash(
-        &time_of_day_, sizeof(boost::posix_time::time_duration), seed);
-    return HashUtil::Hash(&time_of_day_, sizeof(boost::gregorian::date), hash);
-  }
-
-  void ToTimestampVal(impala_udf::TimestampVal* tv) const {
-    memcpy(&tv->date, &date_, sizeof(date_));
-    memcpy(&tv->time_of_day, &time_of_day_, sizeof(time_of_day_));
-    tv->is_null = false;
-  }
-
-  static TimestampValue FromTimestampVal(const impala_udf::TimestampVal& tv) {
-    TimestampValue v;
-    memcpy(&v.date_, &tv.date, sizeof(v.date_));
-    memcpy(&v.time_of_day_, &tv.time_of_day, sizeof(v.time_of_day_));
-    return v;
-  }
-
   // Formats the timestamp using the given date/time context and places the result in the
   // string buffer. The size of the buffer should be at least dt_ctx.fmt_out_len + 1. A
   // string terminator will be appended to the string.
@@ -218,6 +136,98 @@ class TimestampValue {
   // buff -- the buffer that will hold the result
   // Returns the number of characters copied in to the buffer (minus the terminator)
   int Format(const DateTimeFormatContext& dt_ctx, int len, char* buff);
+
+  void set_date(boost::gregorian::date d) { date_ = d; }
+  void set_time(boost::posix_time::time_duration t) { time_of_day_ = t; }
+  boost::gregorian::date date() { return date_;}
+  boost::posix_time::time_duration time_of_day() { return time_of_day_; }
+  const boost::gregorian::date& get_date() const { return date_; }
+  const boost::posix_time::time_duration& get_time() const { return time_of_day_; }
+
+  TimestampValue& operator=(const boost::posix_time::ptime& t) {
+    *this = TimestampValue(t);
+    return *this;
+  }
+
+  bool operator==(const TimestampValue& other) const {
+    return this->date_ == other.date_ && this->time_of_day_ == other.time_of_day_;
+  }
+
+  bool operator!=(const TimestampValue& other) const { return !(*this == other); }
+
+  bool operator<(const TimestampValue& other) const {
+    return this->date_ < other.date_ ||
+        (this->date_ == other.date_ && this->time_of_day_ < other.time_of_day_);
+  }
+
+  bool operator<=(const TimestampValue& other) const {
+    return this->date_ < other.date_ || (this->date_ == other.date_ &&
+        (this->time_of_day_ <= other.time_of_day_));
+  }
+
+  bool operator>(const TimestampValue& other) const {
+    return this->date_ > other.date_ ||
+        (this->date_ == other.date_ && this->time_of_day_ > other.time_of_day_);
+  }
+
+  bool operator>=(const TimestampValue& other) const {
+    return this->date_ > other.date_ ||
+        (this->date_ == other.date_ && this->time_of_day_ >= other.time_of_day_);
+  }
+
+  operator bool() const {
+    boost::posix_time::ptime temp;
+    this->ToPtime(&temp);
+    return static_cast<bool>(to_time_t(temp));
+  }
+
+  operator int8_t() const {
+    boost::posix_time::ptime temp;
+    this->ToPtime(&temp);
+    return static_cast<char>(to_time_t(temp));
+  }
+
+  operator int16_t() const {
+    boost::posix_time::ptime temp;
+    this->ToPtime(&temp);
+    return static_cast<int16_t>(to_time_t(temp));
+  }
+
+  operator int32_t() const {
+    boost::posix_time::ptime temp;
+    this->ToPtime(&temp);
+    return static_cast<int32_t>(to_time_t(temp));
+  }
+
+  operator int64_t() const {
+    boost::posix_time::ptime temp;
+    this->ToPtime(&temp);
+    return static_cast<int64_t>(to_time_t(temp));
+  }
+
+  operator float() const {
+    boost::posix_time::ptime temp;
+    this->ToPtime(&temp);
+    return static_cast<float>(to_time_t(temp)) +
+        static_cast<float>(time_of_day_.fractional_seconds() * FRACTIONAL);
+  }
+
+  operator double() const {
+    boost::posix_time::ptime temp;
+    this->ToPtime(&temp);
+    return static_cast<double>(to_time_t(temp)) +
+        static_cast<double>(time_of_day_.fractional_seconds() * FRACTIONAL);
+  }
+
+  static size_t Size() {
+    return sizeof(boost::posix_time::time_duration) + sizeof(boost::gregorian::date);
+  }
+
+  inline uint32_t Hash(int seed = 0) const {
+    uint32_t hash = HashUtil::Hash(
+        &time_of_day_, sizeof(boost::posix_time::time_duration), seed);
+    return HashUtil::Hash(&time_of_day_, sizeof(boost::gregorian::date), hash);
+  }
 
   static const char* LLVM_CLASS_NAME;
 
