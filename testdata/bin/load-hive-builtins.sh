b01252267a2c858cb497d41652702a23b9ed233a
ishaan <ishaan@cloudera.com>
2015-02-04 00:32:23 -0800

    Fix the hive environment breakage caused by a malformed environment variable.
    We build some of the jars that hive needs fe/target/. A recent change resulted in these
    jars not being loaded, causing a bad hive environment. This patch restores proper
    behaviour.
    
    Change-Id: Icb27ab04f7f77cb4ddab51326eedfd11a6cdf960
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/5930
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: jenkins

--- testdata/bin/load-hive-builtins.sh
+++ testdata/bin/load-hive-builtins.sh
@@ -25,12 +25,12 @@ ${HADOOP_HOME}/bin/hadoop fs -put ${HADOOP_HOME}/share/hadoop/mapreduce/*.jar \
     ${FILESYSTEM_PREFIX}${HADOOP_HOME}/share/hadoop/mapreduce/
 
 
-${HADOOP_HOME}/bin/hadoop fs -rm -r -f ${FILESYSTEM_PREFIX}${IMPALA_HOME_HOME}/fe/target/
-${HADOOP_HOME}/bin/hadoop fs -mkdir -p ${FILESYSTEM_PREFIX}${IMPALA_HOME_HOME}/fe/target/
+${HADOOP_HOME}/bin/hadoop fs -rm -r -f ${FILESYSTEM_PREFIX}${IMPALA_HOME}/fe/target/
+${HADOOP_HOME}/bin/hadoop fs -mkdir -p ${FILESYSTEM_PREFIX}${IMPALA_HOME}/fe/target/
 
 ${HADOOP_HOME}/bin/hadoop fs -rm -r -f ${FILESYSTEM_PREFIX}${HADOOP_LZO}/build
 ${HADOOP_HOME}/bin/hadoop fs -mkdir -p ${FILESYSTEM_PREFIX}${HADOOP_LZO}/build
 ${HADOOP_HOME}/bin/hadoop fs -put ${IMPALA_HOME}/fe/target/impala-frontend*.jar \
-    ${FILESYSTEM_PREFIX}${IMPALA_HOME_HOME}/fe/target/
+    ${FILESYSTEM_PREFIX}${IMPALA_HOME}/fe/target/
 ${HADOOP_HOME}/bin/hadoop fs -put ${HADOOP_LZO}/build/hadoop-lzo*.jar \
     ${FILESYSTEM_PREFIX}${HADOOP_LZO}/build/
