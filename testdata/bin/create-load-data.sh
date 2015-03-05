4a9adfd68543d72bef9d44343fb892a6e4c2aa02
ishaan <ishaan@cloudera.com>
2015-03-04 16:39:54 -0800

    Fix the full data load build by not hardcoding the lzo index file's name.
    After the hive/hdfs rebase, the indexed lzo file names changed. This patch uses a
    wildcard rather than a specific file name to protect against such changes. It's safe
    because the test simply expects a partition that does not have index files.
    
    Change-Id: I6d32609b62df83fe2a8ef935d7ca6506ecff5e0d
    Reviewed-on: http://gerrit.cloudera.org:8080/150
    Reviewed-by: Ishaan Joshi <ishaan@cloudera.com>
    Tested-by: Internal Jenkins

--- testdata/bin/create-load-data.sh
+++ testdata/bin/create-load-data.sh
@@ -260,8 +260,8 @@ function load-custom-data {
   hadoop fs -put -f ${IMPALA_HOME}/testdata/data/long_page_header.parquet \
                     /test-warehouse/bad_parquet_parquet
 
-  # Remove an index file so we test an un-indexed LZO file
-  hadoop fs -rm /test-warehouse/alltypes_text_lzo/year=2009/month=1/000000_0.lzo.index
+  # Remove all index files in this partition.
+  hadoop fs -rm /test-warehouse/alltypes_text_lzo/year=2009/month=1/*.lzo.index
 
   # Add a sequence file that only contains a header (see IMPALA-362)
   hadoop fs -put -f ${IMPALA_HOME}/testdata/tinytable_seq_snap/tinytable_seq_snap_header_only \
