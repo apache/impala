eca9df6458f08ebd8ec793789c3cbfd6658d6216
David Alves <david.alves@cloudera.com>
2015-06-08 18:42:31 -0700

    Pull KuduTestHelper into its own file
    The test helper is useful in other tests so this patch pulls it to
    its own file so that it can be reused.
    
    This also makes the helper always use the same schema so that
    we can avoid boilerplate schema creation code in the test classes.
    
    Change-Id: I27e37a0a9363990fd501d61242260a8806769a95
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/6875
    Tested-by: jenkins
    Reviewed-by: Martin Grund <mgrund@cloudera.com>

--- be/src/exec/kudu-scan-node-test.cc
+++ be/src/exec/kudu-scan-node-test.cc
@@ -12,10 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <gtest/gtest.h>
-#include <string>
-#include <tr1/memory>
-#include <vector>
+#include "exec/kudu-testutil.h"
 
 #include "common/init.h"
 #include "common/object-pool.h"
@@ -26,16 +23,7 @@
 #include "gen-cpp/PlanNodes_types.h"
 #include "gen-cpp/Types_types.h"
 #include "gutil/strings/split.h"
-#include "kudu/client/client.h"
-#include "kudu/client/encoded_key.h"
-#include "kudu/client/schema.h"
-#include "kudu/common/partial_row.h"
-#include "kudu/gutil/gscoped_ptr.h"
-#include "kudu/gutil/ref_counted.h"
-#include "kudu/util/slice.h"
-#include "kudu/util/status.h"
 #include "runtime/descriptors.h"
-#include "runtime/exec-env.h"
 #include "runtime/row-batch.h"
 #include "runtime/runtime-state.h"
 #include "service/fe-support.h"
@@ -44,136 +32,11 @@
 #include "util/cpu-info.h"
 #include "util/test-info.h"
 
-#include "common/names.h"
-
 using apache::thrift::ThriftDebugString;
-using kudu::client::KuduClient;
-using kudu::client::KuduClientBuilder;
-using kudu::client::KuduColumnSchema;
-using kudu::client::KuduEncodedKey;
-using kudu::client::KuduEncodedKeyBuilder;
-using kudu::client::KuduInsert;
-using kudu::client::KuduSchema;
-using kudu::client::KuduSession;
-using kudu::client::KuduTable;
-using kudu::KuduPartialRow;
-using kudu::Slice;
-
-typedef kudu::Status KuduStatus;
-typedef impala::Status ImpalaStatus;
 
 namespace impala {
 
 static const char* BASE_TABLE_NAME = "TestScanNodeTable";
-
-#define KUDU_ASSERT_OK(status) do { \
-    KuduStatus _s = status; \
-    if (_s.ok()) { \
-      SUCCEED(); \
-    } else { \
-      FAIL() << "Bad Kudu Status: " << _s.ToString();  \
-    } \
-  } while (0);
-
-
-// Helper class to assist in tests agains a Kudu cluster, namely with
-// table creation/deletion with insertion of rows.
-class KuduTestHelper {
- public:
-
-  void CreateClient() {
-    LOG(INFO) << "Creating Kudu client.";
-    KUDU_ASSERT_OK(KuduClientBuilder()
-                   .add_master_server_addr("127.0.0.1:7051")
-                   .Build(&client_));
-  }
-
-  void CreateTable(const string& table_name_prefix, const KuduSchema& schema) {
-    // Kudu's table delete functionality is in flux, meaning a table may reappear
-    // after being deleted. To work around this we add the time in milliseconds to
-    // the required table name, making it unique. When Kudu's delete table functionality
-    // is solid we should change this to avoid creating, and possibly leaving, many
-    // similar tables in the local Kudu test cluster. See KUDU-676
-    struct timeval tv;
-    gettimeofday(&tv, NULL);
-    int64_t millis = tv.tv_sec * 1000 + tv.tv_usec / 1000;
-    table_name_ = strings::Substitute("$0-$1", table_name_prefix, millis);
-    schema_ = schema;
-
-    while(true) {
-      LOG(INFO) << "Creating Kudu table: " << table_name_;
-      kudu::Status s = client_->NewTableCreator()->table_name(table_name_)
-                             .schema(&schema_)
-                             .num_replicas(3)
-                             .split_keys(GenerateSplitKeys())
-                             .Create();
-      if (s.IsAlreadyPresent()) {
-        LOG(INFO) << "Table existed, deleting. " << table_name_;
-        KUDU_ASSERT_OK(client_->DeleteTable(table_name_));
-        sleep(1);
-        continue;
-      }
-      KUDU_CHECK_OK(s);
-      KUDU_ASSERT_OK(client_->OpenTable(table_name_, &client_table_));
-      break;
-    }
-  }
-
-  gscoped_ptr<KuduInsert> BuildTestRow(KuduTable* table, int index) {
-    gscoped_ptr<KuduInsert> insert = table->NewInsert();
-    KuduPartialRow* row = insert->mutable_row();
-    KUDU_CHECK_OK(row->SetInt32(0, index));
-    KUDU_CHECK_OK(row->SetInt32(1, index * 2));
-    KUDU_CHECK_OK(row->SetStringCopy(2, Slice(StringPrintf("hello_%d", index))));
-    return insert.Pass();
-  }
-
-  void InsertTestRows(KuduClient* client, KuduTable* table, int num_rows,
-      int first_row = 0) {
-    std::tr1::shared_ptr<KuduSession> session = client->NewSession();
-    KUDU_ASSERT_OK(session->SetFlushMode(KuduSession::MANUAL_FLUSH));
-    session->SetTimeoutMillis(10000);
-    for (int i = first_row; i < num_rows + first_row; i++) {
-      KUDU_ASSERT_OK(session->Apply(BuildTestRow(table, i).Pass()));
-    }
-    KUDU_ASSERT_OK(session->Flush());
-    ASSERT_FALSE(session->HasPendingOperations());
-  }
-
-
-  void DeleteTable() {
-    LOG(INFO) << "Deleting Kudu table: " << table_name_;
-    KUDU_ASSERT_OK(client_->DeleteTable(table_name_));
-  }
-
-  vector<string> GenerateSplitKeys() {
-    vector<string> keys;
-    KuduEncodedKeyBuilder builder(schema_);
-    int val = 5;
-    builder.AddColumnKey(&val);
-    gscoped_ptr<KuduEncodedKey> key(builder.BuildEncodedKey());
-    keys.push_back(key->ToString());
-    return keys;
-  }
-
-  const string& table_name() const {
-    return table_name_;
-  }
-
-  const std::tr1::shared_ptr<KuduClient>& client() const {
-    return client_;
-  }
-
-  const scoped_refptr<KuduTable>& table() const {
-    return client_table_;
-  }
-
- private:
-  string table_name_;
-  KuduSchema schema_;;
-  std::tr1::shared_ptr<KuduClient> client_;
-  scoped_refptr<KuduTable> client_table_;
-};
 
 class KuduScanNodeTest : public testing::Test {
  public:
@@ -184,15 +47,7 @@ class KuduScanNodeTest : public testing::Test {
     // Create a Kudu client and the table (this will abort the test here
     // if a Kudu cluster is not available).
     kudu_test_helper_.CreateClient();
-
-    vector<KuduColumnSchema> column_schemas;
-    column_schemas.push_back(KuduColumnSchema("key", KuduColumnSchema::INT32));
-    column_schemas.push_back(KuduColumnSchema("int_val", KuduColumnSchema::INT32, true));
-    column_schemas.push_back(KuduColumnSchema("string_val", KuduColumnSchema::STRING,
-                                              true));
-    schema_ = KuduSchema(column_schemas, 1);
-
-    kudu_test_helper_.CreateTable(BASE_TABLE_NAME, schema_);
+    kudu_test_helper_.CreateTable(BASE_TABLE_NAME);
 
     // Initialize the environment/runtime so that we can use a scan node in
     // isolation.
@@ -211,29 +66,7 @@ class KuduScanNodeTest : public testing::Test {
     kudu_node_.__set_node_type(TPlanNodeType::KUDU_SCAN_NODE);
     kudu_node_.__set_limit(-1);
 
-    DescriptorTblBuilder desc_builder(&obj_pool_);
-    DCHECK_GE(num_cols_materialize, 0);
-    DCHECK_LE(num_cols_materialize, schema_.num_columns());
-
-    desc_builder.DeclareTuple()
-        .AddSlot(TYPE_INT, num_cols_materialize > 0)
-        .AddSlot(TYPE_INT, num_cols_materialize > 1)
-        .AddSlot(TYPE_STRING, num_cols_materialize > 2);
-
-    TKuduTable t_kudu_table;
-    t_kudu_table.__set_table_name(kudu_test_helper_.table_name());
-    t_kudu_table.__set_master_addresses(vector<string>(1, "0.0.0.0:7051"));
-    t_kudu_table.__set_key_columns(boost::assign::list_of("key"));
-
-    TTableDescriptor t_tbl_desc;
-    t_tbl_desc.__set_id(0);
-    t_tbl_desc.__set_tableType(::impala::TTableType::KUDU_TABLE);
-    t_tbl_desc.__set_kuduTable(t_kudu_table);
-    t_tbl_desc.__set_colNames(boost::assign::list_of("key")("int_val")("string_val"));
-
-    desc_builder.SetTableDescriptor(t_tbl_desc);
-
-    desc_tbl_ = desc_builder.Build();
+    kudu_test_helper_.CreateTableDescriptor(num_cols_materialize, &desc_tbl_);
 
     row_desc_ = obj_pool_.Add(
         new RowDescriptor(*desc_tbl_,
@@ -256,7 +89,7 @@ class KuduScanNodeTest : public testing::Test {
     string batch_as_string = PrintBatch(batch);
 
     DCHECK_GE(num_materialized_cols, 0);
-    DCHECK_LE(num_materialized_cols, schema_.num_columns());
+    DCHECK_LE(num_materialized_cols, kudu_test_helper_.test_schema().num_columns());
 
     string base_row = "[(";
     if (num_materialized_cols >= 1) base_row.append("$0");
@@ -339,7 +172,6 @@ class KuduScanNodeTest : public testing::Test {
   }
 
  protected:
-  KuduSchema schema_;
   KuduTestHelper kudu_test_helper_;
   MemTracker mem_tracker_;
   ObjectPool obj_pool_;
