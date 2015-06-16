c8aab96886cd89810b7c756bc271964cc57f5674
David Alves <david.alves@cloudera.com>
2015-06-16 13:43:36 -0700

    Stop creating test tables in KuduPlannerTest
    Right now we're unecessarily creating 'liketbl' in the planner test
    since it's also created as part of the load_data.py script which reads
    from 'schema_constraints.csv'.
    
    This patch removes the table creation/data insertion from the planner
    test.
    
    Change-Id: I22051f5cabd4cb7b56b286baae05e81419082d86
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/6941
    Tested-by: jenkins
    Reviewed-by: Martin Grund <mgrund@cloudera.com>

--- fe/src/test/java/com/cloudera/impala/planner/KuduPlannerTest.java
+++ fe/src/test/java/com/cloudera/impala/planner/KuduPlannerTest.java
@@ -14,69 +14,12 @@
 
 package com.cloudera.impala.planner;
 
-import java.util.List;
-
-import org.junit.BeforeClass;
 import org.junit.Test;
-import org.kududb.ColumnSchema;
-import org.kududb.Schema;
-import org.kududb.Type;
-import org.kududb.client.CreateTableResponse;
-import org.kududb.client.Insert;
-import org.kududb.client.KuduClient;
-import org.kududb.client.KuduSession;
-import org.kududb.client.KuduTable;
-
-import com.cloudera.impala.common.RuntimeEnv;
-import com.google.common.collect.Lists;
-import com.google.common.net.HostAndPort;
 
 /**
  * This class contains all plan generation related tests with Kudu as a storage backend.
  */
 public class KuduPlannerTest extends PlannerTestBase {
-
-
-  @BeforeClass
-  public static void setUp() throws Exception {
-    // Use 8 cores for resource estimation.
-    RuntimeEnv.INSTANCE.setNumCores(8);
-    // Set test env to control the explain level.
-    RuntimeEnv.INSTANCE.setTestEnv(true);
-
-    KuduClient c = new KuduClient(Lists.newArrayList(
-        HostAndPort.fromParts("127.0.0.1", 7051)));
-
-    // Create the Schema
-    List<org.kududb.ColumnSchema> cols = Lists.newArrayList(
-        new ColumnSchema.ColumnSchemaBuilder("str_col", Type.STRING).key(true).build(),
-        new ColumnSchema.ColumnSchemaBuilder("match_like_col", Type.STRING).build(),
-        new ColumnSchema.ColumnSchemaBuilder("no_match_like_col", Type.STRING).build(),
-        new ColumnSchema.ColumnSchemaBuilder("match_regex_col", Type.STRING).build(),
-        new ColumnSchema.ColumnSchemaBuilder("no_match_regex_col", Type.STRING).build());
-
-
-    if (c.tableExists("liketbl")) {
-      c.deleteTable("liketbl");
-    }
-
-    // Create the table
-    CreateTableResponse res = c.createTable("liketbl", new Schema(cols));
-
-    KuduTable table = c.openTable("liketbl");
-    KuduSession session = c.newSession();
-    // Load data
-    for (int i = 0; i < 1000; ++i) {
-      Insert insert = table.newInsert();
-      insert.addString("str_col", String.valueOf(i));
-      insert.addString("match_like_col", String.valueOf(i));
-      insert.addString("no_match_like_col", String.valueOf(i));
-      insert.addString("match_regex_col", String.valueOf(i));
-      insert.addString("no_match_regex_col", String.valueOf(i));
-      session.apply(insert);
-    }
-    session.flush();
-  }
 
   @Test
   public void testKudu() {
