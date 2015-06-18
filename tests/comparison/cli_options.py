a4fe24c1b2049e6d59e28b56a2c6795775b7c1a1
Casey Ching <casey@cloudera.com>
2015-06-17 20:14:03 -0700

    Python: Add more logging and CM options to common CLI parser
    Example output of --help:
    
    Options:
      --debug-log-file=DEBUG_LOG_FILE
                            Path to debug log file. [default:
                            /tmp/concurrent_select.py.log]
      --cm-host=host name   The host name of the CM server.
      --cm-port=port number
                            The port of the CM server. [default: 7180]
      --cm-user=user name   The name of the CM user. [default: admin]
      --cm-password=password
                            The password for the CM user. [default: admin]
      --cm-cluster-name=name
                            If CM manages multiple clusters, use this to
                            specify which cluster to use.
    
    Change-Id: I614383f4a65e700348572204e3d8fd5670f5bcf7
    Reviewed-on: http://gerrit.cloudera.org:8080/472
    Reviewed-by: Casey Ching <casey@cloudera.com>
    Tested-by: Casey Ching <casey@cloudera.com>

--- tests/comparison/cli_options.py
+++ tests/comparison/cli_options.py
@@ -15,20 +15,59 @@
 '''Helpers for parsing command line options'''
 
 import logging
+import os
+import sys
 from optparse import NO_DEFAULT, OptionGroup
+from tempfile import gettempdir
 
 from tests.comparison.types import TYPES
 
-def add_logging_options(section):
+def add_logging_options(section, default_debug_log_file=None):
+  if not default_debug_log_file:
+    default_debug_log_file = os.path.join(
+        gettempdir(), os.path.basename(sys.modules["__main__"].__file__) + ".log")
   section.add_option('--log-level', default='INFO',
       help='The log level to use.', choices=('DEBUG', 'INFO', 'WARN', 'ERROR'))
+  section.add_option('--debug-log-file', default=default_debug_log_file,
+      help='Path to debug log file.')
 
 
-def configure_logging(log_level):
-  logging.basicConfig(
-      level=getattr(logging, log_level),
-      format='%(asctime)s %(levelname)s:%(module)s[%(lineno)s]:%(message)s',
-      datefmt='%H:%M:%S')
+def configure_logging(log_level, debug_log_file=None, log_thread_id=False,
+    log_process_id=False):
+  root_logger = logging.getLogger()
+  root_logger.setLevel(logging.DEBUG)
+
+  console_logger = logging.StreamHandler(sys.stdout)
+  console_logger.name = "console"
+  console_logger.setLevel(getattr(logging, log_level))
+  format = "%(asctime)s"
+  if log_process_id:
+    format += " %(process)d"
+  if log_thread_id:
+    format += " %(thread)d"
+  format += " %(levelname)s:%(module)s[%(lineno)s]:%(message)s"
+  console_logger.setFormatter(logging.Formatter(format, "%H:%M:%S"))
+  root_logger.addHandler(console_logger)
+
+  if debug_log_file:
+    file_logger = logging.FileHandler(debug_log_file, mode="w")
+    file_logger.name = "file"
+    file_logger.setFormatter(logging.Formatter(format, "%H:%M:%S"))
+    file_logger.setLevel(logging.DEBUG)
+    root_logger.addHandler(file_logger)
+
+
+def add_cm_options(section):
+  section.add_option('--cm-host', metavar='host name',
+      help='The host name of the CM server.')
+  section.add_option('--cm-port', default=7180, type=int, metavar='port number',
+      help='The port of the CM server.')
+  section.add_option('--cm-user', default="admin", metavar='user name',
+      help='The name of the CM user.')
+  section.add_option('--cm-password', default="admin", metavar='password',
+      help='The password for the CM user.')
+  section.add_option('--cm-cluster-name', metavar='name',
+      help='If CM manages multiple clusters, use this to specify which cluster to use.')
 
 
 def add_db_name_option(section):
@@ -115,5 +154,5 @@ def add_connection_option_groups(parser):
 def add_default_values_to_help(parser):
   for group in parser.option_groups + [parser]:
     for option in group.option_list:
-      if option.default != NO_DEFAULT:
+      if option.default != NO_DEFAULT and option.help:
         option.help += ' [default: %default]'
