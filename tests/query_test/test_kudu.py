12052021559217ea54a6ab114e4f8c26e4a84fa5
Martin Grund <mgrund@cloudera.com>
2015-06-25 17:35:00 -0700

    Add boilerplate code for Kudu end-to-end test
    Change-Id: I568719afe5c172ac7e4ac98f9fe030f9710f26f1
    Reviewed-on: http://gerrit.sjc.cloudera.com:8080/7038
    Reviewed-by: David Alves <david.alves@cloudera.com>
    Tested-by: jenkins

---
+++ tests/query_test/test_kudu.py
@@ -0,0 +1,43 @@
+# Copyright 2012 Cloudera Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from tests.common.impala_test_suite import ImpalaTestSuite
+
+class TestKuduOperations(ImpalaTestSuite):
+  """
+  This suite tests the different modification operations when using a kudu table.
+  """
+
+  @classmethod
+  def file_format_constraint(cls, v):
+    return v.get_value('table_format').file_format in ["parquet"]
+
+  @classmethod
+  def get_workload(cls):
+    return 'functional-query'
+
+  @classmethod
+  def add_test_dimensions(cls):
+    super(TestKuduOperations, cls).add_test_dimensions()
+    cls.TestMatrix.add_constraint(cls.file_format_constraint)
+
+  def setup_method(self, method):
+    self.cleanup_db("kududb_test")
+    self.client.execute("create database kududb_test")
+
+  def teardown_method(self, methd):
+    self.cleanup_db("kududb_test")
+
+  def test_sample(self, vector):
+    pass
