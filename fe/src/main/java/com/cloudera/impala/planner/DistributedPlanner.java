d94a930ef07f691e5202a6d6c473d6bf5fa0ee33
Alex Behm <alex.behm@cloudera.com>
2015-08-29 12:39:40 -0700

    IMPALA-2266: Pass correct child node in 2nd phase merge aggregation.
    The bug was that we were not passing the correct child node in the
    constructor of the AggregationNode for the 2nd phase merge. As a result,
    the 2nd phase merge aggregation node did not have the correct output
    smap set, and all exprs on top were not substituted properly.
    
    The bug was not apparent because the child node of the 2nd phase distinct
    aggregation was fixed up after construction when setting the new plan
    root of the corresponding fragment, so the bug did not manifest itself in
    cases where the output smap was empty anyway (e.g., distinct aggregations
    in the top-level select block).
    
    Change-Id: I0617b2389e55ebcaf424600803babb250323ad3c
    Reviewed-on: http://gerrit.cloudera.org:8080/715
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Internal Jenkins

--- fe/src/main/java/com/cloudera/impala/planner/DistributedPlanner.java
+++ fe/src/main/java/com/cloudera/impala/planner/DistributedPlanner.java
@@ -136,7 +136,7 @@ public class DistributedPlanner {
       if (((SortNode) root).isAnalyticSort()) {
         // don't parallelize this like a regular SortNode
         result = createAnalyticFragment(
-            (SortNode) root, childFragments.get(0), fragments);
+            root, childFragments.get(0), fragments);
       } else {
         result = createOrderByFragment(
             (SortNode) root, childFragments.get(0), fragments);
@@ -379,7 +379,7 @@ public class DistributedPlanner {
         && node.getJoinOp() != JoinOperator.RIGHT_SEMI_JOIN
         && node.getJoinOp() != JoinOperator.RIGHT_ANTI_JOIN
         && (perNodeMemLimit == 0
-            || Math.round((double) rhsDataSize * PlannerContext.HASH_TBL_SPACE_OVERHEAD)
+            || Math.round(rhsDataSize * PlannerContext.HASH_TBL_SPACE_OVERHEAD)
                 <= perNodeMemLimit)
         && (node.getDistributionModeHint() == DistributionMode.BROADCAST
             || (node.getDistributionModeHint() != DistributionMode.PARTITIONED
@@ -828,8 +828,7 @@ public class DistributedPlanner {
       node.unsetLimit();
       mergeFragment = createParentFragment(mergeFragment, DataPartition.UNPARTITIONED);
       mergeAggInfo = node.getAggInfo().getMergeAggInfo();
-      mergeAggNode =
-          new AggregationNode(ctx_.getNextNodeId(), node.getChild(0), mergeAggInfo);
+      mergeAggNode = new AggregationNode(ctx_.getNextNodeId(), node, mergeAggInfo);
       mergeAggNode.init(ctx_.getRootAnalyzer());
       // Transfer having predicates. If hasGrouping == true, the predicates should
       // instead be evaluated by the 2nd phase agg (the predicates are already there).
