b0bfe410461736276a61af4649a80ae77c40cae6
Sailesh Mukil <sailesh@cloudera.com>
2015-09-22 15:41:23 -0700

    IMPALA-2252: Crash (likely race) tearing down BufferedBlockMgr on query failure
    When running certain heavy workloads it was observed that a
    'RuntimeState' member variable was being accessed in WriteComplete()
    on a RequestContext::Cancel() path after that RuntimeState object was
    destroyed. This is a temporary fix to ensure that the destroyed member
    variable is not accessed if the write status is cancelled.
    
    A test is not included as it is not deterministically reproducible.
    
    Change-Id: I8a55c070d25f0ca5c830a955e84df450061753a3
    Reviewed-on: http://gerrit.cloudera.org:8080/897
    Reviewed-by: Sailesh Mukil <sailesh@cloudera.com>
    Tested-by: Internal Jenkins

--- be/src/runtime/buffered-block-mgr.cc
+++ be/src/runtime/buffered-block-mgr.cc
@@ -786,6 +786,8 @@ void BufferedBlockMgr::WriteComplete(Block* block, const Status& write_status) {
   if (encryption_) EncryptDone(block);
 
   // ReturnUnusedBlock() will clear the block, so save the client pointer.
+  // We have to be careful while touching the state because it may have been cleaned up by
+  // another thread.
   RuntimeState* state = block->client_->state_;
   // If the block was re-pinned when it was in the IOMgr queue, don't free it.
   if (block->is_pinned_) {
@@ -818,8 +820,12 @@ void BufferedBlockMgr::WriteComplete(Block* block, const Status& write_status) {
   DCHECK(Validate()) << endl << DebugInternal();
 
   if (!write_status.ok() || !status.ok() || is_cancelled_) {
+    VLOG_FILE << "Query: " << query_id_ << ". Write did not complete successfully: "
+        "write_status=" << write_status.GetDetail() << ", status=" << status.GetDetail()
+        << ". is_cancelled_=" << is_cancelled_;
+
     // If the instance is already cancelled, don't confuse things with these errors.
-    if (!state->is_cancelled()) {
+    if (!write_status.IsCancelled() && !state->is_cancelled()) {
       if (!write_status.ok()) {
         // Report but do not attempt to recover from write error.
         DCHECK(block->tmp_file_ != NULL);
