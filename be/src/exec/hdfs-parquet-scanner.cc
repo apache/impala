1a09ca10856af757962094bce4c5424c696a0bd2
Tim Armstrong <tarmstrong@cloudera.com>
2015-10-02 12:36:56 -0700

    IMPALA-2468: error processing multiblock Parquet files
    A recent commit (IMPALA-2444: Part 5) introduced a bug with
    handling of dictionary pages - the dicionary wasn't reset
    after every row group. This fix resets the dictionary when
    BaseScalarColumnReader::Reset() is called.
    
    Change-Id: I699f4b33f7b75e837ca421ecace3d771fe2fcf56
    Reviewed-on: http://gerrit.cloudera.org:8080/1136
    Reviewed-by: Dan Hecht <dhecht@cloudera.com>
    Tested-by: Internal Jenkins

--- be/src/exec/hdfs-parquet-scanner.cc
+++ be/src/exec/hdfs-parquet-scanner.cc
@@ -458,6 +458,7 @@ class HdfsParquetScanner::BaseScalarColumnReader :
       RETURN_IF_ERROR(Codec::CreateDecompressor(
           NULL, false, PARQUET_TO_IMPALA_CODEC[metadata_->codec], &decompressor_));
     }
+    ClearDictionaryDecoder();
     return Status::OK();
   }
 
@@ -564,6 +565,10 @@ class HdfsParquetScanner::BaseScalarColumnReader :
   /// implement this.
   virtual bool HasDictionaryDecoder() = 0;
 
+  /// Clear the dictionary decoder so HasDictionaryDecoder() will return false. Subclass
+  /// must implement this.
+  virtual void ClearDictionaryDecoder() = 0;
+
   /// Initializes the reader with the data contents. This is the content for the entire
   /// decompressed data page. Decoders can initialize state from here.
   virtual Status InitDataPage(uint8_t* data, int size) = 0;
@@ -663,6 +668,10 @@ class HdfsParquetScanner::ScalarColumnReader :
 
   virtual bool HasDictionaryDecoder() {
     return dict_decoder_init_;
+  }
+
+  virtual void ClearDictionaryDecoder() {
+    dict_decoder_init_ = false;
   }
 
   virtual Status InitDataPage(uint8_t* data, int size) {
@@ -845,6 +854,8 @@ class HdfsParquetScanner::BoolColumnReader :
     // Decoder should never be created for bools.
     return false;
   }
+
+  virtual void ClearDictionaryDecoder() { }
 
   virtual Status InitDataPage(uint8_t* data, int size) {
     // Initialize bool decoder
