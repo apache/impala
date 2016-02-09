apache/impala

commit 7e2a8032647f30abdd2829952268de0399c6aeb4

IMPALA-2432: Add query endtime to impalad's lineage
This commit adds query endtime to impalad's lineage log entries consumed
by navigator. The lineage graph is constructed in the frontend and is then
passed to the backend as a serialized thrift object. When the query terminates
(includes cancellations and aborts), the backend appends the query endtime
("endTime") to the lineage graph and generates the lineage log entry in
JSON format.

Change-Id: I2236e98895ae9a159ad6e78b0e18e3622fdc3306
Reviewed-on: http://gerrit.cloudera.org:8080/934
Reviewed-by: Bharath Vissapragada <bharathv@cloudera.com>
Tested-by: Internal Jenkins

be/src/service/impala-server.cc
@@ -64,6 +64,7 @@
 #include "util/error-util.h"
 #include "util/histogram-metric.h"
 #include "util/impalad-metrics.h"
+#include "util/lineage-util.h"
 #include "util/network-util.h"
 #include "util/parse-util.h"
 #include "util/redactor.h"
@@ -76,6 +77,7 @@
 #include "gen-cpp/DataSinks_types.h"
 #include "gen-cpp/ImpalaService_types.h"
 #include "gen-cpp/ImpalaInternalService.h"
+#include "gen-cpp/LineageGraph_types.h"
 
 #include "common/names.h"
 
@@ -362,11 +364,12 @@ ImpalaServer::ImpalaServer(ExecEnv* exec_env)
   exec_env_->SetImpalaServer(this);
 }
 
-Status ImpalaServer::LogLineageRecord(const TExecRequest& request) {
+Status ImpalaServer::LogLineageRecord(const QueryExecState& query_exec_state) {
+  const TExecRequest& request = query_exec_state.exec_request();
   if (!request.__isset.query_exec_request && !request.__isset.catalog_op_request) {
     return Status::OK();
   }
-  string lineage_graph;
+  TLineageGraph lineage_graph;
   if (request.__isset.query_exec_request &&
       request.query_exec_request.__isset.lineage_graph) {
     lineage_graph = request.query_exec_request.lineage_graph;
@@ -376,8 +379,11 @@ Status ImpalaServer::LogLineageRecord(const TExecRequest& request) {
   } else {
     return Status::OK();
   }
-  DCHECK(!lineage_graph.empty());
-  Status status = lineage_logger_->AppendEntry(lineage_graph);
+  // Set the query end time in TLineageGraph
+  lineage_graph.__set_ended(query_exec_state.end_time().ToUnixTime());
+
+  const Status& status = lineage_logger_->AppendEntry(
+      LineageUtil::TLineageToJSON(lineage_graph));
   if (!status.ok()) {
     LOG(ERROR) << "Unable to record query lineage record: " << status.GetDetail();
     if (FLAGS_abort_on_failed_lineage_event) {
@@ -534,7 +540,7 @@ void ImpalaServer::LogQueryEvents(const QueryExecState& exec_state) {
     LogAuditRecord(exec_state, exec_state.exec_request());
   }
   if (IsLineageLoggingEnabled() && log_events) {
-    LogLineageRecord(exec_state.exec_request());
+    LogLineageRecord(exec_state);
   }
 }
 

be/src/util/lineage-util.h
@@ -1,0 +1,116 @@
+// Copyright 2015 Cloudera Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+
+#ifndef IMPALA_UTIL_LINEAGE_H
+#define IMPALA_UTIL_LINEAGE_H
+
+
+#include <rapidjson/rapidjson.h>
+#include <rapidjson/stringbuffer.h>
+#include <rapidjson/writer.h>
+
+#include "common/logging.h"
+#include "gen-cpp/LineageGraph_types.h"
+
+namespace impala {
+
+/// Utility class to serialize column lineage graphs to JSON
+class LineageUtil {
+  private:
+    /// Serializes a TVertex object to JSON
+    static string TVertexToJSON(TVertex vertex) {
+      rapidjson::StringBuffer buffer;
+      rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+      writer.StartObject();
+      writer.String("id");
+      writer.Int64(vertex.id);
+      writer.String("vertexType");
+      writer.String("COLUMN");
+      writer.String("vertexId");
+      writer.String(vertex.label.c_str());
+      writer.EndObject();
+      // Return a string representation of json
+      return buffer.GetString();
+    }
+
+    /// Serializes a TMultiEdge object to JSON
+    static string TMultiEdgeToJSON(TMultiEdge obj) {
+      rapidjson::StringBuffer buffer;
+      rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+      writer.StartObject();
+      // Write source vertices
+      writer.String("sources");
+      writer.StartArray();
+      for(int i=0; i < obj.sources.size(); ++i) {
+        writer.Int64(obj.sources[i].id);
+      }
+      writer.EndArray();
+      // Write target vertices
+      writer.String("targets");
+      writer.StartArray();
+      for(int i=0; i < obj.targets.size(); ++i) {
+        writer.Int64(obj.targets[i].id);
+      }
+      writer.EndArray();
+      // Write edgetype
+      writer.String("edgeType");
+      string edge_type =
+          (obj.edgetype == TEdgeType::PROJECTION) ? "PROJECTION" : "PREDICATE";
+      writer.String(edge_type.c_str());
+      writer.EndObject();
+      return buffer.GetString();
+    }
+
+  public:
+    /// Serializes a TLineageGraph object to JSON
+    static string TLineageToJSON(TLineageGraph lineage) {
+      rapidjson::StringBuffer buffer;
+      rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+      writer.StartObject();
+      writer.String("queryText");
+      writer.String(lineage.query_text.c_str());
+      writer.String("hash");
+      writer.String(lineage.hash.c_str());
+      writer.String("user");
+      writer.String(lineage.user.c_str());
+      // write query start time
+      writer.String("timestamp");
+      writer.Int64(lineage.started);
+      // write query end time
+      writer.String("endTime");
+      DCHECK(lineage.ended > lineage.started);
+      writer.Int64(lineage.ended);
+      // Write edges
+      writer.String("edges");
+      writer.StartArray();
+      for(int i=0; i < lineage.edges.size(); ++i) {
+        writer.String(TMultiEdgeToJSON(lineage.edges[i]).c_str());
+      }
+      writer.EndArray();
+      // Write vertices
+      writer.String("vertices");
+      writer.StartArray();
+      for(int i=0; i < lineage.vertices.size(); ++i) {
+        writer.String(TVertexToJSON(lineage.vertices[i]).c_str());
+      }
+      writer.EndArray();
+      writer.EndObject();
+      return buffer.GetString();
+    }
+
+};
+
+}
+#endif

be/generated-sources/gen-cpp/CMakeLists.txt
@@ -65,6 +65,8 @@ set(SRC_FILES
   JniCatalog_constants.cpp
   JniCatalog_types.cpp
   Logging_types.cpp
+  LineageGraph_types.cpp
+  LineageGraph_constants.cpp
   Metrics_constants.cpp
   Metrics_types.cpp
   MetricDefs_constants.cpp

be/src/service/impala-server.h
@@ -521,7 +521,7 @@ class ImpalaServer : public ImpalaServiceIf, public ImpalaHiveServer2ServiceIf,
 
   Status LogAuditRecord(const QueryExecState& exec_state, const TExecRequest& request);
 
-  Status LogLineageRecord(const TExecRequest& request);
+  Status LogLineageRecord(const QueryExecState& exec_state);
 
   /// Log audit and column lineage events
   void LogQueryEvents(const QueryExecState& exec_state);

common/thrift/CMakeLists.txt
@@ -158,6 +158,7 @@ set (SRC_FILES
   ImpalaInternalService.thrift
   ImpalaService.thrift
   JniCatalog.thrift
+  LineageGraph.thrift
   Llama.thrift
   Logging.thrift
   NetworkTest.thrift

common/thrift/Frontend.thrift
@@ -28,6 +28,7 @@ include "TCLIService.thrift"
 include "Status.thrift"
 include "CatalogObjects.thrift"
 include "CatalogService.thrift"
+include "LineageGraph.thrift"
 
 // These are supporting structs for JniFrontend.java, which serves as the glue
 // between our C++ execution environment and the Java frontend.
@@ -368,8 +369,8 @@ struct TQueryExecRequest {
   // List of replica hosts.  Used by the host_idx field of TScanRangeLocation.
   12: required list<Types.TNetworkAddress> host_list
 
-  // Column lineage graph serialized into JSON
-  13: optional string lineage_graph
+  // Column lineage graph
+  13: optional LineageGraph.TLineageGraph lineage_graph
 }
 
 enum TCatalogOpType {
@@ -436,8 +437,8 @@ struct TCatalogOpRequest {
   // Parameters for SHOW FILES
   14: optional TShowFilesParams show_files_params
 
-  // Column lineage graph serialized into JSON
-  15: optional string lineage_graph
+  // Column lineage graph
+  15: optional LineageGraph.TLineageGraph lineage_graph
 
   // Parameters for SHOW_CREATE_FUNCTION
   16: optional TGetFunctionsParams show_create_function_params

common/thrift/LineageGraph.thrift
@@ -1,0 +1,62 @@
+// Copyright 2015 Cloudera Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+namespace cpp impala
+namespace java com.cloudera.impala.thrift
+
+struct TVertex {
+  // Vertex id
+  1: required i64 id
+
+  // Column label
+  2: required string label
+}
+
+enum TEdgeType {
+    PROJECTION,
+    PREDICATE
+}
+
+struct TMultiEdge {
+  // Set of source vertices
+  1: list<TVertex> sources
+
+  // Set of target vertices
+  2: list<TVertex> targets
+
+  // Connecting edge type
+  3: TEdgeType edgetype
+}
+
+struct TLineageGraph {
+  // Input SQL query
+  1: required string query_text
+
+  // md5 hash of the query string
+  2: required string hash
+
+  // Name of the user who issued this query
+  3: required string user
+
+  // Query start time as a unix epoch, set in planner
+  4: required i64 started
+
+  // Query end time as unix epoch.
+  // Marked as optional as it is set in the backend
+  5: optional i64 ended
+
+  6: list<TMultiEdge> edges
+
+  7: list<TVertex> vertices
+}

fe/src/main/java/com/cloudera/impala/analysis/AnalysisContext.java
@@ -37,6 +37,7 @@ import com.cloudera.impala.catalog.Table;
 import com.cloudera.impala.common.AnalysisException;
 import com.cloudera.impala.thrift.TAccessEvent;
 import com.cloudera.impala.thrift.TDescribeTableOutputStyle;
+import com.cloudera.impala.thrift.TLineageGraph;
 import com.cloudera.impala.thrift.TQueryCtx;
 import com.cloudera.impala.common.Pair;
 
@@ -305,7 +306,9 @@ public class AnalysisContext {
       return analyzer_.containsSubquery() && !(stmt_ instanceof CreateViewStmt)
           && !(stmt_ instanceof AlterViewStmt);
     }
-    public String getJsonLineageGraph() { return analyzer_.getSerializedLineageGraph(); }
+    public TLineageGraph getThriftLineageGraph() {
+      return analyzer_.getThriftSerializedLineageGraph();
+    }
   }
 
   /**

commit 4cbb8f07ae82de4dcb564bba6f51a6a4ab3dca02

CDH-34514: Fix impalad lineage output format
Commit 7e2a803 (IMPALA-2432) introduced a minor bug to the lineage
output from impalad as it writes the Vertex and MultiEdge objects
as strings rather than nested json structures. This commit fixes
that issue.

Change-Id: Id53f6a9b3df76acb592ed8babdc7c7dac5ba92bb
Reviewed-on: http://gerrit.cloudera.org:8080/2097
Reviewed-by: Bharath Vissapragada <bharathv@cloudera.com>
Tested-by: Internal Jenkins

be/src/service/impala-server.cc
@@ -380,8 +380,9 @@ Status ImpalaServer::LogLineageRecord(const QueryExecState& query_exec_state) {
   }
   // Set the query end time in TLineageGraph
   lineage_graph.__set_ended(query_exec_state.end_time().ToUnixTimeInUTC());
-  const Status& status = lineage_logger_->AppendEntry(
-      LineageUtil::TLineageToJSON(lineage_graph));
+  string lineage_record;
+  LineageUtil::TLineageToJSON(lineage_graph, &lineage_record);
+  const Status& status = lineage_logger_->AppendEntry(lineage_record);
   if (!status.ok()) {
     LOG(ERROR) << "Unable to record query lineage record: " << status.GetDetail();
     if (FLAGS_abort_on_failed_lineage_event) {

be/src/util/lineage-util.h
@@ -30,52 +30,47 @@ namespace impala {
 class LineageUtil {
   private:
     /// Serializes a TVertex object to JSON
-    static string TVertexToJSON(TVertex vertex) {
-      rapidjson::StringBuffer buffer;
-      rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-      writer.StartObject();
-      writer.String("id");
-      writer.Int64(vertex.id);
-      writer.String("vertexType");
-      writer.String("COLUMN");
-      writer.String("vertexId");
-      writer.String(vertex.label.c_str());
-      writer.EndObject();
-      // Return a string representation of json
-      return buffer.GetString();
+    static void TVertexToJSON(const TVertex &vertex,
+        rapidjson::Writer<rapidjson::StringBuffer>* writer) {
+      writer->StartObject();
+      writer->String("id");
+      writer->Int64(vertex.id);
+      writer->String("vertexType");
+      writer->String("COLUMN");
+      writer->String("vertexId");
+      writer->String(vertex.label.c_str());
+      writer->EndObject();
     }
 
     /// Serializes a TMultiEdge object to JSON
-    static string TMultiEdgeToJSON(TMultiEdge obj) {
-      rapidjson::StringBuffer buffer;
-      rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
-      writer.StartObject();
+    static void TMultiEdgeToJSON(const TMultiEdge &obj,
+        rapidjson::Writer<rapidjson::StringBuffer>* writer) {
+      writer->StartObject();
       // Write source vertices
-      writer.String("sources");
-      writer.StartArray();
+      writer->String("sources");
+      writer->StartArray();
       for(int i=0; i < obj.sources.size(); ++i) {
-        writer.Int64(obj.sources[i].id);
+        writer->Int64(obj.sources[i].id);
       }
-      writer.EndArray();
+      writer->EndArray();
       // Write target vertices
-      writer.String("targets");
-      writer.StartArray();
+      writer->String("targets");
+      writer->StartArray();
       for(int i=0; i < obj.targets.size(); ++i) {
-        writer.Int64(obj.targets[i].id);
+        writer->Int64(obj.targets[i].id);
       }
-      writer.EndArray();
+      writer->EndArray();
       // Write edgetype
-      writer.String("edgeType");
+      writer->String("edgeType");
       string edge_type =
           (obj.edgetype == TEdgeType::PROJECTION) ? "PROJECTION" : "PREDICATE";
-      writer.String(edge_type.c_str());
-      writer.EndObject();
-      return buffer.GetString();
+      writer->String(edge_type.c_str());
+      writer->EndObject();
     }
 
   public:
     /// Serializes a TLineageGraph object to JSON
-    static string TLineageToJSON(TLineageGraph lineage) {
+    static void TLineageToJSON(const TLineageGraph &lineage, string* out) {
       rapidjson::StringBuffer buffer;
       rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
       writer.StartObject();
@@ -96,20 +91,19 @@ class LineageUtil {
       writer.String("edges");
       writer.StartArray();
       for(int i=0; i < lineage.edges.size(); ++i) {
-        writer.String(TMultiEdgeToJSON(lineage.edges[i]).c_str());
+        TMultiEdgeToJSON(lineage.edges[i], &writer);
       }
       writer.EndArray();
       // Write vertices
       writer.String("vertices");
       writer.StartArray();
       for(int i=0; i < lineage.vertices.size(); ++i) {
-        writer.String(TVertexToJSON(lineage.vertices[i]).c_str());
+        TVertexToJSON(lineage.vertices[i], &writer);
       }
       writer.EndArray();
       writer.EndObject();
-      return buffer.GetString();
+      *out = buffer.GetString();
     }
-
 };
 
 }