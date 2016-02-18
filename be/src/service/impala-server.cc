4ddba998b1518d19b996011d5a175911e6d4f9af
Henry Robinson <henry@cloudera.com>
2016-02-17 16:42:17 -0800

    IMPALA-3020: Don't take QueryExecState::lock() needlessly
    ImpalaServer::UpdateFilter() takes QueryExecState::lock(), but doesn't
    need to, leading to excessive contention for that lock if UpdateFilter()
    takes a long time due to Bloom Filters being large.
    
    Change-Id: Ia252f4dadbd5c4c9ae375e4c9ec8f2862d3bff82
    Reviewed-on: http://gerrit.cloudera.org:8080/2221
    Reviewed-by: Henry Robinson <henry@cloudera.com>
    Tested-by: Henry Robinson <henry@cloudera.com>

--- be/src/service/impala-server.cc
+++ be/src/service/impala-server.cc
@@ -1952,13 +1952,11 @@ void ImpalaServer::DetectNmFailures() {
 
 void ImpalaServer::UpdateFilter(TUpdateFilterResult& result,
     const TUpdateFilterParams& params) {
-  shared_ptr<QueryExecState> query_exec_state = GetQueryExecState(params.query_id, true);
+  shared_ptr<QueryExecState> query_exec_state = GetQueryExecState(params.query_id, false);
   if (query_exec_state.get() == NULL) {
     LOG(INFO) << "Could not find query exec state: " << params.query_id;
     return;
   }
-
-  lock_guard<mutex> l(*query_exec_state->lock(), adopt_lock_t());
   query_exec_state->coord()->UpdateFilter(params);
 }
 
