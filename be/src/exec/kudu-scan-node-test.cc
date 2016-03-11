mjacobs/impala-kudu

Get and use Kudu from the toolchain by default
This is for review purposes only. This patch will be merged with David's
big merge patch.

Changes:
1) Make Kudu compilation dependent on the OS since not all OSs support
   Kudu.
2) Only run Kudu related tests when Kudu is supported (see #1).
3) Look for Kudu locally, but in a different location. To use a local
   build of Kudu, set KUDU_BUILD_DIR to the path Kudu was built in and
   set KUDU_CLIENT_DIR to the path KUDU was installed in.
   Example:
     git clone https://github.com/cloudera/kudu.git
     ...build 3rd party etc...
     mkdir -p $KUDU_BUILD_DIR
     cd $KUDU_BUILD_DIR
     cmake <path to Kudu source dir>
     make
     DESTDIR=$KUDU_CLIENT_DIR make install
4) Look for Kudu in the toolchain if not using a local Kudu build.
5) Add Kudu service startup scripts. The Kudu in the toolchain is
   actually a parcel that has been renamed (the contents were not
   modified in any way), that mean the Kudu service binaries are there.
   Those binaries are now used to run the Kudu service.

Change-Id: I3db88cbd27f2ea2394f011bc8d1face37411ed58

be/src/exec/kudu-scan-node-test.cc
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifndef KUDU
+#error Kudu is required
+#endif
+
 #include "exec/kudu-testutil.h"
 
 #include "common/init.h"

be/src/exec/kudu-scan-node.h
@@ -17,7 +17,9 @@
 
 #include <boost/scoped_ptr.hpp>
 #include <gtest/gtest.h>
+#ifdef KUDU
 #include <kudu/client/client.h>
+#endif
 
 #include "exec/scan-node.h"
 #include "runtime/descriptors.h"
@@ -29,6 +31,8 @@ namespace impala {
 
 class KuduScanner;
 class Tuple;
+
+#ifdef KUDU
 
 /// A scan node that scans Kudu TabletServers.
 ///
@@ -53,20 +57,6 @@ class KuduScanNode : public ScanNode {
 
   /// Close connections to Kudu.
   virtual void Close(RuntimeState* state);
-
-  const std::vector<std::string>& projected_columns() const { return projected_columns_; }
-
-  const TupleDescriptor* tuple_desc() const { return tuple_desc_; }
-
-  // Returns a cloned copy of the scan node's conjuncts. Requires that the expressions
-  // have been open previously.
-  Status GetConjunctCtxs(vector<ExprContext*>* ctxs);
-
-  // Clones the set of predicates to be set on scanners.
-  void ClonePredicates(vector<kudu::client::KuduPredicate*>* predicates);
-
-  RuntimeProfile::Counter* kudu_read_timer() const { return kudu_read_timer_; }
-  RuntimeProfile::Counter* kudu_round_trips() const { return kudu_round_trips_; }
 
  protected:
   /// Write debug string of this into out.
@@ -176,7 +166,47 @@ class KuduScanNode : public ScanNode {
   /// Returns the next partition key range to read. Thread safe. Returns NULL if there are
   /// no more ranges.
   TKuduKeyRange* GetNextKeyRange();
+
+  const std::vector<std::string>& projected_columns() const { return projected_columns_; }
+
+  const TupleDescriptor* tuple_desc() const { return tuple_desc_; }
+
+  // Returns a cloned copy of the scan node's conjuncts. Requires that the expressions
+  // have been open previously.
+  Status GetConjunctCtxs(vector<ExprContext*>* ctxs);
+
+  // Clones the set of predicates to be set on scanners.
+  void ClonePredicates(vector<kudu::client::KuduPredicate*>* predicates);
+
+  RuntimeProfile::Counter* kudu_read_timer() const { return kudu_read_timer_; }
+  RuntimeProfile::Counter* kudu_round_trips() const { return kudu_round_trips_; }
 };
+
+#else // No Kudu
+
+class KuduScanNode : public ScanNode {
+ public:
+  KuduScanNode(ObjectPool *pool, const TPlanNode &tnode, const DescriptorTbl &descs)
+      : ScanNode(pool, tnode, descs) {}
+
+  ~KuduScanNode() {}
+
+  virtual Status Prepare(RuntimeState *state) {
+    return Status(TErrorCode::KUDU_NOT_SUPPORTED_ON_OS);
+  }
+
+  virtual Status Open(RuntimeState *state) {
+    return Status(TErrorCode::KUDU_NOT_SUPPORTED_ON_OS);
+  }
+
+  virtual Status GetNext(RuntimeState *state, RowBatch *row_batch, bool *eos) {
+    return Status(TErrorCode::KUDU_NOT_SUPPORTED_ON_OS);
+  }
+
+  virtual void Close(RuntimeState *state) {}
+};
+
+#endif
 
 }
 

be/src/exec/kudu-scanner.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef KUDU
+
 #include "exec/kudu-scanner.h"
 
 #include <boost/foreach.hpp>
@@ -344,3 +346,5 @@ Status KuduScanner::GetNextBlock() {
 }
 
 }  // namespace impala
+
+#endif

be/src/exec/kudu-table-sink.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef KUDU
+
 #include "exec/kudu-table-sink.h"
 
 #include <sstream>
@@ -287,3 +289,5 @@ void KuduTableSink::Close(RuntimeState* state) {
 }
 
 }  // namespace impala
+
+#endif

be/src/exec/kudu-util.h
@@ -15,11 +15,16 @@
 #ifndef IMPALA_UTIL_KUDU_UTIL_H_
 #define IMPALA_UTIL_KUDU_UTIL_H_
 
+#ifdef KUDU
 #include <kudu/client/callbacks.h>
 #include <kudu/client/client.h>
+#endif
+
 #include <boost/unordered_map.hpp>
 
 namespace impala {
+
+#ifdef KUDU
 
 class TExpr;
 class KuduTableDescriptor;
@@ -65,6 +70,12 @@ void LogKuduMessage(kudu::client::KuduLogSeverity severity, const char* filename
       return Status(strings::Substitute("$0: $1", prepend, _s.ToString())); \
     } \
   } while (0)
+
+#else  // No Kudu
+
+void InitKuduLogging() {}
+
+#endif
 
 } /// namespace impala
 #endif

apache/impala

be/src/exec/kudu-scan-node-test.cc
@@ -12,6 +12,10 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifndef USE_KUDU
+#error Kudu is required
+#endif
+
 #include "exec/kudu-testutil.h"
 
 #include "common/init.h"

be/src/exec/kudu-scan-node.h
@@ -17,7 +17,9 @@
 
 #include <boost/scoped_ptr.hpp>
 #include <gtest/gtest.h>
+#ifdef USE_KUDU
 #include <kudu/client/client.h>
+#endif
 
 #include "exec/scan-node.h"
 #include "runtime/descriptors.h"
@@ -29,6 +31,8 @@ namespace impala {
 
 class KuduScanner;
 class Tuple;
+
+#ifdef USE_KUDU
 
 /// A scan node that scans Kudu TabletServers.
 ///
@@ -53,20 +57,6 @@ class KuduScanNode : public ScanNode {
 
   /// Close connections to Kudu.
   virtual void Close(RuntimeState* state);
-
-  const std::vector<std::string>& projected_columns() const { return projected_columns_; }
-
-  const TupleDescriptor* tuple_desc() const { return tuple_desc_; }
-
-  // Returns a cloned copy of the scan node's conjuncts. Requires that the expressions
-  // have been open previously.
-  Status GetConjunctCtxs(vector<ExprContext*>* ctxs);
-
-  // Clones the set of predicates to be set on scanners.
-  void ClonePredicates(vector<kudu::client::KuduPredicate*>* predicates);
-
-  RuntimeProfile::Counter* kudu_read_timer() const { return kudu_read_timer_; }
-  RuntimeProfile::Counter* kudu_round_trips() const { return kudu_round_trips_; }
 
  protected:
   /// Write debug string of this into out.
@@ -180,7 +170,47 @@ class KuduScanNode : public ScanNode {
   /// Returns the next partition key range to read. Thread safe. Returns NULL if there are
   /// no more ranges.
   TKuduKeyRange* GetNextKeyRange();
+
+  const std::vector<std::string>& projected_columns() const { return projected_columns_; }
+
+  const TupleDescriptor* tuple_desc() const { return tuple_desc_; }
+
+  // Returns a cloned copy of the scan node's conjuncts. Requires that the expressions
+  // have been open previously.
+  Status GetConjunctCtxs(vector<ExprContext*>* ctxs);
+
+  // Clones the set of predicates to be set on scanners.
+  void ClonePredicates(vector<kudu::client::KuduPredicate*>* predicates);
+
+  RuntimeProfile::Counter* kudu_read_timer() const { return kudu_read_timer_; }
+  RuntimeProfile::Counter* kudu_round_trips() const { return kudu_round_trips_; }
 };
+
+#else // No Kudu
+
+class KuduScanNode : public ScanNode {
+ public:
+  KuduScanNode(ObjectPool *pool, const TPlanNode &tnode, const DescriptorTbl &descs)
+      : ScanNode(pool, tnode, descs) {}
+
+  ~KuduScanNode() {}
+
+  virtual Status Prepare(RuntimeState *state) {
+    return Status(TErrorCode::KUDU_NOT_SUPPORTED_ON_OS);
+  }
+
+  virtual Status Open(RuntimeState *state) {
+    return Status(TErrorCode::KUDU_NOT_SUPPORTED_ON_OS);
+  }
+
+  virtual Status GetNext(RuntimeState *state, RowBatch *row_batch, bool *eos) {
+    return Status(TErrorCode::KUDU_NOT_SUPPORTED_ON_OS);
+  }
+
+  virtual void Close(RuntimeState *state) {}
+};
+
+#endif
 
 }
 

be/src/exec/kudu-scanner.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef USE_KUDU
+
 #include "exec/kudu-scanner.h"
 
 #include <boost/foreach.hpp>
@@ -368,3 +370,5 @@ Status KuduScanner::GetNextBlock() {
 }
 
 }  // namespace impala
+
+#endif

be/src/exec/kudu-table-sink.cc
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+#ifdef USE_KUDU
+
 #include "exec/kudu-table-sink.h"
 
 #include <sstream>
@@ -288,3 +290,5 @@ void KuduTableSink::Close(RuntimeState* state) {
 }
 
 }  // namespace impala
+
+#endif

be/src/exec/kudu-util.h
@@ -15,11 +15,16 @@
 #ifndef IMPALA_UTIL_KUDU_UTIL_H_
 #define IMPALA_UTIL_KUDU_UTIL_H_
 
+#ifdef USE_KUDU
 #include <kudu/client/callbacks.h>
 #include <kudu/client/client.h>
+#endif
+
 #include <boost/unordered_map.hpp>
 
 namespace impala {
+
+#ifdef USE_KUDU
 
 class TExpr;
 class KuduTableDescriptor;
@@ -65,6 +70,12 @@ void LogKuduMessage(kudu::client::KuduLogSeverity severity, const char* filename
       return Status(strings::Substitute("$0: $1", prepend, _s.ToString())); \
     } \
   } while (0)
+
+#else  // No Kudu
+
+void InitKuduLogging() {}
+
+#endif
 
 } /// namespace impala
 #endif