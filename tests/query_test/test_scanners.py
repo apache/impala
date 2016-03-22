a75d7dd282bd6bdd3b0b82d389403a80c6732feb
Skye Wanderman-Milne <skye@cloudera.com>
2016-03-22 16:16:18 -0700

    IMPALA-3081: increase mem limit (again) for TestWideRow
    LZO occasionally needs > 80MB to run this query. We still need to
    investigate why it needs so much memory (the scan alone takes > 70MB),
    but for now bump the mem limit again.
    
    Change-Id: Ifdb7c6d33ab2de0f06e7322aa8f8ba107da84d49
    Reviewed-on: http://gerrit.cloudera.org:8080/2602
    Reviewed-by: Jim Apple <jbapple@cloudera.com>
    Tested-by: Internal Jenkins

--- tests/query_test/test_scanners.py
+++ tests/query_test/test_scanners.py
@@ -142,10 +142,10 @@ class TestWideRow(ImpalaTestSuite):
     # We need > 10 MB of memory because we're creating extra buffers:
     # - 10 MB table / 5 MB scan range = 2 scan ranges, each of which may allocate ~20MB
     # - Sync reads will allocate ~5MB of space
-    # The 80MB value used here was determined empirically by raising the limit until the
+    # The 100MB value used here was determined empirically by raising the limit until the
     # query succeeded for all file formats -- I don't know exactly why we need this much.
     # TODO: figure out exact breakdown of memory usage (IMPALA-681)
-    new_vector.get_value('exec_option')['mem_limit'] = 80 * 1024 * 1024
+    new_vector.get_value('exec_option')['mem_limit'] = 100 * 1024 * 1024
     self.run_test_case('QueryTest/wide-row', new_vector)
 
 class TestWideTable(ImpalaTestSuite):
