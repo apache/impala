95064359cc5948d043e271fbb18621fb1dffc02f
Alex Behm <alex.behm@cloudera.com>
2016-05-16 20:06:13 -0700

    IMPALA-3491: Use unique_database fixture in test_delimited_text.py.
    Testing: Ran the test locally 10 times in a loop on exhaustive.
    
    Change-Id: Idedd5f03984e41a4b3ebf271e50863e980c66cb6
    Reviewed-on: http://gerrit.cloudera.org:8080/3096
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Alex Behm <alex.behm@cloudera.com>

--- tests/query_test/test_delimited_text.py
+++ tests/query_test/test_delimited_text.py
@@ -14,8 +14,6 @@ class TestDelimitedText(ImpalaTestSuite):
   and escape characters.
   """
 
-  TEST_DB_NAME = "delim_text_test_db"
-
   @classmethod
   def get_workload(self):
     return 'functional-query'
@@ -27,26 +25,10 @@ class TestDelimitedText(ImpalaTestSuite):
     # Only run on delimited text with no compression.
     cls.TestMatrix.add_dimension(create_uncompressed_text_dimension(cls.get_workload()))
 
-  def setup_method(self, method):
-    # cleanup and create a fresh test database
-    self.__cleanup()
-    self.execute_query("create database %s" % self.TEST_DB_NAME)
+  def test_delimited_text(self, vector, unique_database):
+    self.run_test_case('QueryTest/delimited-text', vector, unique_database)
 
-  def teardown_method(self, method):
-    self.__cleanup()
-
-  def __cleanup(self):
-    self.client.execute('use default')
-    self.client.execute('drop table if exists %s.cbn' % self.TEST_DB_NAME)
-    self.client.execute('drop table if exists %s.dhp' % self.TEST_DB_NAME)
-    self.client.execute('drop table if exists %s.tecn' % self.TEST_DB_NAME)
-    self.client.execute('drop table if exists %s.nl_queries' % self.TEST_DB_NAME)
-    self.client.execute('drop database if exists %s' % self.TEST_DB_NAME)
-
-  def test_delimited_text(self, vector):
-    self.run_test_case('QueryTest/delimited-text', vector)
-
-  def test_delimited_text_newlines(self, vector):
+  def test_delimited_text_newlines(self, vector, unique_database):
     """ Test text with newlines in strings - IMPALA-1943. Execute queries from Python to
     avoid issues with newline handling in test file format. """
     self.execute_query_expect_success(self.client, """
@@ -56,24 +38,24 @@ class TestDelimitedText(ImpalaTestSuite):
       fields terminated by '\002'
       lines terminated by '\001'
       stored as textfile
-      """ % self.TEST_DB_NAME)
+      """ % unique_database)
     # Create test data with newlines in various places
     self.execute_query_expect_success(self.client, """
       insert into %s.nl_queries
       values ("the\\n","\\nquick\\nbrown","fox\\n"),
-             ("\\njumped","over the lazy\\n","\\ndog")""" % self.TEST_DB_NAME)
-    result = self.execute_query("select * from %s.nl_queries" % self.TEST_DB_NAME)
+             ("\\njumped","over the lazy\\n","\\ndog")""" % unique_database)
+    result = self.execute_query("select * from %s.nl_queries" % unique_database)
     assert len(result.data) == 2
     assert result.data[0].split("\t") == ["the\n", "\nquick\nbrown", "fox\n"]
     assert result.data[1].split("\t") == ["\njumped","over the lazy\n","\ndog"]
     # The row count may be computed without parsing each row, so could be inconsistent.
-    result = self.execute_query("select count(*) from %s.nl_queries" % self.TEST_DB_NAME)
+    result = self.execute_query("select count(*) from %s.nl_queries" % unique_database)
     assert len(result.data) == 1
     assert result.data[0] == "2"
 
-  @pytest.mark.execute_serially
-  def test_delimited_text_latin_chars(self, vector):
+  def test_delimited_text_latin_chars(self, vector, unique_database):
     """Verifies Impala is able to properly handle delimited text that contains
     extended ASCII/latin characters. Marked as running serial because of shared
     cleanup/setup"""
-    self.run_test_case('QueryTest/delimited-latin-text', vector, encoding="latin-1")
+    self.run_test_case('QueryTest/delimited-latin-text', vector, unique_database,
+      encoding="latin-1")
