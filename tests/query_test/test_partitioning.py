8c37bf35438aa827f092d99d6cff83ce1ed83fbb
Alex Behm <alex.behm@cloudera.com>
2016-09-02 08:44:25 -0700

    IMPALA-3491: Use unique database fixture in test_partitioning.py
    Testing: Ran the test locally in a loop on exhaustive. Did a private
    debug/exhaustive/hdfs test run.
    
    Change-Id: Ib1b33d9977a98894288662a711805e9a54329ec8
    Reviewed-on: http://gerrit.cloudera.org:8080/4316
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Internal Jenkins

--- tests/query_test/test_partitioning.py
+++ tests/query_test/test_partitioning.py
@@ -16,7 +16,6 @@
 # under the License.
 
 import pytest
-from subprocess import call
 
 from tests.beeswax.impala_beeswax import ImpalaBeeswaxException
 from tests.common.impala_test_suite import ImpalaTestSuite
@@ -25,8 +24,6 @@ from tests.common.test_dimensions import create_single_exec_option_dimension
 
 # Tests to validate HDFS partitioning.
 class TestPartitioning(ImpalaTestSuite):
-  TEST_DBS = ['hdfs_partitioning', 'bool_partitions']
-
   @classmethod
   def get_workload(self):
     return 'functional-query'
@@ -41,71 +38,53 @@ class TestPartitioning(ImpalaTestSuite):
         v.get_value('table_format').file_format == 'text' and\
         v.get_value('table_format').compression_codec == 'none')
 
-  @classmethod
-  def setup_class(cls):
-    super(TestPartitioning, cls).setup_class()
-    map(cls.cleanup_db, cls.TEST_DBS)
-    cls.filesystem_client.delete_file_dir("test-warehouse/all_insert_partition_col_types/",\
-        recursive=True)
-
-  @classmethod
-  def teardown_class(cls):
-    map(cls.cleanup_db, cls.TEST_DBS)
-    super(TestPartitioning, cls).teardown_class()
-
   @SkipIfLocal.root_path
-  @pytest.mark.execute_serially
-  def test_partition_col_types(self, vector):
-    self.execute_query("create database hdfs_partitioning");
+  def test_partition_col_types(self, vector, unique_database):
     self.run_test_case('QueryTest/partition-col-types', vector,
-        use_db='hdfs_partitioning')
+        use_db=unique_database)
 
   # Missing Coverage: Impala deals with boolean partitions created by Hive on a non-hdfs
   # filesystem.
   @SkipIfS3.hive
   @SkipIfIsilon.hive
   @SkipIfLocal.hive
-  @pytest.mark.execute_serially
-  def test_boolean_partitions(self, vector):
+  def test_boolean_partitions(self, vector, unique_database):
     # This test takes about a minute to complete due to the Hive commands that are
     # executed. To cut down on runtime, limit the test to exhaustive exploration
     # strategy.
     if self.exploration_strategy() != 'exhaustive': pytest.skip()
 
-    db_name = 'bool_partitions'
-    tbl_name = 'tbl'
-    self.execute_query("create database " + db_name)
-    self.execute_query("use " + db_name)
+    full_name = unique_database + ".bool_test"
 
-    self.execute_query("create table %s (i int) partitioned by (b boolean)" % tbl_name)
+    self.execute_query("create table %s (i int) partitioned by (b boolean)" % full_name)
 
     # Insert some data using Hive. Due to HIVE-6590, Hive may create multiple
     # partitions, mapping to the same boolean literal value.
     # For example, Hive may create partitions: /b=FALSE and /b=false, etc
-    call(["hive", "-e", "INSERT OVERWRITE TABLE %s.%s PARTITION(b=false) SELECT 1 from "\
-        "functional.alltypes limit 1" % (db_name, tbl_name)])
-    call(["hive", "-e", "INSERT OVERWRITE TABLE %s.%s PARTITION(b=FALSE) SELECT 2 from "\
-        "functional.alltypes limit 1" % (db_name, tbl_name)])
-    call(["hive", "-e", "INSERT OVERWRITE TABLE %s.%s PARTITION(b=true) SELECT 10 from "\
-        "functional.alltypes limit 1" % (db_name, tbl_name)])
+    self.run_stmt_in_hive("INSERT INTO TABLE %s PARTITION(b=false) SELECT 1 from "\
+        "functional.alltypes limit 1" % full_name)
+    self.run_stmt_in_hive("INSERT INTO TABLE %s PARTITION(b=FALSE) SELECT 2 from "\
+        "functional.alltypes limit 1" % full_name)
+    self.run_stmt_in_hive("INSERT INTO TABLE %s PARTITION(b=true) SELECT 10 from "\
+        "functional.alltypes limit 1" % full_name)
 
     # Update the Impala metadata
-    self.execute_query("invalidate metadata " + tbl_name)
+    self.execute_query("invalidate metadata " + full_name)
 
     # List the partitions. Show table stats returns 1 row for each partition + 1 summary
     # row
-    result = self.execute_query("show table stats %s" % tbl_name)
+    result = self.execute_query("show table stats %s" % full_name)
     assert len(result.data) == 3 + 1
 
     # Verify Impala properly merges the results of the bad Hive metadata.
-    assert '13' == self.execute_scalar("select sum(i) from %s" % tbl_name);
-    assert '10' == self.execute_scalar("select sum(i) from %s where b=true" % tbl_name)
-    assert '3' == self.execute_scalar("select sum(i) from %s where b=false" % tbl_name)
+    assert '13' == self.execute_scalar("select sum(i) from %s" % full_name);
+    assert '10' == self.execute_scalar("select sum(i) from %s where b=true" % full_name)
+    assert '3' == self.execute_scalar("select sum(i) from %s where b=false" % full_name)
 
     # INSERT into a boolean column is disabled in Impala due to this Hive bug.
     try:
-      self.execute_query("insert into %s partition(bool_col=true) select 1" % tbl_name)
+      self.execute_query("insert into %s partition(bool_col=true) select 1" % full_name)
     except ImpalaBeeswaxException, e:
       assert 'AnalysisException: INSERT into table with BOOLEAN partition column (%s) '\
-          'is not supported: %s.%s' % ('b', db_name, tbl_name) in str(e)
+          'is not supported: %s' % ('b', full_name) in str(e)
 
