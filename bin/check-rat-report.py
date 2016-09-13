aa28e37eb7df9a53dbcf7aabe910095681dd3a51
Jim Apple <jbapple@cloudera.com>
2016-09-13 14:47:57 -0700

    IMPALA-4110: Apache RAT script on Impala tarballs.
    Apache RAT is a tool for license auditing. It will be used as part of
    the Apache release process. This patch includes a script for parsing
    its output and a file containing a list of filename globs that should
    be ignored. The script takes two command line parameters as input -
    the filename of the ignored file globs, and the filename of the RAT
    xml output.
    
    Change-Id: Ic95bd38fbb90f9a901602dd91cee541b16bf4714
    Reviewed-on: http://gerrit.cloudera.org:8080/4405
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Jim Apple <jbapple@cloudera.com>

---
+++ bin/check-rat-report.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# Apache RAT is a tool for checking license compliance. This is a script that uses Apache
+# RAT to check licenses in Impala.
+#
+# It takes as command line parameters two file names - the first is the name of a file
+# containing globs of files to ignore, and the second is the XML output of RAT.
+#
+# I tested this with
+#
+#    pushd "${IMPALA_HOME}"
+#    export SANDBOX=$(mktemp -d) # Just a place to put files for testing
+#    echo "${SANDBOX}"
+#    git archive -o "${SANDBOX}/test-impala.tar.gz" HEAD # Make the tarball to check
+#    java -jar ~/Downloads/apache-rat-0.12/apache-rat-0.12.jar -x \
+#        "${SANDBOX}/test-impala.tar.gz" >"${SANDBOX}/rat.xml"
+#    bin/check-rat-report.py bin/rat_exclude_files.txt "${SANDBOX}/rat.xml"
+#
+# This is copied from a similar file in Apache Kudu. Only RAT 0.12 is supported at this
+# time.
+
+import fnmatch
+import re
+import sys
+import xml.etree.ElementTree as ET
+
+if len(sys.argv) != 3:
+  sys.stderr.write("Usage: %s exclude_globs.lst rat_report.xml\n" % (sys.argv[0],))
+  sys.exit(1)
+
+exclude_globs_filename = sys.argv[1]
+xml_filename = sys.argv[2]
+
+globs = [line.strip() for line in open(exclude_globs_filename, "r") if "# " != line[0:2]]
+
+tree = ET.parse(xml_filename)
+root = tree.getroot()
+all_ok = True
+
+resources = root.findall('resource')
+for r in resources:
+  approvals = r.findall('license-approval')
+  if approvals and approvals[0].attrib['name'] == 'true':
+    continue
+  clean_name = re.sub('^[^/]+/', '', r.attrib['name'])
+  excluded = False
+  for g in globs:
+    if fnmatch.fnmatch(clean_name, g):
+      excluded = True
+      break
+  if not excluded:
+    typename = r.findall('type')[0].attrib['name']
+    if not (clean_name[0:9] == 'testdata/' and typename in ['archive', 'binary']):
+      sys.stderr.write(
+          "%s: %s\n" %
+          ('UNAPPROVED' if approvals else "NO APPROVALS; " + typename, clean_name))
+      all_ok = False
+
+if not all_ok:
+  sys.exit(1)
+
+print 'OK'
+sys.exit(0)
