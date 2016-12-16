b3636c97d4b872e1640955974409c57459d655e0
Amos Bird <amosbird@gmail.com>
2016-12-16 14:23:22 +0800

    IMPALA-4033: Treat string-partition key values as case sensitive.
    This commit makes ADD PARTITION operations treat string partition-key
    values as case sensitive in consistent with other related partition DDL
    operations.
    
    Change-Id: I6fbe67d99df8a50a16a18456fde85d03d622c7a1
    Reviewed-on: http://gerrit.cloudera.org:8080/5535
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Internal Jenkins

--- fe/src/main/java/org/apache/impala/catalog/HdfsTable.java
+++ fe/src/main/java/org/apache/impala/catalog/HdfsTable.java
@@ -570,7 +570,7 @@ public class HdfsTable extends Table {
     for (FieldSchema fs: getMetaStoreTable().getPartitionKeys()) {
       for (TPartitionKeyValue kv: partitionSpec) {
         if (fs.getName().toLowerCase().equals(kv.getName().toLowerCase())) {
-          targetValues.add(kv.getValue().toLowerCase());
+          targetValues.add(kv.getValue());
           // Same key was specified twice
           if (!keys.add(kv.getName().toLowerCase())) {
             return null;
@@ -604,7 +604,7 @@ public class HdfsTable extends Table {
           // backwards compatibility with Hive, and is clearly broken.
           if (value.isEmpty()) value = getNullPartitionKeyValue();
         }
-        if (!targetValues.get(i).equals(value.toLowerCase())) {
+        if (!targetValues.get(i).equals(value)) {
           matchFound = false;
           break;
         }
