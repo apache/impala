6b90aa3a115f8274a5380945bc4e2b940ef4f711
Tim Armstrong <tarmstrong@cloudera.com>
2016-12-16 11:10:09 -0800

    IMPALA-4653: fix sticky config variable problem
    Previously we could get a developer's shell into a bad state where a
    value of a config variable from a previous impala-config.sh version
    would override the value from the new impala-config.sh version.
    
    This change adds a new mechanism to override settings locally by adding
    settings to impala-config-local.sh. This alternative approach is more
    robust, because the config variables will be reset to the intended
    values when impala-config.sh is re-sourced.
    
    impala-config-branch.sh can also be used to override settings in a
    version-controlled way, e.g. to support having different settings for
    different branches.
    
    I did not convert all variables to use this approach, since many people
    and Jenkins jobs depend on setting these variables from the environment.
    The remaining "sticky" variables are ones where default values should
    not change frequently, e.g. source directory locations and build
    settings.
    
    Change-Id: I930e2ca825142428d17a6981c77534ab0c8e3489
    Reviewed-on: http://gerrit.cloudera.org:8080/5545
    Reviewed-by: Matthew Jacobs <mj@cloudera.com>
    Tested-by: Impala Public Jenkins

---
+++ bin/impala-config-branch.sh
@@ -0,0 +1,24 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# Variables in the file override the default values from impala-config.sh.
+# Config changes for release or features branches should go here so they
+# can be version controlled but not conflict with changes on the master
+# branch.
+#
+# E.g. to override IMPALA_HADOOP_VERSION, you could uncomment this line:
+# IMPALA_HADOOP_VERSION=3.0.0
