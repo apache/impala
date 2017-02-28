60c1c6e81b8705f493c6f9844e4c9a0aeb813721
Dimitris Tsirogiannis <dtsirogiannis@cloudera.com>
2017-02-28 13:33:11 -0800

    IMPALA-4966: Add flatbuffers to build
    FlatBuffers version 1.6.0 is already included in the toolchain. This
    commit adds it to the build system.
    
    Change-Id: I2ca255ddf08ac846b454bfa1470ed67b1338d2b0
    Reviewed-on: http://gerrit.cloudera.org:8080/6180
    Reviewed-by: Dimitris Tsirogiannis <dtsirogiannis@cloudera.com>
    Tested-by: Impala Public Jenkins

--- bin/bootstrap_toolchain.py
+++ bin/bootstrap_toolchain.py
@@ -349,10 +349,10 @@ if __name__ == "__main__":
   if not os.path.exists(toolchain_root):
     os.makedirs(toolchain_root)
 
-  packages = ["avro", "binutils", "boost", "breakpad", "bzip2", "cmake", "crcutil", "gcc",
-      "gflags", "glog", "gperftools", "gtest", "kudu", "libev", "llvm",
-      ("llvm", "3.8.0-asserts-p1"), "lz4", "openldap", "protobuf", "rapidjson", "re2",
-      "snappy", "thrift", "tpc-h", "tpc-ds", "zlib"]
+  packages = ["avro", "binutils", "boost", "breakpad", "bzip2", "cmake", "crcutil",
+      "flatbuffers", "gcc", "gflags", "glog", "gperftools", "gtest", "kudu", "libev",
+      "llvm", ("llvm", "3.8.0-asserts-p1"), "lz4", "openldap", "protobuf", "rapidjson",
+      "re2", "snappy", "thrift", "tpc-h", "tpc-ds", "zlib"]
   bootstrap(toolchain_root, packages)
 
   # Download the CDH components if necessary.
