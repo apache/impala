244ec22d8e8b2f108f723d49dc95a016a74d6d6f
Joe McDonnell <joemcdonnell@cloudera.com>
2017-03-09 09:12:12 -0800

    IMPALA-5055: Fix DCHECK in parquet-column-readers.cc ReadPageHeader()
    GetBytes only sets status in the case of an error. This means that
    ReadPageHeader needs to initialize the status variable so that the
    status.ok() check is accurate after the GetBytes call.
    
    I verified that the other uses of status are ok. Most do not check
    status.ok() directly, but rely on the return value of the function
    setting status.
    
    Change-Id: Ie22a8cf6b53f507c378c2efe302482409935184e
    Reviewed-on: http://gerrit.cloudera.org:8080/6328
    Reviewed-by: Marcel Kornacker <marcel@cloudera.com>
    Tested-by: Impala Public Jenkins

--- be/src/exec/parquet-column-readers.cc
+++ be/src/exec/parquet-column-readers.cc
@@ -798,6 +798,7 @@ Status BaseScalarColumnReader::ReadPageHeader(bool peek,
 
     // Didn't read entire header, increase buffer size and try again
     int64_t new_buffer_size = max<int64_t>(buffer_size * 2, 1024);
+    status = Status::OK();
     bool success = stream_->GetBytes(
         new_buffer_size, &buffer, &new_buffer_size, &status, /* peek */ true);
     if (!success) {
