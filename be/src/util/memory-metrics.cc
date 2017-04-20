apache/impala

commit 955b257cfb5f94775318c466ee18771ff167026d

IMPALA-5073: Part 1: add option to use mmap() for buffer pool
Support allocating with mmap instead of TCMalloc to give more control
over memory usage. Also tell Linux to back larger buffers with huge
pages when possible to reduce TLB pressure. The main complication is
that memory returned by mmap() is not necessarily aligned to a huge
page boundary, so we need to "fix up" the mapping ourselves.

Adds additional memory metrics, since we previously relied on the
assumption that all memory was allocated through TCMalloc.
memory.total-used tracks the total across the buffer pool and
TCMalloc. When the buffer pool is not present, they just report
the TCMalloc values.

This can be enabled with the --mmap_buffers flag. The transparent
huge pages support can be disabled with the --madvise_huge_pages
startup flag.

At some point this should become the default, but it requires
more work to validate perf and resource used (virtual address
space, etc).

Testing:
Added some unit tests to test edge cases and the different supported
flags. Many pre-existing tests also exercise the modified code.

Change-Id: Ifbc748f74adcbbdcfa45f3ec7df98284925acbd6
Reviewed-on: http://gerrit.cloudera.org:8080/6474
Reviewed-by: Tim Armstrong <tarmstrong@cloudera.com>
Tested-by: Impala Public Jenkins

be/src/util/memory-metrics.cc
@@ -20,6 +20,8 @@
 #include <boost/algorithm/string.hpp>
 #include <gutil/strings/substitute.h>
 
+#include "runtime/bufferpool/buffer-pool.h"
+#include "runtime/bufferpool/reservation-tracker.h"
 #include "util/jni-util.h"
 #include "util/time.h"
 
@@ -27,25 +29,40 @@ using boost::algorithm::to_lower;
 using namespace impala;
 using namespace strings;
 
+DECLARE_bool(mmap_buffers);
+
+SumGauge<uint64_t>* AggregateMemoryMetric::TOTAL_USED = nullptr;
+
 TcmallocMetric* TcmallocMetric::BYTES_IN_USE = NULL;
 TcmallocMetric* TcmallocMetric::PAGEHEAP_FREE_BYTES = NULL;
 TcmallocMetric* TcmallocMetric::TOTAL_BYTES_RESERVED = NULL;
 TcmallocMetric* TcmallocMetric::PAGEHEAP_UNMAPPED_BYTES = NULL;
 TcmallocMetric::PhysicalBytesMetric* TcmallocMetric::PHYSICAL_BYTES_RESERVED = NULL;
 
-TcmallocMetric* TcmallocMetric::CreateAndRegister(MetricGroup* metrics, const string& key,
-  const string& tcmalloc_var) {
-    return metrics->RegisterMetric(
-        new TcmallocMetric(MetricDefs::Get(key), tcmalloc_var));
+BufferPoolMetric* BufferPoolMetric::LIMIT = nullptr;
+BufferPoolMetric* BufferPoolMetric::SYSTEM_ALLOCATED = nullptr;
+BufferPoolMetric* BufferPoolMetric::RESERVED = nullptr;
+
+TcmallocMetric* TcmallocMetric::CreateAndRegister(
+    MetricGroup* metrics, const string& key, const string& tcmalloc_var) {
+  return metrics->RegisterMetric(new TcmallocMetric(MetricDefs::Get(key), tcmalloc_var));
 }
 
-Status impala::RegisterMemoryMetrics(MetricGroup* metrics, bool register_jvm_metrics) {
+Status impala::RegisterMemoryMetrics(MetricGroup* metrics, bool register_jvm_metrics,
+    ReservationTracker* global_reservations, BufferPool* buffer_pool) {
+  if (global_reservations != nullptr) {
+    DCHECK(buffer_pool != nullptr);
+    RETURN_IF_ERROR(
+        BufferPoolMetric::InitMetrics(metrics, global_reservations, buffer_pool));
+  }
 #ifndef ADDRESS_SANITIZER
-  TcmallocMetric::BYTES_IN_USE = TcmallocMetric::CreateAndRegister(metrics,
-      "tcmalloc.bytes-in-use", "generic.current_allocated_bytes");
+  // We rely on TCMalloc for our global memory metrics, so skip setting them up
+  // if we're not using TCMalloc.
+  TcmallocMetric::BYTES_IN_USE = TcmallocMetric::CreateAndRegister(
+      metrics, "tcmalloc.bytes-in-use", "generic.current_allocated_bytes");
 
-  TcmallocMetric::TOTAL_BYTES_RESERVED = TcmallocMetric::CreateAndRegister(metrics,
-      "tcmalloc.total-bytes-reserved", "generic.heap_size");
+  TcmallocMetric::TOTAL_BYTES_RESERVED = TcmallocMetric::CreateAndRegister(
+      metrics, "tcmalloc.total-bytes-reserved", "generic.heap_size");
 
   TcmallocMetric::PAGEHEAP_FREE_BYTES = TcmallocMetric::CreateAndRegister(metrics,
       "tcmalloc.pageheap-free-bytes", "tcmalloc.pageheap_free_bytes");
@@ -53,11 +70,22 @@ Status impala::RegisterMemoryMetrics(MetricGroup* metrics, bool register_jvm_met
   TcmallocMetric::PAGEHEAP_UNMAPPED_BYTES = TcmallocMetric::CreateAndRegister(metrics,
       "tcmalloc.pageheap-unmapped-bytes", "tcmalloc.pageheap_unmapped_bytes");
 
-  TcmallocMetric::PHYSICAL_BYTES_RESERVED = metrics->RegisterMetric(
-      new TcmallocMetric::PhysicalBytesMetric(
+  TcmallocMetric::PHYSICAL_BYTES_RESERVED =
+      metrics->RegisterMetric(new TcmallocMetric::PhysicalBytesMetric(
           MetricDefs::Get("tcmalloc.physical-bytes-reserved")));
 #endif
 
+  // Add compound metrics that track totals across TCMalloc and the buffer pool.
+  // total-used should track the total physical memory in use.
+  vector<UIntGauge*> used_metrics{TcmallocMetric::PHYSICAL_BYTES_RESERVED};
+  if (FLAGS_mmap_buffers && global_reservations != nullptr) {
+    // If we mmap() buffers, the buffers are not allocated via TCMalloc. Ensure they are
+    // properly tracked.
+    used_metrics.push_back(BufferPoolMetric::SYSTEM_ALLOCATED);
+  }
+
+  AggregateMemoryMetric::TOTAL_USED = metrics->RegisterMetric(
+      new SumGauge<uint64_t>(MetricDefs::Get("memory.total-used"), used_metrics));
   if (register_jvm_metrics) {
     RETURN_IF_ERROR(JvmMetric::InitMetrics(metrics->GetOrCreateChildGroup("jvm")));
   }
@@ -131,6 +159,44 @@ void JvmMetric::CalculateValue() {
       return;
     case PEAK_COMMITTED: value_ = pool.peak_committed;
       return;
-    default: DCHECK(false) << "Unknown JvmMetricType: " << metric_type_;
+    default:
+      DCHECK(false) << "Unknown JvmMetricType: " << metric_type_;
+  }
+}
+
+Status BufferPoolMetric::InitMetrics(MetricGroup* metrics,
+    ReservationTracker* global_reservations, BufferPool* buffer_pool) {
+  LIMIT = metrics->RegisterMetric(
+      new BufferPoolMetric(MetricDefs::Get("buffer-pool.limit"),
+          BufferPoolMetricType::LIMIT, global_reservations, buffer_pool));
+  SYSTEM_ALLOCATED = metrics->RegisterMetric(
+      new BufferPoolMetric(MetricDefs::Get("buffer-pool.system-allocated"),
+          BufferPoolMetricType::SYSTEM_ALLOCATED, global_reservations, buffer_pool));
+  RESERVED = metrics->RegisterMetric(
+      new BufferPoolMetric(MetricDefs::Get("buffer-pool.reserved"),
+          BufferPoolMetricType::RESERVED, global_reservations, buffer_pool));
+  return Status::OK();
+}
+
+BufferPoolMetric::BufferPoolMetric(const TMetricDef& def, BufferPoolMetricType type,
+    ReservationTracker* global_reservations, BufferPool* buffer_pool)
+  : UIntGauge(def, 0),
+    type_(type),
+    global_reservations_(global_reservations),
+    buffer_pool_(buffer_pool) {}
+
+void BufferPoolMetric::CalculateValue() {
+  switch (type_) {
+    case BufferPoolMetricType::LIMIT:
+      value_ = buffer_pool_->GetSystemBytesLimit();
+      break;
+    case BufferPoolMetricType::SYSTEM_ALLOCATED:
+      value_ = buffer_pool_->GetSystemBytesAllocated();
+      break;
+    case BufferPoolMetricType::RESERVED:
+      value_ = global_reservations_->GetReservation();
+      break;
+    default:
+      DCHECK(false) << "Unknown BufferPoolMetricType: " << static_cast<int>(type_);
   }
 }

be/src/util/memory-metrics.h
@@ -29,7 +29,19 @@
 
 namespace impala {
 
+class BufferPool;
+class ReservationTracker;
 class Thread;
+
+/// Memory metrics including TCMalloc and BufferPool memory.
+class AggregateMemoryMetric {
+ public:
+  /// The sum of Tcmalloc TOTAL_BYTES_RESERVED and BufferPool SYSTEM_ALLOCATED.
+  /// Approximates the total amount of physical memory consumed by the backend (i.e. not
+  /// including JVM memory), which is either in use by queries or cached by the BufferPool
+  /// or TcMalloc.
+  static SumGauge<uint64_t>* TOTAL_USED;
+};
 
 /// Specialised metric which exposes numeric properties from tcmalloc.
 class TcmallocMetric : public UIntGauge {
@@ -128,10 +140,44 @@ class JvmMetric : public IntGauge {
   JvmMetricType metric_type_;
 };
 
-/// Registers common tcmalloc memory metrics. If register_jvm_metrics is true, the JVM
-/// memory metrics are also registered.
-Status RegisterMemoryMetrics(MetricGroup* metrics, bool register_jvm_metrics);
+/// Metric that reports information about the buffer pool.
+class BufferPoolMetric : public UIntGauge {
+ public:
+  static Status InitMetrics(MetricGroup* metrics, ReservationTracker* global_reservations,
+      BufferPool* buffer_pool);
 
+  /// Global metrics, initialized by CreateAndRegisterMetrics().
+  static BufferPoolMetric* LIMIT;
+  static BufferPoolMetric* SYSTEM_ALLOCATED;
+  static BufferPoolMetric* RESERVED;
+
+ protected:
+  virtual void CalculateValue();
+
+ private:
+  enum class BufferPoolMetricType {
+    LIMIT, // Limit on memory allocated to buffers.
+    // Total amount of buffer memory allocated from the system. Always <= LIMIT.
+    SYSTEM_ALLOCATED,
+    // Total of all buffer reservations. May be < SYSTEM_ALLOCATED if not all reservations
+    // are fulfilled, or > SYSTEM_ALLOCATED because of additional memory cached by
+    // BufferPool. Always <= LIMIT.
+    RESERVED,
+  };
+
+  BufferPoolMetric(const TMetricDef& def, BufferPoolMetricType type,
+      ReservationTracker* global_reservations, BufferPool* buffer_pool);
+
+  BufferPoolMetricType type_;
+  ReservationTracker* global_reservations_;
+  BufferPool* buffer_pool_;
+};
+
+/// Registers common tcmalloc memory metrics. If 'register_jvm_metrics' is true, the JVM
+/// memory metrics are also registered. If 'global_reservations' and 'buffer_pool' are
+/// not NULL, also register buffer pool metrics.
+Status RegisterMemoryMetrics(MetricGroup* metrics, bool register_jvm_metrics,
+    ReservationTracker* global_reservations, BufferPool* buffer_pool);
 }
 
 #endif

be/src/catalog/catalogd-main.cc
@@ -69,8 +69,8 @@ int CatalogdMain(int argc, char** argv) {
     LOG(INFO) << "Not starting webserver";
   }
 
-  metrics->Init(FLAGS_enable_webserver ? webserver.get() : NULL);
-  ABORT_IF_ERROR(RegisterMemoryMetrics(metrics.get(), true));
+  metrics->Init(FLAGS_enable_webserver ? webserver.get() : nullptr);
+  ABORT_IF_ERROR(RegisterMemoryMetrics(metrics.get(), true, nullptr, nullptr));
   StartThreadInstrumentation(metrics.get(), webserver.get(), true);
 
   InitRpcEventTracing(webserver.get());

be/src/runtime/bufferpool/buffer-allocator-test.cc
@@ -21,13 +21,20 @@
 #include "runtime/bufferpool/buffer-allocator.h"
 #include "runtime/bufferpool/buffer-pool-internal.h"
 #include "runtime/bufferpool/buffer-pool.h"
+#include "runtime/bufferpool/system-allocator.h"
 #include "testutil/cpu-util.h"
 #include "testutil/gtest-util.h"
 #include "util/cpu-info.h"
 
 #include "common/names.h"
 
+DECLARE_bool(mmap_buffers);
+DECLARE_bool(madvise_huge_pages);
+
 namespace impala {
+
+using BufferAllocator = BufferPool::BufferAllocator;
+using BufferHandle = BufferPool::BufferHandle;
 
 class BufferAllocatorTest : public ::testing::Test {
  public:
@@ -45,7 +52,7 @@ class BufferAllocatorTest : public ::testing::Test {
     CpuTestUtil::ResetAffinity(); // Some tests modify affinity.
   }
 
-  int GetFreeListSize(BufferPool::BufferAllocator* allocator, int core, int64_t len) {
+  int GetFreeListSize(BufferAllocator* allocator, int core, int64_t len) {
     return allocator->GetFreeListSize(core, len);
   }
 
@@ -70,7 +77,7 @@ TEST_F(BufferAllocatorTest, FreeListSizes) {
   const int NUM_BUFFERS = 512;
   const int64_t TOTAL_BYTES = NUM_BUFFERS * TEST_BUFFER_LEN;
 
-  BufferPool::BufferAllocator allocator(dummy_pool_, TEST_BUFFER_LEN, TOTAL_BYTES);
+  BufferAllocator allocator(dummy_pool_, TEST_BUFFER_LEN, TOTAL_BYTES);
 
   // Allocate a bunch of buffers - all free list checks should miss.
   vector<BufferHandle> buffers(NUM_BUFFERS);
@@ -121,5 +128,69 @@ TEST_F(BufferAllocatorTest, FreeListSizes) {
   allocator.ReleaseMemory(TOTAL_BYTES);
   ASSERT_EQ(0, GetFreeListSize(&allocator, CORE, TEST_BUFFER_LEN));
 }
+
+class SystemAllocatorTest : public ::testing::Test {
+ public:
+  virtual void SetUp() {}
+
+  virtual void TearDown() {}
+
+  static const int64_t MIN_BUFFER_LEN = 4 * 1024;
+  static const int64_t MAX_BUFFER_LEN = 1024 * 1024 * 1024;
+};
+
+/// Basic test that checks that we can allocate buffers of the expected power-of-two
+/// sizes.
+TEST_F(SystemAllocatorTest, BasicPowersOfTwo) {
+  SystemAllocator allocator(MIN_BUFFER_LEN);
+
+  // Iterate a few times to make sure we can reallocate.
+  for (int iter = 0; iter < 5; ++iter) {
+    // Allocate buffers of a mix of sizes.
+    vector<BufferHandle> buffers;
+    for (int alloc_iter = 0; alloc_iter < 2; ++alloc_iter) {
+      for (int64_t len = MIN_BUFFER_LEN; len <= MAX_BUFFER_LEN; len *= 2) {
+        BufferHandle buffer;
+        ASSERT_OK(allocator.Allocate(len, &buffer));
+        ASSERT_TRUE(buffer.is_open());
+        // Write a few bytes to the buffer to check it's valid memory.
+        buffer.data()[0] = 0;
+        buffer.data()[buffer.len() / 2] = 0;
+        buffer.data()[buffer.len() - 1] = 0;
+        buffers.push_back(move(buffer));
+      }
+    }
+
+    // Free all the buffers.
+    for (BufferHandle& buffer : buffers) allocator.Free(move(buffer));
+  }
 }
-IMPALA_TEST_MAIN();
+
+/// Make an absurdly large allocation to test the failure path.
+TEST_F(SystemAllocatorTest, LargeAllocFailure) {
+  SystemAllocator allocator(MIN_BUFFER_LEN);
+  BufferHandle buffer;
+  Status status = allocator.Allocate(1LL << 48, &buffer);
+  EXPECT_FALSE(status.ok());
+  EXPECT_EQ(status.msg().error(), TErrorCode::BUFFER_ALLOCATION_FAILED);
+}
+}
+
+int main(int argc, char** argv) {
+  ::testing::InitGoogleTest(&argc, argv);
+  impala::InitCommonRuntime(argc, argv, true, impala::TestInfo::BE_TEST);
+  int result = 0;
+  for (bool mmap : {false, true}) {
+    for (bool madvise : {false, true}) {
+      if (madvise && !mmap) continue; // Not an interesting combination.
+      std::cerr << "+==================================================" << std::endl
+                << "| Running tests with mmap=" << mmap << " madvise=" << madvise
+                << std::endl
+                << "+==================================================" << std::endl;
+      FLAGS_mmap_buffers = mmap;
+      FLAGS_madvise_huge_pages = madvise;
+      if (RUN_ALL_TESTS() != 0) result = 1;
+    }
+  }
+  return result;
+}

be/src/runtime/bufferpool/buffer-allocator.h
@@ -122,6 +122,8 @@ class BufferPool::BufferAllocator {
   /// Try to release at least 'bytes_to_free' bytes of memory to the system allocator.
   void ReleaseMemory(int64_t bytes_to_free);
 
+  int64_t system_bytes_limit() const { return system_bytes_limit_; }
+
   /// Return the amount of memory currently allocated from the system.
   int64_t GetSystemBytesAllocated() const {
     return system_bytes_limit_ - system_bytes_remaining_.Load();

be/src/runtime/bufferpool/buffer-pool.cc
@@ -240,6 +240,14 @@ void BufferPool::ReleaseMemory(int64_t bytes_to_free) {
   allocator_->ReleaseMemory(bytes_to_free);
 }
 
+int64_t BufferPool::GetSystemBytesLimit() const {
+  return allocator_->system_bytes_limit();
+}
+
+int64_t BufferPool::GetSystemBytesAllocated() const {
+  return allocator_->GetSystemBytesAllocated();
+}
+
 bool BufferPool::ClientHandle::IncreaseReservation(int64_t bytes) {
   return impl_->reservation()->IncreaseReservation(bytes);
 }

be/src/runtime/bufferpool/buffer-pool.h
@@ -252,6 +252,8 @@ class BufferPool : public CacheLineAligned {
   std::string DebugString();
 
   int64_t min_buffer_len() const { return min_buffer_len_; }
+  int64_t GetSystemBytesLimit() const;
+  int64_t GetSystemBytesAllocated() const;
 
   /// Generous upper bounds on page and buffer size and the number of different
   /// power-of-two buffer sizes.

be/src/runtime/bufferpool/system-allocator.cc
@@ -17,9 +17,25 @@
 
 #include "runtime/bufferpool/system-allocator.h"
 
+#include <sys/mman.h>
+
 #include "util/bit-util.h"
 
+// TODO: IMPALA-5073: this should eventually become the default once we are confident
+// that it is superior to allocating via TCMalloc.
+DEFINE_bool(mmap_buffers, false,
+    "(Advanced) If true, allocate buffers directly from the operating system instead of "
+    "with TCMalloc.");
+
+DEFINE_bool(madvise_huge_pages, true,
+    "(Advanced) If true and --mmap_buffers is also "
+    "true, advise operating system to back large memory buffers with huge pages");
+
 namespace impala {
+
+/// This is the huge page size on x86-64. We could parse /proc/meminfo to programmatically
+/// get this, but it is unlikely to change unless we port to a different architecture.
+static int64_t HUGE_PAGE_SIZE = 2LL * 1024 * 1024;
 
 SystemAllocator::SystemAllocator(int64_t min_buffer_len)
   : min_buffer_len_(min_buffer_len) {
@@ -31,14 +47,73 @@ Status SystemAllocator::Allocate(int64_t len, BufferPool::BufferHandle* buffer) 
   DCHECK_LE(len, BufferPool::MAX_BUFFER_BYTES);
   DCHECK(BitUtil::IsPowerOf2(len)) << len;
 
-  uint8_t* alloc = reinterpret_cast<uint8_t*>(malloc(len));
-  if (alloc == NULL) return Status(TErrorCode::BUFFER_ALLOCATION_FAILED, len);
-  buffer->Open(alloc, len, CpuInfo::GetCurrentCore());
+  uint8_t* buffer_mem;
+  if (FLAGS_mmap_buffers) {
+    RETURN_IF_ERROR(AllocateViaMMap(len, &buffer_mem));
+  } else {
+    // AddressSanitizer does not instrument mmap(). Use malloc() to preserve
+    // instrumentation.
+    buffer_mem = reinterpret_cast<uint8_t*>(malloc(len));
+    if (buffer_mem == nullptr) {
+      return Status(
+          TErrorCode::BUFFER_ALLOCATION_FAILED, len, "malloc() failed under asan");
+    }
+  }
+  buffer->Open(buffer_mem, len, CpuInfo::GetCurrentCore());
+  return Status::OK();
+}
+
+Status SystemAllocator::AllocateViaMMap(int64_t len, uint8_t** buffer_mem) {
+  int64_t map_len = len;
+  bool use_huge_pages = len % HUGE_PAGE_SIZE == 0 && FLAGS_madvise_huge_pages;
+  if (use_huge_pages) {
+    // Map an extra huge page so we can fix up the alignment if needed.
+    map_len += HUGE_PAGE_SIZE;
+  }
+  uint8_t* mem = reinterpret_cast<uint8_t*>(
+      mmap(nullptr, map_len, PROT_READ | PROT_WRITE, MAP_ANONYMOUS | MAP_PRIVATE, -1, 0));
+  if (mem == MAP_FAILED) {
+    const char* error = strerror(errno);
+    return Status(TErrorCode::BUFFER_ALLOCATION_FAILED, len, error);
+  }
+
+  if (use_huge_pages) {
+    // mmap() may return memory that is not aligned to the huge page size. For the
+    // subsequent madvise() call to work well, we need to align it ourselves and
+    // unmap the memory on either side of the buffer that we don't need.
+    uintptr_t misalignment = reinterpret_cast<uintptr_t>(mem) % HUGE_PAGE_SIZE;
+    if (misalignment != 0) {
+      uintptr_t fixup = HUGE_PAGE_SIZE - misalignment;
+      munmap(mem, fixup);
+      mem += fixup;
+      map_len -= fixup;
+    }
+    munmap(mem + len, map_len - len);
+    DCHECK_EQ(reinterpret_cast<uintptr_t>(mem) % HUGE_PAGE_SIZE, 0) << mem;
+    // Mark the buffer as a candidate for promotion to huge pages. The Linux Transparent
+    // Huge Pages implementation will try to back the memory with a huge page if it is
+    // enabled. MADV_HUGEPAGE was introduced in 2.6.38, so we similarly need to skip this
+    // code if we are compiling against an older kernel.
+#ifdef MADV_HUGEPAGE
+    int rc;
+    // According to madvise() docs it may return EAGAIN to signal that we should retry.
+    do {
+      rc = madvise(mem, len, MADV_HUGEPAGE);
+    } while (rc == -1 && errno == EAGAIN);
+    DCHECK(rc == 0) << "madvise(MADV_HUGEPAGE) shouldn't fail" << errno;
+#endif
+  }
+  *buffer_mem = mem;
   return Status::OK();
 }
 
 void SystemAllocator::Free(BufferPool::BufferHandle&& buffer) {
-  free(buffer.data());
+  if (FLAGS_mmap_buffers) {
+    int rc = munmap(buffer.data(), buffer.len());
+    DCHECK_EQ(rc, 0) << "Unexpected munmap() error: " << errno;
+  } else {
+    free(buffer.data());
+  }
   buffer.Reset(); // Avoid DCHECK in ~BufferHandle().
 }
 }

commit 830889a68709db345002bbf867ea86ac10235e9d

IMPALA-5230: fix non-functional impalad under ASAN
The bug is that TCMalloc metrics are not initialised under
ASAN but other metrics that reference them are created. As
soon as a debug webpage tries to access that metric, Impala
crashes.

The bug was introduced by commit 955b257cfb5: "IMPALA-5073:
Part 1: add option to use mmap() for buffer pool".

Testing:
Started the cluster under ASAN, ran some query tests, checked the /memz
and /metrics pages.

Change-Id: Ic71fa57e0444f470398e16513e4ba6e7f71565e4
Reviewed-on: http://gerrit.cloudera.org:8080/6694
Reviewed-by: Alex Behm <alex.behm@cloudera.com>
Tested-by: Impala Public Jenkins

be/src/util/memory-metrics.cc
@@ -73,7 +73,6 @@ Status impala::RegisterMemoryMetrics(MetricGroup* metrics, bool register_jvm_met
   TcmallocMetric::PHYSICAL_BYTES_RESERVED =
       metrics->RegisterMetric(new TcmallocMetric::PhysicalBytesMetric(
           MetricDefs::Get("tcmalloc.physical-bytes-reserved")));
-#endif
 
   // Add compound metrics that track totals across TCMalloc and the buffer pool.
   // total-used should track the total physical memory in use.
@@ -86,6 +85,7 @@ Status impala::RegisterMemoryMetrics(MetricGroup* metrics, bool register_jvm_met
 
   AggregateMemoryMetric::TOTAL_USED = metrics->RegisterMetric(
       new SumGauge<uint64_t>(MetricDefs::Get("memory.total-used"), used_metrics));
+#endif
   if (register_jvm_metrics) {
     RETURN_IF_ERROR(JvmMetric::InitMetrics(metrics->GetOrCreateChildGroup("jvm")));
   }

be/src/util/memory-metrics.h
@@ -39,7 +39,7 @@ class AggregateMemoryMetric {
   /// The sum of Tcmalloc TOTAL_BYTES_RESERVED and BufferPool SYSTEM_ALLOCATED.
   /// Approximates the total amount of physical memory consumed by the backend (i.e. not
   /// including JVM memory), which is either in use by queries or cached by the BufferPool
-  /// or TcMalloc.
+  /// or TcMalloc. NULL when running under ASAN.
   static SumGauge<uint64_t>* TOTAL_USED;
 };
 