b73c445eba48a4637c5642fa1866e237617a322d
Taras Bobrovytsky <tbobrovytsky@cloudera.com>
2017-05-11 13:54:13 -0700

    IMPALA-5246: Verify that a UDF test completely finishes before moving on
    A memory intensive UDF test takes a while to completely finish and for
    the memory in Impala to be completely freed. This caused a problem in
    ASAN builds (and potentially in normal builds) because we would start
    the next test right away, before the memory is freed.
    
    We fix the issue by checking that all fragments finish executing before
    starting the next test.
    
    Testing:
    - Ran a private ASAN build which passed.
    
    Change-Id: I0555b5327945c522f70f449caa1214ee0bfd84fe
    Reviewed-on: http://gerrit.cloudera.org:8080/6893
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Reviewed-by: Michael Ho <kwho@cloudera.com>
    Tested-by: Impala Public Jenkins

--- tests/query_test/test_udfs.py
+++ tests/query_test/test_udfs.py
@@ -30,6 +30,7 @@ from tests.common.test_dimensions import (
     create_uncompressed_text_dimension)
 from tests.util.calculation_util import get_random_id
 from tests.util.filesystem_utils import get_fs_path, IS_S3
+from tests.verifiers.metric_verifier import MetricVerifier
 
 class TestUdfBase(ImpalaTestSuite):
   """
@@ -340,6 +341,14 @@ class TestUdfExecution(TestUdfBase):
       assert False, "Query was expected to fail"
     except ImpalaBeeswaxException, e:
       self._check_exception(e)
+
+    # It takes a long time for Impala to free up memory after this test, especially if
+    # ASAN is enabled. Verify that all fragments finish executing before moving on to the
+    # next test to make sure that the next test is not affected.
+    for impalad in ImpalaCluster().impalads:
+      verifier = MetricVerifier(impalad.service)
+      verifier.wait_for_metric("impala-server.num-fragments-in-flight", 0)
+      verifier.verify_num_unused_buffers()
 
   def test_udf_constant_folding(self, vector, unique_database):
     """Test that constant folding of UDFs is handled correctly. Uses count_rows(),
