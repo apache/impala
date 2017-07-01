e3075c39ac488ebbb59a2483c78ebb106d13d5b1
Tim Armstrong <tarmstrong@cloudera.com>
2017-07-01 16:02:35 -0700

    IMPALA-5612: join inversion should factor in parallelism
    The join inversion optimisation did not factor in the degree of
    parallelism that the join executed with after inversion. In some cases
    this lead to bad decisions, e.g. executing a join on a single node
    instead of 20 nodes.
    
    This patch adds a more sophisticated cost model that factors degree
    of parallelism into the join inversion decision.
    
    The behaviour is unchanged if inversion does not change the degree of
    parallelism.
    
    Perf:
    Ran cluster TPC-H and TPC-DS benchmarks. Average changes were small:
    < 3%. Saw a mix of improvements and regressions. We were satisfied
    that the regressions were cases when the planner "got lucky" previously.
    E.g. on TPC-H Q2 a join was flipped to put lineitem on the left as a
    result of inaccurate cardinality estimates.
    
    Mostafa also ran a TPC-DS benchmark where the dimension tables were
    loaded with num_nodes=1 to minimise the number of files. We saw some
    huge speedups there on the unmodified queries, e.g. TPCDS-Q10 went from
    291s to 32.25s. The worst percentage regression was Q50, which went
    from 1.61s to 2.4s and the worst absolute regression was Q72, which
    went from 694s to 874s (25%).
    
    Change-Id: Icacea4565ce25ef15aaab014684c9440dd501d4e
    Reviewed-on: http://gerrit.cloudera.org:8080/7351
    Reviewed-by: Tim Armstrong <tarmstrong@cloudera.com>
    Tested-by: Impala Public Jenkins

--- fe/src/main/java/org/apache/impala/planner/Planner.java
+++ fe/src/main/java/org/apache/impala/planner/Planner.java
@@ -30,6 +30,7 @@ import org.apache.impala.analysis.InsertStmt;
 import org.apache.impala.analysis.JoinOperator;
 import org.apache.impala.analysis.QueryStmt;
 import org.apache.impala.analysis.SortInfo;
+import org.apache.impala.analysis.TupleId;
 import org.apache.impala.catalog.HBaseTable;
 import org.apache.impala.catalog.KuduTable;
 import org.apache.impala.catalog.Table;
@@ -394,14 +395,14 @@ public class Planner {
 
 
   /**
-   * Traverses the plan tree rooted at 'root' and inverts outer and semi joins
-   * in the following situations:
+   * Traverses the plan tree rooted at 'root' and inverts joins in the following
+   * situations:
    * 1. If the left-hand side is a SingularRowSrcNode then we invert the join because
    *    then the build side is guaranteed to have only a single row.
    * 2. There is no backend support for distributed non-equi right outer/semi joins,
    *    so we invert them (any distributed left semi/outer join is ok).
-   * 3. Invert semi/outer joins if the right-hand size is estimated to have a higher
-   *    cardinality*avgSerializedSize. Do not invert if relevant stats are missing.
+   * 3. If we estimate that the inverted join is cheaper (see isInvertedJoinCheaper()).
+   *    Do not invert if relevant stats are missing.
    * The first two inversion rules are independent of the presence/absence of stats.
    * Left Null Aware Anti Joins are never inverted due to lack of backend support.
    * Joins that originate from query blocks with a straight join hint are not inverted.
@@ -436,24 +437,98 @@ public class Planner {
         // The current join is a distributed non-equi right outer or semi join
         // which has no backend support. Invert the join to make it executable.
         joinNode.invertJoin();
-      } else {
-        // Invert the join if doing so reduces the size of the materialized rhs
-        // (may also reduce network costs depending on the join strategy).
-        // Only consider this optimization if both the lhs/rhs cardinalities are known.
-        long lhsCard = joinNode.getChild(0).getCardinality();
-        long rhsCard = joinNode.getChild(1).getCardinality();
-        float lhsAvgRowSize = joinNode.getChild(0).getAvgRowSize();
-        float rhsAvgRowSize = joinNode.getChild(1).getAvgRowSize();
-        if (lhsCard != -1 && rhsCard != -1 &&
-            lhsCard * lhsAvgRowSize < rhsCard * rhsAvgRowSize) {
-          joinNode.invertJoin();
-        }
+      } else if (isInvertedJoinCheaper(joinNode, isLocalPlan)) {
+        joinNode.invertJoin();
       }
     }
 
     // Re-compute tuple ids because the backend assumes that their order corresponds to
     // the order of children.
     root.computeTupleIds();
+  }
+
+  /**
+   * Return true if we estimate that 'joinNode' will be cheaper to execute after
+   * inversion. Returns false if any join input is missing relevant stats.
+   *
+   * For nested loop joins, we simply assume that the cost is determined by the size of
+   * the build side.
+   *
+   * For hash joins, the cost model is more nuanced and depends on:
+   * - est. number of rows in the build and probe: lhsCard and rhsCard
+   * - est. size of the rows in the build and probe: lhsAvgRowSize and rhsAvgRowSize
+   * - est. parallelism with which the lhs and rhs trees execute: lhsNumNodes
+   *   and rhsNumNodes. The parallelism of the join is determined by the lhs.
+   *
+   * The assumptions are:
+   * - the join strategy is PARTITIONED and rows are distributed evenly. We don't know
+   *   what join strategy will be chosen until later in planning so this assumption
+   *   simplifies the analysis. Generally if one input is small enough that broadcast
+   *   join is viable then this formula will prefer to put that input on the right side
+   *   anyway.
+   * - processing a build row is twice as expensive as processing a probe row of the
+   *   same size.
+   * - the cost of processing each byte of a row has a fixed component (C) (e.g.
+   *   hashing and comparing the row) and a variable component (e.g. looking up the
+   *   hash table).
+   * - The variable component grows proportionally to the log of the build side, to
+   *   approximate the effect of accesses to the the hash table hitting slower levels
+   *   of the memory hierarchy.
+   *
+   * The estimated per-host cost of a hash join before and after inversion, measured in
+   * an arbitrary unit of time, is then:
+   *
+   *    (log_b(rhsBytes) + C) * (lhsBytes + 2 * rhsBytes) / lhsNumNodes
+   *    vs.
+   *    (log_b(lhsBytes) + C) * (rhsBytes + 2 * lhsBytes) / rhsNumNodes
+   *
+   * where lhsBytes = lhsCard * lhsAvgRowSize and rhsBytes = rhsCard * rhsAvgRowSize
+   *
+   * We choose b = 10 and C = 5 empirically because it seems to give reasonable
+   * results for a range of inputs. The model is not particularly sensitive to the
+   * parameters.
+   *
+   * If the parallelism of both sides is the same then this reduces to comparing
+   * the size of input on both sides. Otherwise, if inverting a hash join reduces
+   * parallelism significantly, then a significant difference between lhs and rhs
+   * bytes is needed to justify inversion.
+   */
+  private boolean isInvertedJoinCheaper(JoinNode joinNode, boolean isLocalPlan) {
+    long lhsCard = joinNode.getChild(0).getCardinality();
+    long rhsCard = joinNode.getChild(1).getCardinality();
+    // Need cardinality estimates to make a decision.
+    if (lhsCard == -1 || rhsCard == -1) return false;
+    double lhsBytes = lhsCard * joinNode.getChild(0).getAvgRowSize();
+    double rhsBytes = rhsCard * joinNode.getChild(1).getAvgRowSize();
+    if (joinNode instanceof NestedLoopJoinNode) {
+      // For NLJ, simply try to minimize the size of the build side, since it needs to
+      // be broadcast to all participating nodes.
+      return lhsBytes < rhsBytes;
+    }
+    Preconditions.checkState(joinNode instanceof HashJoinNode);
+    int lhsNumNodes = isLocalPlan ? 1 : joinNode.getChild(0).getNumNodes();
+    int rhsNumNodes = isLocalPlan ? 1 : joinNode.getChild(1).getNumNodes();
+    // Need parallelism to determine whether inverting a hash join is profitable.
+    if (lhsNumNodes <= 0 || rhsNumNodes <= 0) return false;
+
+    final long CONSTANT_COST_PER_BYTE = 5;
+    // Add 1 to the log argument to avoid taking log of 0.
+    double totalCost =
+        (Math.log10(rhsBytes + 1) + CONSTANT_COST_PER_BYTE) * (lhsBytes + 2 * rhsBytes);
+    double invertedTotalCost =
+        (Math.log10(lhsBytes + 1) + CONSTANT_COST_PER_BYTE) * (rhsBytes + 2 * lhsBytes);
+    double perNodeCost = totalCost / lhsNumNodes;
+    double invertedPerNodeCost = invertedTotalCost / rhsNumNodes;
+    if (LOG.isTraceEnabled()) {
+      LOG.trace("isInvertedJoinCheaper() " + TupleId.printIds(joinNode.getTupleIds()));
+      LOG.trace("lhsCard " + lhsCard + " lhsBytes " + lhsBytes +
+          " lhsNumNodes " + lhsNumNodes);
+      LOG.trace("rhsCard " + rhsCard + " rhsBytes " + rhsBytes +
+          " rhsNumNodes " + rhsNumNodes);
+      LOG.trace("cost " + perNodeCost + " invCost " + invertedPerNodeCost);
+      LOG.trace("INVERT? " + (invertedPerNodeCost < perNodeCost));
+    }
+    return invertedPerNodeCost < perNodeCost;
   }
 
   /**
