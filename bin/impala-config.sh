e49b3691f391ad5c0883a79ecb696b6a56c5cf2b
Henry Robinson <henry@cloudera.com>
2017-07-14 10:49:00 -0700

    IMPALA-5659: Bump toolchain version
    This bumps the toolchain version to include the fix which ensured gflags
    built shared libraries. Impala does not currently depend on that change,
    but an upcoming change will do so.
    
    Change-Id: I9fe91b37ae00cd3d2e35b6a5c465f55238f32d1a
    Reviewed-on: http://gerrit.cloudera.org:8080/7422
    Reviewed-by: Matthew Jacobs <mj@cloudera.com>
    Tested-by: Impala Public Jenkins

--- bin/impala-config.sh
+++ bin/impala-config.sh
@@ -72,7 +72,7 @@ fi
 # moving to a different build of the toolchain, e.g. when a version is bumped or a
 # compile option is changed. The build id can be found in the output of the toolchain
 # build jobs, it is constructed from the build number and toolchain git hash prefix.
-export IMPALA_TOOLCHAIN_BUILD_ID=421-17d8ecf9cd
+export IMPALA_TOOLCHAIN_BUILD_ID=423-f32e122eaa
 # Versions of toolchain dependencies.
 # -----------------------------------
 export IMPALA_AVRO_VERSION=1.7.4-p4
