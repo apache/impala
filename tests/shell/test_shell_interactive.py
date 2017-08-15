c871e007bed662732944fd5bb8d88e306ff4865b
Tianyi Wang <twang@cloudera.com>
2017-08-14 19:03:09 -0700

    IMPALA-992: Rerun past queries from history in shell
    This patch adds a new command "rerun" and a shortcut "@" to impala-shell
    . Users can rerun a certain query by its index given by history command.
    A valid index is an integer in [1, history_length] or
    [-history_length, -1]. Negative values index history in reverse order.
    For example, "@1;" or "rerun 1;" reruns the first query shown in history
    and "@-1;" reruns the last query. The rerun command itself won't appear
    in history. The history index is 1-based and increasing. Old entries
    might be truncated when impala-shell starts, and the indexes will be
    realigned to 1, so the same index may refer to different commands among
    multiple impala-shell instances.
    
    Testing: A test case test_rerun is added to
    shell/test_shell_interactive.py
    
    Change-Id: Ifc28e8ce07845343267224c3b9ccb71b29a524d2
    Reviewed-on: http://gerrit.cloudera.org:8080/7674
    Reviewed-by: Sailesh Mukil <sailesh@cloudera.com>
    Tested-by: Impala Public Jenkins

--- tests/shell/test_shell_interactive.py
+++ tests/shell/test_shell_interactive.py
@@ -232,6 +232,42 @@ class TestImpalaShellInteractive(object):
       assert query in result.stderr, "'%s' not in '%s'" % (query, result.stderr)
 
   @pytest.mark.execute_serially
+  def test_rerun(self):
+    """Smoke test for the 'rerun' command"""
+    # Clear history first.
+    if os.path.exists(SHELL_HISTORY_FILE):
+      os.remove(SHELL_HISTORY_FILE)
+    assert not os.path.exists(SHELL_HISTORY_FILE)
+    child_proc = pexpect.spawn(SHELL_CMD)
+    child_proc.expect(":21000] >")
+    self._expect_with_cmd(child_proc, "@1", ("Command index out of range"))
+    self._expect_with_cmd(child_proc, "rerun -1", ("Command index out of range"))
+    self._expect_with_cmd(child_proc, "select 'first_command'", ("first_command"))
+    self._expect_with_cmd(child_proc, "rerun 1", ("first_command"))
+    self._expect_with_cmd(child_proc, "@ -1", ("first_command"))
+    self._expect_with_cmd(child_proc, "select 'second_command'", ("second_command"))
+    child_proc.sendline('history;')
+    child_proc.expect(":21000] >")
+    assert '[1]: select \'first_command\';' in child_proc.before;
+    assert '[2]: select \'second_command\';' in child_proc.before;
+    assert '[3]: history;' in child_proc.before;
+    # Rerunning command should not add an entry into history.
+    assert '[4]' not in child_proc.before;
+    self._expect_with_cmd(child_proc, "@0", ("Command index out of range"))
+    self._expect_with_cmd(child_proc, "rerun   4", ("Command index out of range"))
+    self._expect_with_cmd(child_proc, "@-4", ("Command index out of range"))
+    self._expect_with_cmd(child_proc, " @ 3 ", ("second_command"))
+    self._expect_with_cmd(child_proc, "@-3", ("first_command"))
+    self._expect_with_cmd(child_proc, "@",
+                          ("Command index to be rerun must be an integer."))
+    self._expect_with_cmd(child_proc, "@1foo",
+                          ("Command index to be rerun must be an integer."))
+    self._expect_with_cmd(child_proc, "@1 2",
+                          ("Command index to be rerun must be an integer."))
+    self._expect_with_cmd(child_proc, "rerun1", ("Syntax error"))
+    child_proc.sendline('quit;')
+
+  @pytest.mark.execute_serially
   def test_tip(self):
     """Smoke test for the TIP command"""
     # Temporarily add impala_shell module to path to get at TIPS list for verification
