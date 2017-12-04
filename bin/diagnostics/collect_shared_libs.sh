2883c9950026db74240a69ab07e867810b8547b0
Bharath Vissapragada <bharathv@cloudera.com>
2017-12-04 13:38:09 -0800

    IMPALA-6747: Automate diagnostics collection.
    This commit adds the necessary tooling to automate diagnostics
    collection for Impala daemons. Following diagnostics are supported.
    
    1. Native core dump (+ shared libs)
    2. GDB/Java thread dump (pstack + jstack)
    3. Java heap dump (jmap)
    4. Minidumps (using breakpad) *
    5. Profiles
    
    Given the required inputs, the script outputs a zip compressed
    impala diagnostic bundle with all the diagnostics collected.
    
    The script can be run manually with the following command.
    
    python collect_diagnostics.py --help
    
    * minidumps collected here correspond to the state of the Impala
    process at the time this script is triggered. This is different
    from collect_minidumps.py which archives the entire minidump
    directory.
    
    Change-Id: Ib29caec7c3be5b6a31e60461294979c318300f64
    Reviewed-on: http://gerrit.cloudera.org:8080/9815
    Reviewed-by: Lars Volker <lv@cloudera.com>
    Reviewed-by: Philip Zeyliger <philip@cloudera.com>
    Tested-by: Impala Public Jenkins

---
+++ bin/diagnostics/collect_shared_libs.sh
@@ -0,0 +1,52 @@
+#!/usr/bin/env bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+# $1 - gdb binary path
+# $2 - pid of the Impala process
+# $3 - Output directory to copy the sharedlibs to.
+
+set -euxo pipefail
+
+if [ "$#" -ne 3 ]; then
+  echo "Incorrect usage. Expected: $0 <gdb executable path> <target PID> <output dir>"
+  exit 1
+fi
+
+if [ ! -d $3 ]; then
+  echo "Directory $3 does not exist. This script expects the output directory to exist."
+  exit 1
+fi
+
+# Generate the list of shared libs path to copy.
+shared_libs_to_copy=$(mktemp)
+$1 --pid $2 --batch -ex 'info shared' 2> /dev/null | sed '1,/Shared Object Library/d' |
+    sed 's/\(.*\s\)\(\/.*\)/\2/' | grep \/ > $shared_libs_to_copy
+
+echo "Generated shared library listing for the process."
+
+# Copy the files to the target directory keeping the directory structure intact.
+# We use rsync instead of 'cp --parents' since the latter has permission issues
+# copying from system level directories. https://goo.gl/6yYNhw
+rsync -LR --files-from=$shared_libs_to_copy / $3
+
+echo "Copied the shared libraries to the target directory: $3"
+
+rm -f $shared_libs_to_copy
+# Make sure the impala user has write permissions on all the copied sharedlib paths.
+chmod 755 -R $3
