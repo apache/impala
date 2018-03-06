b98ec45249eb007707de615485dee0d774c24993
Lars Volker <lv@cloudera.com>
2018-03-06 13:38:12 -0800

    IMPALA-6613: Change TEST_KRPC to DISABLE_KRPC
    This change renames the TEST_KRPC environment variable to DISABLE_KRPC
    to reflect the fact that KRPC is now enabled by default.
    
    Change-Id: I2081d9b53566607c25bcf84c4c92f13ee93be162
    Reviewed-on: http://gerrit.cloudera.org:8080/9516
    Reviewed-by: Dan Hecht <dhecht@cloudera.com>
    Reviewed-by: Michael Ho <kwho@cloudera.com>
    Tested-by: Lars Volker <lv@cloudera.com>

--- bin/run-all-tests.sh
+++ bin/run-all-tests.sh
@@ -38,10 +38,9 @@ if "${CLUSTER_DIR}/admin" is_kerberized; then
   KERB_ARGS="--use_kerberos"
 fi
 
-# Enable KRPC during tests (default: true).
-: ${TEST_KRPC:=true}
-
 # Parametrized Test Options
+# Disable KRPC for test cluster and test execution
+: ${DISABLE_KRPC:=false}
 # Run FE Tests
 : ${FE_TEST:=true}
 # Run Backend Tests
@@ -67,7 +66,7 @@ else
 fi
 
 # If KRPC tests are disabled, pass the flag to disable KRPC during cluster start.
-if [[ "${TEST_KRPC}" == "false" ]]; then
+if [[ "${DISABLE_KRPC}" == "true" ]]; then
   TEST_START_CLUSTER_ARGS="${TEST_START_CLUSTER_ARGS} --disable_krpc"
 fi
 
@@ -121,7 +120,7 @@ fi
 
 # If KRPC tests are disabled, pass test_no_krpc flag to pytest.
 # This includes the end-to-end tests and the custom cluster tests.
-if [[ "${TEST_KRPC}" == "false" ]]; then
+if [[ "${DISABLE_KRPC}" == "true" ]]; then
   COMMON_PYTEST_ARGS+=" --test_no_krpc"
 fi
 
