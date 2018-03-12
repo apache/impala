45aee121eb95505396c63f08c48f2cd0e0182e41
Philip Zeyliger <philip@cloudera.com>
2018-03-12 13:55:37 -0700

    Removing (broken) retries from split-hbase.sh.
    The retries in split-hbase.sh don't work in the common case,
    because $MINIKDC_PRINC_HIVE is not set in non-kerberized (common)
    environments. The regular data load scripts (create-load-data.sh)
    have code to manage that, but split-hbase.sh blindly forges ahead,
    leading to errors like:
    
      /home/impdev/Impala/testdata/bin/split-hbase.sh: line 49: MINIKDC_PRINC_HIVE: unbound variable
      Error in /home/impdev/Impala/testdata/bin/create-load-data.sh at line 48: LOAD_DATA_ARGS=""
    
    Since this hasn't been working, I opted to remove it entirely, as a failure on
    the line where HBase splitting actually failed would be significantly more
    useful than the error here. A search of mailing lists suggested that I was at
    least the second person to have run into this. (In my case, I did break HBase
    splitting, but it took me a second to identify the error, since the log was
    spammed with unrelated information relating to the cluster restart.)
    
    Testing: core tests.
    
    Change-Id: I715891c9e744f21002330c3ae3ebc14095d94ffd
    Reviewed-on: http://gerrit.cloudera.org:8080/9588
    Reviewed-by: Philip Zeyliger <philip@cloudera.com>
    Tested-by: Impala Public Jenkins

--- testdata/bin/split-hbase.sh
+++ testdata/bin/split-hbase.sh
@@ -38,17 +38,6 @@ mvn $IMPALA_MAVEN_OPTIONS -q dependency:copy-dependencies
 export CLASSPATH=$IMPALA_HOME/testdata/target/impala-testdata-0.1-SNAPSHOT.jar:$CLASSPATH
 
 : ${JAVA_KERBEROS_MAGIC=}
-for ATTEMPT in {1..10}; do
-  if "$JAVA" ${JAVA_KERBEROS_MAGIC} \
-      org.apache.impala.datagenerator.HBaseTestDataRegionAssigment \
-      functional_hbase.alltypesagg functional_hbase.alltypessmall; then
-    break
-  fi
-  # Hopefully reloading the data will somehow help the splitting succeed.
-  $IMPALA_HOME/bin/start-impala-cluster.py
-  $IMPALA_HOME/bin/load-data.py -w functional-query \
-      --table_names=alltypesagg,alltypessmall --table_formats=hbase/none --force \
-      ${KERB_ARGS} --principal=${MINIKDC_PRINC_HIVE}
-  $IMPALA_HOME/tests/util/compute_table_stats.py --db_names=functional_hbase \
-      --table_names=alltypesagg,alltypessmall ${KERB_ARGS}
-done
+"$JAVA" ${JAVA_KERBEROS_MAGIC} \
+    org.apache.impala.datagenerator.HBaseTestDataRegionAssigment \
+    functional_hbase.alltypesagg functional_hbase.alltypessmall
