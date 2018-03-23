a026f265a260462015d90ad3d3be7686d8486865
Fredy wijaya <fwijaya@cloudera.com>
2018-03-23 11:23:21 -0700

    IMPALA-6719: Reset metadata database name case sensitivity
    Fix issue with database case name case sensitivity in reset metadata
    statement.
    
    Testing:
    - Created end-to-end reset metadata tests.
    
    Change-Id: Id880aa559cec0afe8fbb7d33ccce83f7b5e474cb
    Reviewed-on: http://gerrit.cloudera.org:8080/9788
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Impala Public Jenkins

---
+++ tests/metadata/test_reset_metadata.py
@@ -0,0 +1,34 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+from test_ddl_base import TestDdlBase
+
+class TestResetMetadata(TestDdlBase):
+  def test_reset_metadata_case_sensitivity(self, unique_database):
+    # IMPALA-6719: Issue with database name case sensitivity in reset metadata.
+    table = 'newtable'
+    self.client.execute('create table %s.%s (i int)' % (unique_database, table))
+
+    self.client.execute('refresh %s.%s' % (unique_database, table))
+    self.client.execute('refresh %s.%s' % (unique_database.upper(), table.upper()))
+
+    self.client.execute('invalidate metadata %s.%s' % (unique_database, table))
+    self.client.execute('invalidate metadata %s.%s' % (unique_database.upper(),
+                                                       table.upper()))
+
+    self.client.execute('refresh functions %s' % unique_database)
+    self.client.execute('refresh functions %s' % unique_database.upper())
