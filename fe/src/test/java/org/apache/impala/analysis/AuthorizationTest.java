9ecbb1846b2cc6d062106f86bb193a34f92536ef
Fredy wijaya <fwijaya@cloudera.com>
2018-04-03 09:43:18 -0700

    IMPALA-6800: Add test cases for statements that require ALTER privilege
    Testing:
    - Added new test cases
    - Ran front-end tests
    
    Change-Id: Ie9fc1249420771ee857259c01acc3f9e7c9ed383
    Reviewed-on: http://gerrit.cloudera.org:8080/9905
    Reviewed-by: Alex Behm <alex.behm@cloudera.com>
    Tested-by: Impala Public Jenkins

--- fe/src/test/java/org/apache/impala/analysis/AuthorizationTest.java
+++ fe/src/test/java/org/apache/impala/analysis/AuthorizationTest.java
@@ -1642,6 +1642,8 @@ public class AuthorizationTest extends FrontendTestBase {
   @Test
   public void TestComputeStatsTable() throws ImpalaException {
     AuthzOk("compute stats functional_seq_snap.alltypes");
+    // User has ALTER privilege on functional.alltypeserror table.
+    AuthzOk("compute stats functional.alltypeserror");
 
     AuthzError("compute stats functional.alltypes",
         "User '%s' does not have privileges to execute 'ALTER' on: functional.alltypes");
@@ -1656,6 +1658,8 @@ public class AuthorizationTest extends FrontendTestBase {
   @Test
   public void TestDropStats() throws ImpalaException {
     AuthzOk("drop stats functional_seq_snap.alltypes");
+    // User has ALTER privilege on functional.alltypeserror table.
+    AuthzOk("drop stats functional.alltypeserror");
 
     AuthzError("drop stats functional.alltypes",
         "User '%s' does not have privileges to execute 'ALTER' on: functional.alltypes");
