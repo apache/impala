c4b214b3a2ef8ca4692f74e6a289ddf76d1b3f58
Sailesh Mukil <sailesh@cloudera.com>
2018-04-09 15:26:06 -0700

    IMPALA-6813: Hedged reads metrics broken when scanning non-HDFS based table
    We realized that the libHDFS API call hdfsGetHedgedReadMetrics() crashes
    when the 'fs' argument passed to it is not a HDFS filesystem.
    
    There is an open bug for it on the HDFS side: HDFS-13417
    However, it looks like we won't be getting a fix for it in the short term,
    so our only option at this point is to skip it.
    
    Testing: Made sure that enabling preads and scanning from S3 doesn't
    cause a crash.
    Also, added a custom cluster test to exercise the pread code path. We
    are unable to verify hedged reads in a minicluster, but we can at least
    exercise the code path to make sure that nothing breaks.
    
    Change-Id: I48fe80dfd9a1ed68a8f2b7038e5f42b5a3df3baa
    Reviewed-on: http://gerrit.cloudera.org:8080/9966
    Reviewed-by: Sailesh Mukil <sailesh@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

---
+++ tests/custom_cluster/test_hedged_reads.py
@@ -0,0 +1,30 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import pytest
+from tests.common.custom_cluster_test_suite import CustomClusterTestSuite
+from tests.common.skip import SkipIf
+
+@SkipIf.not_hdfs
+class TestHedgedReads(CustomClusterTestSuite):
+  """ Exercises the hedged reads code path.
+      NOTE: We unfortunately cannot force hedged reads on a minicluster, but we enable
+      this test to at least make sure that the code path doesn't break."""
+  @CustomClusterTestSuite.with_args("--use_hdfs_pread=true")
+  def test_hedged_reads(self, vector):
+    QUERY = "select * from tpch_parquet.lineitem limit 100"
+    self.client.execute(QUERY)
