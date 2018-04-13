ce09269fde901a262b9e1ce0c7259d8864c1bb3b
Bharath Vissapragada <bharathv@cloudera.com>
2018-04-12 21:52:29 -0700

    IMPALA-6747: Automate diagnostics collection.
    This commit adds the necessary tooling to automate diagnostics
    collection for Impala daemons. Following diagnostics are supported.
    
    1. Native core dump (+ shared libs)
    2. GDB/Java thread dump (pstack + jstack)
    3. Java heap dump (jmap)
    4. Minidumps (using breakpad) *
    5. Profiles
    
    Given the required inputs, the script outputs a zip compressed
    impala diagnostic bundle with all the diagnostics collected.
    
    The script can be run manually with the following command.
    
    python collect_diagnostics.py --help
    
    Tested with python 2.6 and later.
    
    * minidumps collected here correspond to the state of the Impala
    process at the time this script is triggered. This is different
    from collect_minidumps.py which archives the entire minidump
    directory.
    
    Change-Id: I166e726f1dd1ce81187616e4f06d2404fa379bf8
    Reviewed-on: http://gerrit.cloudera.org:8080/10056
    Reviewed-by: Philip Zeyliger <philip@cloudera.com>
    Reviewed-by: Bharath Vissapragada <bharathv@cloudera.com>
    Tested-by: Bharath Vissapragada <bharathv@cloudera.com>

---
+++ tests/unittests/test_command.py
@@ -0,0 +1,49 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+#
+# Unit tests for collect_diagnostics.Command
+
+import os
+import pytest
+import sys
+
+# Update the sys.path to include the modules from bin/diagnostics.
+sys.path.insert(0,
+    os.path.abspath(os.path.join(os.path.dirname(__file__), '../../bin/diagnostics')))
+from collect_diagnostics import Command
+
+class TestCommand(object):
+  """ Unit tests for the Command class"""
+
+  def test_simple_commands(self):
+    # Successful command
+    c = Command(["echo", "foo"], 1000)
+    assert c.run() == 0, "Command expected to succeed, but failed"
+    assert c.stdout.strip("\n") == "foo"
+
+    # Failed command, check return code
+    c = Command(["false"], 1000)
+    assert c.run() == 1
+
+  def test_command_timer(self):
+    # Try to run a command that sleeps for 1000s and set a
+    # timer for 1 second. The command should timed out.
+    c = Command(["sleep", "1000"], 1)
+    assert c.run() != 0, "Command expected to timeout but succeeded."
+    assert c.child_killed_by_timeout, "Command didn't timeout as expected."
+
+
