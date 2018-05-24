1e946fbbea0d15e4022088e57f162cc81015ac15
Joe McDonnell <joemcdonnell@cloudera.com>
2018-05-24 14:58:16 -0700

    IMPALA-7199: Add scripts to create code coverage reports
    gcovr is a python library that uses gcov to generate
    code coverage reports. This adds gcovr to the python
    dependencies and adds bin/impala-gcovr to provide
    easy access to gcovr's command line. gcovr 3.4
    supports python 2.6+.
    
    This also adds bin/coverage_helper.sh to provide a
    simplified interface to generate reports and zero
    coverage counters.
    
    Code coverage data is written out when a program
    exits, so it is important to avoid hard kills
    to shut down the impalads when generating coverage.
    This modifies testdata/bin/kill-all.sh to call
    start-impala-cluster.py --kill when shutting down
    the minicluster to try to avoid doing a hard kill.
    It will still do a hard kill if impala is still
    running after the softer kill.
    
    Change-Id: I5b2e0b794c64f9343ec976de7a3f235e54d2badd
    Reviewed-on: http://gerrit.cloudera.org:8080/10791
    Reviewed-by: Joe McDonnell <joemcdonnell@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

---
+++ bin/coverage_helper.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+set -eu -o pipefail
+
+ZERO_COUNTERS_ACTION=0
+REPORT_ACTION=0
+REPORT_DIRECTORY=${IMPALA_HOME}/logs/coverage
+
+function print_usage {
+  echo "coverage_helper.sh - Driver script for coverage"
+  echo "[-zerocounters] : Reset coverage counters"
+  echo "[-report] : Generate a detailed coverage report"
+  echo "[-reportdirectory <directory>] : Output directory for coverage report files"
+}
+
+while [ -n "$*" ]
+do
+  case "$1" in
+    -zerocounters)
+       ZERO_COUNTERS_ACTION=1
+       ;;
+    -report)
+       REPORT_ACTION=1
+       ;;
+    -reportdirectory)
+       REPORT_DIRECTORY="${2-}"
+       shift
+       ;;
+    -help|*)
+       print_usage
+       exit 1
+       ;;
+  esac
+  shift
+done
+
+if [[ ${ZERO_COUNTERS_ACTION} -eq 0 && ${REPORT_ACTION} -eq 0 ]]; then
+  print_usage
+  exit 1
+fi
+
+if pgrep -U "$USER" impalad; then
+  echo "Warning: impalad is running. Coverage counters are only updated when"
+  echo "a program exits. Any report will not include the information from"
+  echo "the running impalad. Similarly, zeroing the counters will not zero"
+  echo "the counters for the running impalad."
+fi
+
+if [ ${REPORT_ACTION} -eq 1 ]; then
+  mkdir -p "${REPORT_DIRECTORY}"
+  rm -f "${REPORT_DIRECTORY}"/index*.html
+  # src/util/bit-packing.inline.h gets lots of hits, so generating a detailed report
+  # for it takes several minutes. Exclude it to keep the execution time down.
+  # gcovr excludes are buggy, so on some environments these excludes won't work.
+  echo "Generating report in directory: ${REPORT_DIRECTORY}"
+  cd "${IMPALA_HOME}"
+  "${IMPALA_HOME}/bin/impala-gcovr" -v -r "${IMPALA_HOME}/be" \
+    --exclude=".*src/benchmarks.*" \
+    --exclude=".*generated-sources/gen-cpp.*" \
+    --exclude=".*src/util/bit-packing.inline.h.*" \
+    --html --html-details -o "${REPORT_DIRECTORY}/index.html" > "${REPORT_DIRECTORY}/gcovr.out"
+fi
+
+if [ ${ZERO_COUNTERS_ACTION} -eq 1 ]; then
+  # The .gcda files contain the counters for coverage. Deleting them zeros the
+  # the counters.
+  echo "Zeroing Counters"
+  find "${IMPALA_HOME}/be" -name "*.gcda" -delete
+fi
