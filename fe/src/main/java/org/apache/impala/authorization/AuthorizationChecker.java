0ae9d58c0f760dace23e31780bfe67cfc025b155
Adam Holley <git@holleyism.com>
2018-07-10 22:58:17 -0500

    IMPALA-7275: Create table authorization error should not show table name
    The authorization correctly checks database level authorization but
    the exception includes the table name.  This fix adds a check for
    create and modifies the error string to remove the table name.  Having
    an error with CREATE on a table is not correct since to create a table
    a user needs CREATE on the parent database.
    
    Example:
    SQL: CREATE TABLE db.tbl(col1 INT)
    Before:
    User 'user1' does not have privileges to execute 'CREATE' on: db.tbl
    After:
    User 'user1' does not have privileges to execute 'CREATE' on: db
    
    Testsing: Ran all FE tests
    
    Change-Id: I6711a744541abd5ff26574974ba8517b6e51c453
    Reviewed-on: http://gerrit.cloudera.org:8080/10919
    Reviewed-by: Fredy Wijaya <fwijaya@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- fe/src/main/java/org/apache/impala/authorization/AuthorizationChecker.java
+++ fe/src/main/java/org/apache/impala/authorization/AuthorizationChecker.java
@@ -19,6 +19,7 @@ package org.apache.impala.authorization;
 
 import java.util.Collections;
 import java.util.EnumSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Set;
 
@@ -113,10 +114,19 @@ public class AuthorizationChecker {
             "User '%s' does not have privileges to access: %s",
             user.getName(), privilegeRequest.getName()));
       } else if (privilege == Privilege.REFRESH) {
+        throw new AuthorizationException(String.format(
+            "User '%s' does not have privileges to execute " +
+            "'INVALIDATE METADATA/REFRESH' on: %s", user.getName(),
+            privilegeRequest.getName()));
+      } else if (privilege == Privilege.CREATE &&
+          privilegeRequest.getAuthorizeable() instanceof AuthorizeableTable) {
+        // Creating a table requires CREATE on a database and we shouldn't
+        // expose the table name.
+        AuthorizeableTable authorizeableTable =
+            (AuthorizeableTable) privilegeRequest.getAuthorizeable();
           throw new AuthorizationException(String.format(
-              "User '%s' does not have privileges to execute " +
-              "'INVALIDATE METADATA/REFRESH' on: %s", user.getName(),
-              privilegeRequest.getName()));
+              "User '%s' does not have privileges to execute '%s' on: %s",
+              user.getName(), privilege, authorizeableTable.getDbName()));
       } else {
         throw new AuthorizationException(String.format(
             "User '%s' does not have privileges to execute '%s' on: %s",
