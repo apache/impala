55083f38cce74347a3a528dd59c402f5cfd4441a
Tim Armstrong <tarmstrong@cloudera.com>
2018-07-30 16:20:11 -0700

    IMPALA-7317: comment on long lines and whitespace
    Add some basic formatting checks that can be implemented
    easily by parsing the diff line-by-line. These are applied
    to Java, Python, C++, shell and thrift source files with
    some exclusions.
    
    Adds a --dryrun option that does not try to post back the
    comments to gerrit.
    
    Remove the option to specify a git revision since flake8-diff doesn't
    work correctly when run on source that isn't checked out.
    
    Testing:
    Added some violations to this code change that will be
    fixed before merging. The output is:
    
      {
       "comments": {
        "bin/jenkins/critique-gerrit-review.py": [
         {
          "message": "flake8: E501 line too long (107 > 90 characters)",
          "range": {
           "start_character": 90,
           "start_line": 124,
           "end_line": 124,
           "end_character": 91
          }
         },
         {
          "message": "tab used for whitespace",
          "line": 125
         }
        ]
       }
      }
    
    Change-Id: I36bb99560ab09d7753ff93227d1c4972582770f2
    Reviewed-on: http://gerrit.cloudera.org:8080/11085
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Tim Armstrong <tarmstrong@cloudera.com>

--- bin/jenkins/critique-gerrit-review.py
+++ bin/jenkins/critique-gerrit-review.py
@@ -16,7 +16,7 @@
 # specific language governing permissions and limitations
 # under the License.
 #
-# Usage: critique-gerrit-review.py <git commit>
+# Usage: critique-gerrit-review.py [--dryrun]
 #
 # This script is meant to run on an jenkins.impala.io build slave and post back comments
 # to a code review. It does not need to run on all supported platforms so we use system
@@ -34,16 +34,16 @@
 # ssh, pip, virtualenv
 #
 # TODO: generalise to other warnings
-# * Lines too long and trailing whitespace
 # * clang-tidy
 
+from argparse import ArgumentParser
 from collections import defaultdict
 import json
 import os
 from os import environ
 import os.path
 import re
-from subprocess import check_call, Popen, PIPE
+from subprocess import check_call, check_output, Popen, PIPE
 import sys
 import virtualenv
 
@@ -54,6 +54,19 @@ VENV_PATH = "gerrit_critic_venv"
 VENV_BIN = os.path.join(VENV_PATH, "bin")
 PIP_PATH = os.path.join(VENV_BIN, "pip")
 FLAKE8_DIFF_PATH = os.path.join(VENV_BIN, "flake8-diff")
+
+# Limit on length of lines in source files.
+LINE_LIMIT = 90
+
+# Source file extensions that we should apply our line limit and whitespace rules to.
+SOURCE_EXTENSIONS = set([".cc", ".h", ".java", ".py", ".sh", ".thrift"])
+
+# Source file patterns that we exclude from our line limit and whitespace rules.
+EXCLUDE_FILE_PATTERNS = [
+    re.compile(r".*be/src/kudu.*"),  # Kudu source code may have different rules.
+    re.compile(r".*-benchmark.cc"),  # Benchmark files tend to have long lines.
+    re.compile(r".*/function-registry/impala_functions.py")  # Many long strings.
+]
 
 
 def setup_virtualenv():
@@ -108,6 +121,63 @@ def get_flake8_comments(revision):
   return comments
 
 
+def get_misc_comments(revision):
+  """Get miscellaneous warnings for code changes made in the git commit 'revision', e.g.
+  long lines and trailing whitespace. These warnings are produced by directly parsing the
+  diff output."""
+  comments = defaultdict(lambda: [])
+  # Matches range information like:
+  #  @@ -128 +133,2 @@ if __name__ == "__main__":
+  RANGE_RE = re.compile(r"^@@ -[0-9,]* \+([0-9]*).*$")
+
+  diff = check_output(["git", "diff", "-U0", "{0}^..{0}".format(revision)])
+  curr_file = None
+  check_source_file = False
+  curr_line_num = 0
+  for diff_line in diff.splitlines():
+    if diff_line.startswith("+++ "):
+      # Start of diff for a file. Strip off "+++ b/" to get the file path.
+      curr_file = diff_line[6:]
+      check_source_file = os.path.splitext(curr_file)[1] in SOURCE_EXTENSIONS
+      if check_source_file:
+        for pattern in EXCLUDE_FILE_PATTERNS:
+          if pattern.match(curr_file):
+            check_source_file = False
+            break
+    elif diff_line.startswith("@@ "):
+      # Figure out the starting line of the hunk. Format of unified diff is:
+      #  @@ -128 +133,2 @@ if __name__ == "__main__":
+      # We want to extract the start line for the added lines
+      match = RANGE_RE.match(diff_line)
+      if not match:
+        raise Exception("Pattern did not match diff line:\n{0}".format(diff_line))
+      curr_line_num = int(match.group(1))
+    elif diff_line.startswith("+") and check_source_file:
+      # An added or modified line - check it to see if we should generate warnings.
+      add_misc_comments_for_line(comments, diff_line[1:], curr_file, curr_line_num)
+      curr_line_num += 1
+  return comments
+
+
+def add_misc_comments_for_line(comments, line, curr_file, curr_line_num):
+  """Helper for get_misc_comments to generate comments for 'line' at 'curr_line_num' in
+  'curr_file' and append them to 'comments'."""
+  # Check for trailing whitespace.
+  if line.rstrip() != line:
+    comments[curr_file].append(
+        {"message": "line has trailing whitespace", "line": curr_line_num})
+
+  # Check for long lines. Skip .py files since flake8 already flags long lines.
+  if len(line) > 90 and os.path.splitext(curr_file)[1] != ".py":
+    msg = "line too long ({0} > {1})".format(len(line), LINE_LIMIT)
+    comments[curr_file].append(
+        {"message": msg, "line": curr_line_num})
+
+  if '\t' in line:
+    comments[curr_file].append(
+        {"message": "tab used for whitespace", "line": curr_line_num})
+
+
 def post_review_to_gerrit(review_input):
   """Post a review to the gerrit patchset. 'review_input' is a ReviewInput JSON object
   containing the review comments. The gerrit change and patchset are picked up from
@@ -123,8 +193,24 @@ def post_review_to_gerrit(review_input):
     raise Exception("Error posting review to gerrit.")
 
 
+def merge_comments(a, b):
+  for k, v in b.iteritems():
+    a[k].extend(v)
+
+
 if __name__ == "__main__":
+  parser = ArgumentParser(description="Generate and post gerrit comments")
+  parser.add_argument("--dryrun", action='store_true',
+                      help="Don't post comments back to gerrit")
+  args = parser.parse_args()
+
   setup_virtualenv()
-  review_input = {"comments": get_flake8_comments(sys.argv[1])}
+  # flake8-diff only actually works correctly on HEAD, so this is the only revision
+  # we can correctly handle.
+  revision = 'HEAD'
+  comments = get_flake8_comments(revision)
+  merge_comments(comments, get_misc_comments(revision))
+  review_input = {"comments": comments}
   print json.dumps(review_input, indent=True)
-  post_review_to_gerrit(review_input)
+  if not args.dryrun:
+    post_review_to_gerrit(review_input)
