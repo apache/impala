f1a3c47959e17d301f970b7ea2755067d00ae986
Joe McDonnell <joemcdonnell@cloudera.com>
2018-11-19 16:40:09 -0800

    IMPALA-7804: Mitigate s3 consistency issues for test_scanners
    test_scanners.py has seen several flaky failures on
    s3 due to eventual consistency. The symptom is Impala
    being unable to read a file that it just loaded to s3.
    
    A large number of tables used in test_scanners.py
    use the file_utils helper functions for creating
    the tables. These follow the pattern:
    1. Copy files to temporary directory in HDFS/S3/etc
    2. Create table
    3. Run LOAD DATA to move the files to the table
    
    In step #3, LOAD DATA gets the metadata for the
    table before it runs the move statement on the
    files. Subsequent queries on the table will not
    need to reload metadata and can access the file
    quickly after the move.
    
    This changes the ordering to put the files in place
    before loading metadata. This may improve the
    likelihood that the filesystem is consistent by
    the time we read it. Specifically, we now do:
    1. Put the files in directory that the table
       will use when it is created.
    2. Create table
    Neither of these steps load metadata, so the next
    query that runs will load metadata.
    
    Change-Id: Id042496beabe0d0226b347e0653b820fee369f4e
    Reviewed-on: http://gerrit.cloudera.org:8080/11959
    Reviewed-by: Joe McDonnell <joemcdonnell@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- tests/common/file_utils.py
+++ tests/common/file_utils.py
@@ -32,32 +32,43 @@ def create_table_from_parquet(impala_client, unique_database, table_name):
   local_file = os.path.join(os.environ['IMPALA_HOME'],
                             'testdata/data/{0}'.format(filename))
   assert os.path.isfile(local_file)
-  hdfs_file = get_fs_path('/test-warehouse/{0}.db/{1}'.format(unique_database, filename))
-  check_call(['hdfs', 'dfs', '-copyFromLocal', '-f', local_file, hdfs_file])
 
+  # The table doesn't exist, so create the table's directory
+  tbl_dir = get_fs_path('/test-warehouse/{0}.db/{1}'.format(unique_database, table_name))
+  check_call(['hdfs', 'dfs', '-mkdir', '-p', tbl_dir])
+
+  # Put the parquet file in the table's directory
+  # Note: -d skips a staging copy
+  check_call(['hdfs', 'dfs', '-put', '-f', '-d', local_file, tbl_dir])
+
+  # Create the table
+  hdfs_file = '{0}/{1}'.format(tbl_dir, filename)
   qualified_table_name = '{0}.{1}'.format(unique_database, table_name)
   impala_client.execute('create table {0} like parquet "{1}" stored as parquet'.format(
     qualified_table_name, hdfs_file))
-  impala_client.execute('load data inpath "{0}" into table {1}'.format(
-    hdfs_file, qualified_table_name))
 
 
 def create_table_and_copy_files(impala_client, create_stmt, unique_database, table_name,
                                 files):
-  create_stmt = create_stmt.format(db=unique_database, tbl=table_name)
-  impala_client.execute(create_stmt)
+  # Create the directory
+  hdfs_dir = get_fs_path('/test-warehouse/{0}.db/{1}'.format(unique_database, table_name))
+  check_call(['hdfs', 'dfs', '-mkdir', '-p', hdfs_dir])
+
+  # Copy the files
+  #  - build a list of source files
+  #  - issue a single put to the hdfs_dir ( -d skips a staging copy)
+  source_files = []
   for local_file in files:
     # Cut off leading '/' to make os.path.join() happy
     local_file = local_file if local_file[0] != '/' else local_file[1:]
     local_file = os.path.join(os.environ['IMPALA_HOME'], local_file)
     assert os.path.isfile(local_file)
-    basename = os.path.basename(local_file)
-    hdfs_file = get_fs_path('/test-warehouse/{0}.db/{1}'.format(unique_database,
-                                                                basename))
-    check_call(['hdfs', 'dfs', '-copyFromLocal', '-f', local_file, hdfs_file])
-    qualified_table_name = '{0}.{1}'.format(unique_database, table_name)
-    impala_client.execute('load data inpath "{0}" into table {1}'.format(
-      hdfs_file, qualified_table_name))
+    source_files.append(local_file)
+  check_call(['hdfs', 'dfs', '-put', '-f', '-d'] + source_files + [hdfs_dir])
+
+  # Create the table
+  create_stmt = create_stmt.format(db=unique_database, tbl=table_name)
+  impala_client.execute(create_stmt)
 
 
 def grep_dir(dir, search):
