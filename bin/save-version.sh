dd34e1bd8d0f5303d36b9bea62d30d6b979f8c82
Zoltan Borok-Nagy <boroknagyz@cloudera.com>
2018-11-27 12:11:56 +0100

    Update version to 3.1.0-RELEASE
    Also include the last commit's git hash in bin/save-version.sh
    
    Change-Id: Id0327d18f2f1bbdfd430847cf4d414e190319fef

--- bin/save-version.sh
+++ bin/save-version.sh
@@ -21,8 +21,8 @@
 # Note: for internal (aka pre-release) versions, the version should have
 # "-INTERNAL" appended. Parts of the code will look for this to distinguish
 # between released and internal versions.
-VERSION=3.1.0-SNAPSHOT
-GIT_HASH=$(git rev-parse HEAD 2> /dev/null)
+VERSION=3.1.0-RELEASE
+GIT_HASH="7d3e9beee8d91bf775103ee9b362dcd08114abd4"
 if [ -z $GIT_HASH ]
 then
   GIT_HASH="Could not obtain git hash"
