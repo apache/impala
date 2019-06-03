0b564ec374c6e7e50f70fc7607fa4205fecf1916
Hao Hao <hao.hao@cloudera.com>
2019-06-03 10:08:23 -0700

    Disable custom cluster/service FE tests on S3
    This patch disables the custom cluster/service FE tests when running
    against S3, as only S3 FE tests should be ran under such environment.
    
    Change-Id: Iaa95fb1cef6349afafe3fd3677df74f7fa225245
    Reviewed-on: http://gerrit.cloudera.org:8080/13500
    Tested-by: Tim Armstrong <tarmstrong@cloudera.com>
    Reviewed-by: Tim Armstrong <tarmstrong@cloudera.com>

--- bin/run-all-tests.sh
+++ bin/run-all-tests.sh
@@ -246,13 +246,15 @@ do
     fi
     export IMPALA_MAX_LOG_FILES="${IMPALA_MAX_LOG_FILES_SAVE}"
 
-    # Run the FE custom cluster tests.
-    pushd "${IMPALA_FE_DIR}"
-    MVN_ARGS=" -Dtest=org.apache.impala.custom*.*Test "
-    if ! "${IMPALA_HOME}/bin/mvn-quiet.sh" -fae test ${MVN_ARGS}; then
-      TEST_RET_CODE=1
+    # Run the FE custom cluster tests only if not running against S3.
+    if [[ "$FE_TEST" == true && "${TARGET_FILESYSTEM}" != "s3" ]]; then
+      pushd "${IMPALA_FE_DIR}"
+      MVN_ARGS=" -Dtest=org.apache.impala.custom*.*Test "
+      if ! "${IMPALA_HOME}/bin/mvn-quiet.sh" -fae test ${MVN_ARGS}; then
+        TEST_RET_CODE=1
+      fi
+      popd
     fi
-    popd
   fi
 
   # Run the process failure tests.
