99cfa92d3c085aaea838b329097c65221c0cbd38
Abhishek Rawat <arawat@cloudera.com>
2019-07-01 15:32:53 -0700

    IMPALA-8736: test_hive_parquet_codec_interop fails on S3
    The test case fails (hanging untill the timeout is hit),  when trying
    to do hive insert on S3. Support for hive insert is flaky in S3 and so
    the testcase should be disabled for S3.
    
    Change-Id: Ie8eee8ba44e6acf4a069467f749756f9040ef641
    Reviewed-on: http://gerrit.cloudera.org:8080/13781
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- tests/custom_cluster/test_hive_parquet_codec_interop.py
+++ tests/custom_cluster/test_hive_parquet_codec_interop.py
@@ -20,6 +20,7 @@
 import pytest
 
 from tests.common.custom_cluster_test_suite import CustomClusterTestSuite
+from tests.common.skip import SkipIfS3
 from tests.common.test_dimensions import create_exec_option_dimension
 from tests.common.test_result_verifier import verify_query_result_is_equal
 from tests.util.filesystem_utils import get_fs_path
@@ -43,6 +44,7 @@ class TestParquetInterop(CustomClusterTestSuite):
     cls.ImpalaTestMatrix.add_constraint(
         lambda v: v.get_value('table_format').file_format == 'parquet')
 
+  @SkipIfS3.hive
   @pytest.mark.execute_serially
   @CustomClusterTestSuite.with_args("-convert_legacy_hive_parquet_utc_timestamps=true "
       "-hdfs_zone_info_zip=%s" % get_fs_path("/test-warehouse/tzdb/2017c.zip"))
