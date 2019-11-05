f9cf70d0352196e7b1b8465d6507ae4b16a3e82c
David Knupp <dknupp@cloudera.com>
2019-11-05 10:37:12 -0800

    IMPALA-9129: Add a test fixture that cleans up intentional core dumps
    Some negative tests produce core dumps intentionally. We should have a
    way of removing these as part of test cleanup.
    
    For custom cluster tests, it's likely the cores may actually be generated
    during the base class setup phase, which means it's too early for the
    test fixture to really be useful. Such was the case with the test case
    TestAuthorizationProvider::test_invalid_provider_flag. In this instance,
    we had to add the same steps directly to the tests.
    
    Testing done:
    For test_invalid_provider_flag, I made sure I had pre-existing core files
    in the IMPALA_HOME directory, then ran the test to confirm new cores were
    removed.
    
    -- 2019-11-06 19:53:27,303 INFO  MainThread: Removing core.impalad.61852 created by test_invalid_provider_flag
    -- 2019-11-06 19:53:27,375 INFO  MainThread: Removing core.impalad.61856 created by test_invalid_provider_flag
    -- 2019-11-06 19:53:27,450 INFO  MainThread: Removing core.impalad.61849 created by test_invalid_provider_flag
    
    ...and then made sure the pre-existing cores were still present.
    
    Change-Id: I778f27e820a6983894c1294d35627ddb04f5a51a
    Reviewed-on: http://gerrit.cloudera.org:8080/14640
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

---
+++ lib/python/impala_py_lib/helpers.py
@@ -0,0 +1,65 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
+import fnmatch
+import os
+import re
+import subprocess
+
+
+def exec_local_command(cmd):
+  """
+  Executes a command for the local bash shell and return stdout as a string.
+
+  Args:
+    cmd: command as a string
+
+  Return:
+    STDOUT
+  """
+  return subprocess.check_output(cmd.split())
+
+
+def find_all_files(fname_pattern, base_dir=os.getenv('IMPALA_HOME', '.')):
+  """
+  General utility to recursively find files matching a certain unix-like file pattern.
+
+  Args:
+    fname_pattern: Unix glob
+    base_dir: the root directory where searching should start
+
+  Returns:
+    A list of full paths relative to the give base_dir
+  """
+  file_glob = fnmatch.translate(fname_pattern)
+  matching_files = []
+
+  for root, dirs, files in os.walk(base_dir):
+    matching_files += [os.path.join(root, f) for f in files if re.match(file_glob, f)]
+
+  return matching_files
+
+
+def is_core_dump(file_path):
+  """
+  Determine whether given file is a core file. Works on CentOS and Ubuntu.
+
+  Args:
+    file_path: full path to a possible core file
+  """
+  file_std_out = exec_local_command("file %s" % file_path)
+  return "core file" in file_std_out and 'ELF' in file_std_out
