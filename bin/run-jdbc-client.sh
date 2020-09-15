e038db4479e3ae1232da15d3dbbfdd9aeb058006
Zoltan Borok-Nagy <boroknagyz@cloudera.com>
2020-09-15 15:38:59 +0200

    IMPALA-10177: Generate test-classpath.txt
    Currently the hive-jdbc jar is added under the scope 'test'.
    Therefore it won't be included in the build-classpath.txt because it
    only includes jars from the 'runtime' scope. Therefore
    run-hive-jdbc.sh throws ClassNotFoundException exception.
    
    This commit adds a new execution 'write-test-classpath' to pom.xml.
    It creates a test-classpath.txt. This can be used by scripts that need
    classes from the 'test' scope.
    
    Modified set-classpath.sh as well. If its first argument is 'test',
    then it also adds the contents of test-classpath.txt to the CLASSPATH.
    Now run-jdbc-client.sh sources 'set-classpath.sh test'.
    
    Testing:
    * executed core tests, turned out test_describe_formatted is broken,
      see IMPALA-10176 for details. Disabling that test for now.
    
    Change-Id: I47e4340fde9a5678d94459f676d8894dc5da14dc
    Reviewed-on: http://gerrit.cloudera.org:8080/16450
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- bin/run-jdbc-client.sh
+++ bin/run-jdbc-client.sh
@@ -17,5 +17,5 @@
 # specific language governing permissions and limitations
 # under the License.
 
-. ${IMPALA_HOME}/bin/set-classpath.sh
+. ${IMPALA_HOME}/bin/set-classpath.sh test
 "$JAVA" -cp $CLASSPATH org.apache.impala.testutil.ImpalaJdbcClient "$@"
