1d55b76e3a2997128de26929525c52369f0aa772
Joe McDonnell <joemcdonnell@cloudera.com>
2020-10-01 17:38:25 -0700

    Pin the json-smart version to 2.3
    With some maven repositories, Impala builds have been
    picking up json-smart with version 2.3-SNAPSHOT. This
    is not intentional (and it doesn't reproduce with public
    repositories). To improve the consistency of the build,
    pin the json-smart version to 2.3 with appropriate
    exclusions to prevent alternate versions.
    
    This also fixes up bin/jenkins/get_maven_statistics.sh
    to handle cases where maven didn't download anything.
    
    Testing:
     - Ran core job
    
    Change-Id: Iff92a61c9c3164e7e0c63c7569178415dcba9fb4
    Reviewed-on: http://gerrit.cloudera.org:8080/16536
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Reviewed-by: Joe McDonnell <joemcdonnell@cloudera.com>

--- bin/jenkins/get_maven_statistics.sh
+++ bin/jenkins/get_maven_statistics.sh
@@ -32,11 +32,19 @@ MVN_LOG=$1
 
 # Dump how many artifacts were downloaded from each repo
 echo "Number of artifacts downloaded from each repo:"
-cat "${MVN_LOG}" | grep "Downloaded from" | sed 's|.* Downloaded from ||' \
-    | cut -d: -f1 | sort | uniq -c
+if grep -q "Downloaded from" "${MVN_LOG}"; then
+  cat "${MVN_LOG}" | grep "Downloaded from" | sed 's|.* Downloaded from ||' \
+      | cut -d: -f1 | sort | uniq -c
+else
+  echo "No artifacts downloaded"
+fi
 
 # Dump how many artifacts we tried to download from each repo
 echo
 echo "Number of download attempts (successful or unsuccessful) per repo:"
-cat "${MVN_LOG}" | grep "Downloading from" | sed 's|.* Downloading from ||' \
-    | cut -d: -f1 | sort | uniq -c
+if grep -q "Downloading from" "${MVN_LOG}"; then
+  cat "${MVN_LOG}" | grep "Downloading from" | sed 's|.* Downloading from ||' \
+      | cut -d: -f1 | sort | uniq -c
+else
+  echo "No downloads attempted"
+fi
