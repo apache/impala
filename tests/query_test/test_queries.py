6dbf1ca09c9d8b53a0d37d9201508847e3247283
Qifan Chen <qchen@cloudera.com>
2020-10-15 10:18:05 -0400

    IMPALA-6628: Use unqualified table references in .test files run from test_queries.py
    This fix modified the following tests launched from test_queries.py by
    removing references to database 'functional' whenever possible. The
    objective of the change is to allow more testing coverage with different
    databases than the single 'functional' database. In the fix, neither new
    tables were added nor expected results were altered.
    
      empty.test
      inline-view-limit.test
      inline-view.test
      limit.test
      misc.test
      sort.test
      subquery-single-node.test
      subquery.test
      top-n.test
      union.test
      with-clause.test
    
    It was determined that other tests in
    testdata/workloads/functional-query/queries/QueryTest do not refer to
    'functional' or the references are a must for some reason.
    
    Testing
       Ran query_tests on these changed tests with exhaustive exploration
       strategy.
    
    Change-Id: Idd50eaaaba25e3bedc2b30592a314d2b6b83f972
    Reviewed-on: http://gerrit.cloudera.org:8080/16603
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- tests/query_test/test_queries.py
+++ tests/query_test/test_queries.py
@@ -146,6 +146,14 @@ class TestQueries(ImpalaTestSuite):
     self.run_test_case('QueryTest/inline-view-limit', vector)
 
   def test_subquery(self, vector):
+    if vector.get_value('table_format').file_format == 'parquet':
+     pytest.xfail("IMPALA-10252: Query returns less number of rows.")
+    if vector.get_value('table_format').file_format == 'hbase':
+        pytest.xfail("Table alltypesagg is populated differently in database "
+                "functional and functional_hbase: there are nulls in column "
+                "int_col in the former and none in the latter. "
+                "Testing query: select int_col from alltypesagg where int_col "
+                "is null")
     self.run_test_case('QueryTest/subquery', vector)
 
   def test_subquery_single_node(self, vector):
