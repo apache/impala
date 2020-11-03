846ce52b210ea751ba844fed09bc279a8e9c5f05
Thomas Tauber-Marshall <tmarshall@cloudera.com>
2020-11-03 14:22:08 -0800

    IMPALA-6861: Fix OpenSSL initialization
    Impalads currently initialize OpenSSL twice: once when initializing
    Thrift and once when initializing KRPC. The initialization is
    theoretically idempotent but not thread-safe, so its better to clean
    this up.
    
    This patch disables the Thrift version as its older (last updated in
    2015) and the KRPC version contains logic specific to more recent
    versions of OpenSSL. The catalogd and statestored also now use the
    KRPC version instead of the Thrift version.
    
    It also improves debuggability by adding some additional startup
    logging.
    
    Testing:
    - Passed existing SSL tests.
    
    Change-Id: I35b1362d40c8a12082cc8b531a38b4a485bac0e7
    Reviewed-on: http://gerrit.cloudera.org:8080/16704
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- be/src/rpc/authentication.h
+++ be/src/rpc/authentication.h
@@ -52,7 +52,7 @@ class AuthManager {
   ~AuthManager();
 
   /// Set up internal and external AuthProvider classes. This also initializes SSL (via
-  /// the creation of ssl_socket_factory_).
+  /// kudu::security::InitializeOpenSSL()).
   Status Init();
 
   /// Returns the authentication provider to use for "external" communication
@@ -80,14 +80,6 @@ class AuthManager {
   /// don't have to check the auth flags to figure out which auth provider to use.
   boost::scoped_ptr<AuthProvider> internal_auth_provider_;
   boost::scoped_ptr<AuthProvider> external_auth_provider_;
-
-  /// A thrift SSL socket factory must be created and live the lifetime of the process to
-  /// ensure that the thrift OpenSSL initialization code runs at Init(), and is not
-  /// unregistered (which thrift will do when the refcount of TSSLSocketFactory objects
-  /// reach 0), see IMPALA-4933. For simplicity, and because Kudu will expect SSL to be
-  /// initialized, this will be created regardless of whether or not SSL credentials are
-  /// specified. This factory isn't otherwise used.
-  boost::scoped_ptr<TSSLSocketFactory> ssl_socket_factory_;
 
   /// Used to authenticate usernames and passwords to LDAP.
   std::unique_ptr<ImpalaLdap> ldap_;
