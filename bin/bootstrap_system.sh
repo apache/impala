01f2457d0e514fb859bdfac100fd90514cfe301e
Laszlo Gaal <laszlo.gaal@cloudera.com>
2020-12-08 20:57:38 +0100

    IMPALA-10385: Fix RPM repo ID capitalization for Centos 8.3
    Centos 8.3 changed package repo ID capitalization from MixedCase
    to all lowercase. On Centos 8 snappy-devel is installed from the
    PowerTools repo, which is not enabled by default, so the install command
    has to enable is temporarily using the repo ID.
    The capitalization change broke bootstrap_system.sh, failing builds
    on Centos 8.
    
    The patch changes the `dnf install` call to use a glob pattern
    for the PowerTools repo ID to cover the naming conventions in all
    Centos 8.x releases.
    
    Change-Id: I224beb1189ce25ae66ecd78d70757537e117805a
    Reviewed-on: http://gerrit.cloudera.org:8080/16844
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- bin/bootstrap_system.sh
+++ bin/bootstrap_system.sh
@@ -268,7 +268,9 @@ redhat sudo yum install -y curl gawk gcc gcc-c++ git krb5-devel krb5-server \
 
 # Enable the Powertools repo for snappy-devel on RedHat 8
 redhat8 sudo yum install -y dnf-plugins-core
-redhat8 sudo yum install -y --enablerepo="PowerTools*" snappy-devel
+# Package repo IDs changed from mixed case to all-lowercase between Centos 8.2
+# and 8.3, so use globbing to cover both conventions
+redhat8 sudo yum install -y --enablerepo="[Pp]ower[Tt]ools*" snappy-devel
 
 # RedHat / CentOS 8 exposes only specific versions of Python.
 # Set up unversioned default Python 2.x for older CentOS versions
