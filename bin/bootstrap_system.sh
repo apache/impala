ef7de393325a5e9343e24e90f565697b03f25aac
stiga-huang <huangquanlong@gmail.com>
2021-09-24 10:43:46 +0800

    IMPALA-10828: Impala 4.0 should pin to a specifit toolchain commit when building on ARM
    While building on ARM platform, bin/bootstrap_system.sh will compile
    native-toolchain using the latest commit, which will cause build
    failures if we are building branches other than master.
    
    This patch addes a branch name (asf-impala-4.0) when cloning the
    native-toolchain repo. So we can build Impala 4.0 on ARM without
    manually modifying bin/bootstrap_system.sh.
    
    Tests:
     - Tested on ARM platform.
    
    Change-Id: Id353e0132b684a338e2c5454331be9bff9a8be14

--- bin/bootstrap_system.sh
+++ bin/bootstrap_system.sh
@@ -493,8 +493,8 @@ if [[ $ARCH_NAME == 'aarch64' ]]; then
   echo -e "\n$SET_TOOLCHAIN_HOME" >> "${IMPALA_HOME}/bin/impala-config-local.sh"
   eval "$SET_TOOLCHAIN_HOME"
   if ! [[ -d "$NATIVE_TOOLCHAIN_HOME" ]]; then
-    time -p git clone https://github.com/cloudera/native-toolchain/ \
-      "$NATIVE_TOOLCHAIN_HOME"
+    time -p git clone --single-branch -b asf-impala-4.0 \
+      https://github.com/cloudera/native-toolchain/ "$NATIVE_TOOLCHAIN_HOME"
   fi
   cd "$NATIVE_TOOLCHAIN_HOME"
   git pull
