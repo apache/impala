b11436989248f1a8bee16da59fbe033dece374de
stiga-huang <huangquanlong@gmail.com>
2022-03-15 19:51:15 +0800

    Prepare 3.4.1 RC1

--- bin/save-version.sh
+++ bin/save-version.sh
@@ -21,8 +21,8 @@
 # Note: for internal (aka pre-release) versions, the version should have
 # "-INTERNAL" appended. Parts of the code will look for this to distinguish
 # between released and internal versions.
-VERSION=3.4.0-RELEASE
-GIT_HASH=$(git rev-parse HEAD 2> /dev/null)
+VERSION=3.4.1-RELEASE
+GIT_HASH=0d55beb121ba2fdf2c2ec68d1f26c5a8de028803
 if [ -z $GIT_HASH ]
 then
   GIT_HASH="Could not obtain git hash"
