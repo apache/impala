bb53a8d68e6714b877527c3065861a5266c7d076
stiga-huang <huangquanlong@gmail.com>
2022-09-29 06:33:18 +0800

    Update version to 4.1.1-RELEASE
    Change-Id: I28e2acc91165d0fbc769d5ae182fee7b6e76c6ce

--- bin/save-version.sh
+++ bin/save-version.sh
@@ -22,7 +22,7 @@
 # "-INTERNAL" appended. Parts of the code will look for this to distinguish
 # between released and internal versions.
 VERSION=${IMPALA_VERSION}
-GIT_HASH=$(git rev-parse HEAD 2> /dev/null)
+GIT_HASH=dddc17c1ef6037f3881a92f9188ffa57acd1bece
 if [ -z $GIT_HASH ]
 then
   GIT_HASH="Could not obtain git hash"
