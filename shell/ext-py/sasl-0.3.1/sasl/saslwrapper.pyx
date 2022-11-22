16190b4f77a86ef008bf28334dcca50e7c498556
Michael Smith <michael.smith@cloudera.com>
2022-11-22 10:02:20 -0800

    IMPALA-11737: Update sasl to 0.3.1 for Python 3.10
    sasl 0.2.1 fails to build with Python 3.10. Updates to sasl 0.3.1 for
    Python 3.10 compatibility.
    
    Testing:
    - built under Python 3.8
    - automated tests will test with built bundle and pip install using
    current Python version
    - pip3 installed shell/build/dist on Ubuntu 22.04 with Python 3.10
    
    Change-Id: I6b522f2b8cb5546150cd3274c7670a6ca9b8ff63
    Reviewed-on: http://gerrit.cloudera.org:8080/19265
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Reviewed-by: Csaba Ringhofer <csringhofer@cloudera.com>

--- shell/ext-py/sasl-0.3.1/sasl/saslwrapper.pyx
+++ shell/ext-py/sasl-0.3.1/sasl/saslwrapper.pyx
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+#cython: language_level=3
+
 from six import string_types, PY3
 
 from libcpp cimport bool
