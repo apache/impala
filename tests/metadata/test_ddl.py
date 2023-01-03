5b349cb5492827e7584412de3922d263ac441d26
Tamas Mate <tmate@cloudera.com>
2023-01-03 11:42:32 +0100

    IMPALA-11818: Fix flaky TestDdlStatements.test_describe_materialized_view
    This test is executed on multiple ImpalaDs serially. This caused some
    race condition between the CREATE/DROP database statements, the test
    could fail with "Database does not exist". This commit adds SYNC_DDL to
    the test, so the CREATE/DROP db won’t return till the metadata is
    propagated to each ImpalaDs.
    
    Testing:
     - Could not trigger the failure locally, but ran the test
    
    Change-Id: I399cb70d4819f27ecd752bf709a77e2e31c5ab54
    Reviewed-on: http://gerrit.cloudera.org:8080/19395
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- tests/metadata/test_ddl.py
+++ tests/metadata/test_ddl.py
@@ -907,6 +907,7 @@ class TestDdlStatements(TestDdlBase):
     comment = self._get_column_comment(table, 'j')
     assert "comment4" == comment
 
+  @UniqueDatabase.parametrize(sync_ddl=True)
   def test_describe_materialized_view(self, vector, unique_database):
     vector.get_value('exec_option')['abort_on_error'] = False
     self.run_test_case('QueryTest/describe-materialized-view', vector,
