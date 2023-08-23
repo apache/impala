d954c063b5e80ddc20751bd42375a78d9ee8eac0
jasondrogba <757104667@qq.com>
2023-08-23 15:10:00 +0800

    change link and sha512sum of ant and maven

--- bin/bootstrap_system.sh
+++ bin/bootstrap_system.sh
@@ -241,19 +241,19 @@ redhat sudo yum clean all
 
 # Download ant for centos
 redhat sudo wget -nv \
-  https://downloads.apache.org/ant/binaries/apache-ant-1.9.14-bin.tar.gz
-redhat sha512sum -c - <<< '487dbd1d7f678a92924ba884a57e910ccb4fe565c554278795a8fdfc80c4e88d81ebc2ccecb5a8f353f0b2076572bb921499a2cadb064e0f44fc406a3c31da20  apache-ant-1.9.14-bin.tar.gz'
-redhat sudo tar -C /usr/local -xzf apache-ant-1.9.14-bin.tar.gz
-redhat sudo ln -s /usr/local/apache-ant-1.9.14/bin/ant /usr/local/bin
+  https://downloads.apache.org/ant/binaries/apache-ant-1.9.16-bin.tar.gz
+redhat sha512sum -c - <<< '8d542a7a636a491e76170148881b6f413f4564aad1ab034f426bd946be93382001862bd6c60865aa2b57b39b9633e0181fe8997dd6323123aaf76b410ec4a366  apache-ant-1.9.16-bin.tar.gz'
+redhat sudo tar -C /usr/local -xzf apache-ant-1.9.16-bin.tar.gz
+redhat sudo ln -sf /usr/local/apache-ant-1.9.16/bin/ant /usr/local/bin
 
 # Download maven for all OSes, since the OS-packaged version can be
 # pretty old.
-if [ ! -d /usr/local/apache-maven-3.5.4 ]; then
+if [ ! -d /usr/local/apache-maven-3.8.8 ]; then
   sudo wget -nv \
-    https://downloads.apache.org/maven/maven-3/3.5.4/binaries/apache-maven-3.5.4-bin.tar.gz
-  sha512sum -c - <<< '2a803f578f341e164f6753e410413d16ab60fabe31dc491d1fe35c984a5cce696bc71f57757d4538fe7738be04065a216f3ebad4ef7e0ce1bb4c51bc36d6be86  apache-maven-3.5.4-bin.tar.gz'
-  sudo tar -C /usr/local -xzf apache-maven-3.5.4-bin.tar.gz
-  sudo ln -s /usr/local/apache-maven-3.5.4/bin/mvn /usr/local/bin
+    https://downloads.apache.org/maven/maven-3/3.8.8/binaries/apache-maven-3.8.8-bin.tar.gz
+  sha512sum -c - <<< '332088670d14fa9ff346e6858ca0acca304666596fec86eea89253bd496d3c90deae2be5091be199f48e09d46cec817c6419d5161fb4ee37871503f472765d00  apache-maven-3.8.8-bin.tar.gz'
+  sudo tar -C /usr/local -xzf apache-maven-3.8.8-bin.tar.gz
+  sudo ln -sf /usr/local/apache-maven-3.8.8/bin/mvn /usr/local/bin
 fi
 
 if ! { service --status-all | grep -E '^ \[ \+ \]  ssh$'; }
