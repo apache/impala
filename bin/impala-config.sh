7ad94006563b88d9221b4ac978dbf5b4fc0a3ca1
Peter Rozsa <prozsa@cloudera.com>
2024-04-29 18:56:20 +0200

    IMPALA-13044: Upgrade bouncycastle to 1.78
    This patch upgrades bouncycastle to 1.78. As of bouncycastle:1.71, the
    *-jdk15on artifact is no longer available, the artifact is changed to
    *-jdk18on.
    
    Tests:
     - core tests ran
    
    Change-Id: I8372916ab79b863e7a07d22e8333abd54492fa29
    Reviewed-on: http://gerrit.cloudera.org:8080/21371
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- bin/impala-config.sh
+++ bin/impala-config.sh
@@ -278,7 +278,7 @@ export APACHE_OZONE_VERSION=1.3.0
 
 # Java dependencies that are not also runtime components. Declaring versions here allows
 # other branches to override them in impala-config-branch.sh for cleaner patches.
-export IMPALA_BOUNCY_CASTLE_VERSION=1.68
+export IMPALA_BOUNCY_CASTLE_VERSION=1.78
 export IMPALA_COS_VERSION=3.1.0-8.0.8
 export IMPALA_DERBY_VERSION=10.14.2.0
 export IMPALA_GUAVA_VERSION=32.0.1-jre
