22b59d27d0be25999eba4c839ea157c279939d76
Michael Smith <michael.smith@cloudera.com>
2024-07-18 14:28:41 -0700

    IMPALA-13243: Update Dropwizard Metrics to 4.2.x
    Updates Dropwizard Metrics components to the latest 4.2.x release,
    4.2.26. We directly use metrics-core, and metrics-jvm/metrics-json are
    imported via Hive (via
    https://github.com/joshelser/dropwizard-hadoop-metrics2).
    
    Dropwizard Metrics manually tested with these versions on
    https://github.com/joshelser/dropwizard-hadoop-metrics2/pull/8.
    
    Change-Id: Ie9bec7a7c23194604430531bd83b25c5969e888e
    Reviewed-on: http://gerrit.cloudera.org:8080/21599
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- bin/impala-config.sh
+++ bin/impala-config.sh
@@ -297,6 +297,7 @@ export IMPALA_SPRINGFRAMEWORK_VERSION=5.3.37
 export IMPALA_XMLSEC_VERSION=2.2.6
 export IMPALA_OBS_VERSION=3.1.1-hw-42
 export IMPALA_DBCP2_VERSION=2.9.0
+export IMPALA_DROPWIZARD_METRICS_VERSION=4.2.26
 
 # When Impala is building docker images on Redhat-based distributions,
 # it is useful to be able to customize the base image. Some users will
