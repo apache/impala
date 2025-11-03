0572dba2455fd96cf133c505ee2a899437a0802a
Riza Suminto <riza.suminto@cloudera.com>
2025-11-03 07:45:28 -0800

    IMPALA-14529: Bumping Kudu version to pickup latest KUDU-1261 patch
    This commit bump Impala toolchain to pickup latest Kudu version up to
    commit 60f5e5267b92c39485a66121d3ce3cc7ef57b0e0 (KUDU-1261 make
    ArrayCellMetadataView::Init() more robust).
    
    Change-Id: I68009e5fefd053882f5504cd2520bacb189a1b04
    Reviewed-on: http://gerrit.cloudera.org:8080/23631
    Reviewed-by: Riza Suminto <riza.suminto@cloudera.com>
    Reviewed-by: Jason Fehr <jfehr@cloudera.com>
    Tested-by: Riza Suminto <riza.suminto@cloudera.com>

--- bin/impala-config.sh
+++ bin/impala-config.sh
@@ -81,13 +81,13 @@ export USE_AVRO_CPP=${USE_AVRO_CPP:=false}
 # moving to a different build of the toolchain, e.g. when a version is bumped or a
 # compile option is changed. The build id can be found in the output of the toolchain
 # build jobs, it is constructed from the build number and toolchain git hash prefix.
-export IMPALA_TOOLCHAIN_BUILD_ID_AARCH64=141-dce1ad6256
-export IMPALA_TOOLCHAIN_BUILD_ID_X86_64=590-dce1ad6256
+export IMPALA_TOOLCHAIN_BUILD_ID_AARCH64=147-1ee40f5a10
+export IMPALA_TOOLCHAIN_BUILD_ID_X86_64=596-1ee40f5a10
 export IMPALA_TOOLCHAIN_REPO=\
 ${IMPALA_TOOLCHAIN_REPO:-https://github.com/cloudera/native-toolchain.git}
 export IMPALA_TOOLCHAIN_BRANCH=${IMPALA_TOOLCHAIN_BRANCH:-master}
 export IMPALA_TOOLCHAIN_COMMIT_HASH=\
-${IMPALA_TOOLCHAIN_COMMIT_HASH-dce1ad625603e5098a32d4aa767e31d8be87df2e}
+${IMPALA_TOOLCHAIN_COMMIT_HASH-1ee40f5a10e11d5eb836902ecaa673384ca507f5}
 # Compare the build ref in build IDs by removing everything up-to-and-including the
 # first hyphen.
 if [ "${IMPALA_TOOLCHAIN_BUILD_ID_AARCH64#*-}" \
@@ -1123,7 +1123,7 @@ fi
 # overall build type) and does not apply when using a local Kudu build.
 export USE_KUDU_DEBUG_BUILD=${USE_KUDU_DEBUG_BUILD-false}
 
-export IMPALA_KUDU_VERSION=${IMPALA_KUDU_VERSION-"16689973a"}
+export IMPALA_KUDU_VERSION=${IMPALA_KUDU_VERSION-"60f5e5267"}
 export IMPALA_KUDU_HOME=${IMPALA_TOOLCHAIN_PACKAGES_HOME}/kudu-$IMPALA_KUDU_VERSION
 export IMPALA_KUDU_JAVA_HOME=\
 ${IMPALA_TOOLCHAIN_PACKAGES_HOME}/kudu-${IMPALA_KUDU_VERSION}/java
