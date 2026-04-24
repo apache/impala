7ab390a662b4aaf6736b354f75f6f2439c5f1455
Zoltan Borok-Nagy <boroknagyz@cloudera.com>
2026-04-24 17:37:27 +0200

    IMPALA-14923: Run Iceberg tests with no block locations
    When block locations are not available Impala works quite differently.
    E.g. it doesn't create TScanRanges in the Frontend, but generates them
    in the backend from TFileSplitGeneratorSpec objects.
    
    We already have test_disabled_block_locations.py that disables block
    location loading but it didn't test Iceberg V3 tables.
    
    This patch executes a few Iceberg tests without block locations to
    somewhat simulate the behavior on object stores. This way we can
    catch bugs in pre-commit that would otherwise only be found later.
    
    Change-Id: I62c3509ec11e657c634c93377176eec9118c02cc
    Reviewed-on: http://gerrit.cloudera.org:8080/24237
    Reviewed-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>
    Tested-by: Impala Public Jenkins <impala-public-jenkins@cloudera.com>

--- tests/custom_cluster/test_disabled_block_locations.py
+++ tests/custom_cluster/test_disabled_block_locations.py
@@ -18,6 +18,8 @@
 from __future__ import absolute_import, division, print_function
 import pytest
 from os import getenv
+from tests.common.file_utils import create_iceberg_table_from_directory
+from tests.common.skip import SkipIf
 from tests.util.filesystem_utils import IS_HDFS
 
 from tests.common.custom_cluster_test_suite import CustomClusterTestSuite
@@ -31,9 +33,51 @@ class TestDisabledBlockLocations(CustomClusterTestSuite):
   def setup_class(cls):
     super(TestDisabledBlockLocations, cls).setup_class()
 
+  def load_table(self, database, table_name, format="parquet",
+      table_location="${IMPALA_HOME}/testdata/data/iceberg_test/iceberg_v3"):
+    create_iceberg_table_from_directory(self.client, database,
+        table_name, format, table_location)
+
   @pytest.mark.execute_serially
   @CustomClusterTestSuite.with_args(custom_core_site_dir=CORE_SITE_CONFIG_DIR)
   def test_no_block_locations(self, vector):
     self.run_test_case('QueryTest/no-block-locations', vector)
     if IS_HDFS:
       self.run_test_case('QueryTest/no-block-locations-hdfs-only', vector)
+
+  @SkipIf.not_dfs
+  @pytest.mark.execute_serially
+  @CustomClusterTestSuite.with_args(custom_core_site_dir=CORE_SITE_CONFIG_DIR)
+  def test_iceberg_smoke(self, vector, unique_database):
+    self.run_test_case('QueryTest/iceberg-create', vector, unique_database)
+    self.run_test_case('QueryTest/iceberg-external', vector, unique_database)
+    self.run_test_case('QueryTest/iceberg-insert', vector, unique_database)
+    self.run_test_case('QueryTest/iceberg-delete', vector, unique_database)
+    self.run_test_case('QueryTest/iceberg-delete-partitioned', vector, unique_database)
+
+  @SkipIf.not_dfs
+  @pytest.mark.execute_serially
+  @CustomClusterTestSuite.with_args(custom_core_site_dir=CORE_SITE_CONFIG_DIR)
+  def test_iceberg_v3_smoke(self, vector, unique_database):
+    self.load_table(unique_database, "iceberg_v3_row_lineage")
+    self.load_table(unique_database, "iceberg_v3_row_lineage_orc", format="orc")
+    self.run_test_case('QueryTest/iceberg-v3-row-lineage', vector, unique_database)
+
+    self.load_table(unique_database, "iceberg_v3_deletion_vectors")
+    self.run_test_case('QueryTest/iceberg-v3-delete', vector, unique_database)
+    self.run_test_case('QueryTest/iceberg-v3-delete-partition-sort',
+        vector, unique_database)
+
+    self.run_test_case('QueryTest/iceberg-v3-optimize', vector, unique_database)
+
+    self.execute_query("drop table {}.{}".format(
+                       unique_database, "iceberg_v3_deletion_vectors"))
+    self.load_table(unique_database, "iceberg_v3_deletion_vectors")
+    self.run_test_case('QueryTest/iceberg-v3-delete-existing-dv', vector, unique_database)
+
+    self.load_table(unique_database, "iceberg_v2_delete_equality",
+        table_location="${IMPALA_HOME}/testdata/data/iceberg_test/hadoop_catalog/ice")
+    self.run_test_case('QueryTest/iceberg-v3-delete-v2-equality-upgrade',
+        vector, unique_database)
+
+    self.run_test_case('QueryTest/iceberg-v3-update', vector, unique_database)
